439bd3ea883d3e3bd4e2ced8c6c870fddbef0d76
Anwesh43 <anweshthecool0@gmail.com>
2025-05-06 09:31:50 +0530

    linestartfromleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
+import com.example.linestartfromleftview.LineStartFromLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LineStartFromLeftView.create(this)
     }
 }
