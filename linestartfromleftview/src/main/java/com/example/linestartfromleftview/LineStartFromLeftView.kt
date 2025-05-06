b8bfe5fa56dc7d53a228ca5f915a345b90d91a87
Anwesh43 <anweshthecool0@gmail.com>
2025-05-06 09:22:43 +0530

    linestartfromleftview: adding LSFLNode class

--- linestartfromleftview/src/main/java/com/example/linestartfromleftview/LineStartFromLeftView.kt
+++ linestartfromleftview/src/main/java/com/example/linestartfromleftview/LineStartFromLeftView.kt
@@ -116,4 +116,45 @@ class LineStartFromLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSFLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSFLNode? = null
+        private var prev : LSFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSFLNode {
+            var curr : LSFLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
