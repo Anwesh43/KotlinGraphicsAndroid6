c3273dc93515f8c1f224e6fe49633449d6a07347
Anwesh43 <anweshthecool0@gmail.com>
2025-05-06 08:35:47 +0530

    linestartfromleftview: adding module setup

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -16,32 +16,5 @@ import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinGraphicsAndroid6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinGraphicsAndroid6Theme {
-        Greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "KotlinGraphicsAndroid6"
 include(":app")
+include(":linestartfromleftview")
