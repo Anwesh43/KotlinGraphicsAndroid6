5a54b0fd1e624ed28bbd336b488e3f2a7b222872
Anwesh43 <anweshthecool0@gmail.com>
2025-05-07 09:32:22 +0530

    dropmultisemicirclesview: adding Canvas extension methods

--- dropmultisemicirclesview/src/main/java/com/example/dropmultisemicirclesview/DropMultiSemiCirclesView.kt
+++ dropmultisemicirclesview/src/main/java/com/example/dropmultisemicirclesview/DropMultiSemiCirclesView.kt
@@ -22,10 +22,42 @@ val scGap : Float = 0.03f / parts
 val arcs : Int = 4
 val rot : Float = -180f
 val delay : Long = 20
-val sizeFactor : Float = 6.9f
 val strokeFactor : Float = 90f
 val backColor : Int = "#BDBDBD".toColorInt()
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDropMultiSemiCircles(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = (w * 0.5f) / (arcs)
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(1))
+        for (j in 0..(arcs - 1)) {
+            val ds0j : Float = dsc(0).divideScale(j, arcs)
+            val ds2j : Float = dsc(2).divideScale(j, arcs)
+            drawXY(-w / 2 + size * j, h * 0.5f * ds2j) {
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * ds0j, false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawDMSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDropMultiSemiCircles(scale, w, h, paint)
+}
