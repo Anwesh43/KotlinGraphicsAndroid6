b6d3eed92dd3e8e0d81521e778e0c095f89658e3
Anwesh43 <anweshthecool0@gmail.com>
2025-05-08 00:32:43 +0530

    slantlinejoinarcview: adding SLJANode class

--- slantlinejoinarcview/src/main/java/com/example/slantlinejoinarcview/SlantLineJoinArcView.kt
+++ slantlinejoinarcview/src/main/java/com/example/slantlinejoinarcview/SlantLineJoinArcView.kt
@@ -123,4 +123,45 @@ class SlantLineJoinArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLJANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLJANode? = null
+        private var prev : SLJANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLJANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLJANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLJANode {
+            var curr : SLJANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
