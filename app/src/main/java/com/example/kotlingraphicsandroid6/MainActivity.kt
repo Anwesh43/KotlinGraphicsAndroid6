93d0b8458eedcca365596af478f0abacd5d97ce2
Anwesh43 <anweshthecool0@gmail.com>
2025-05-08 00:37:00 +0530

    slantlinejoinarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dropmultisemicirclesview.DropMultiSemiCirclesView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
+import com.example.slantlinejoinarcview.SlantLineJoinArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DropMultiSemiCirclesView.create(this)
+        SlantLineJoinArcView.create(this)
     }
 }
