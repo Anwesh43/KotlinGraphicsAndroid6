13ed89b1f7a79a80e73dbbcbc1feac8332eb874e
Anwesh43 <anweshthecool0@gmail.com>
2025-05-08 00:25:55 +0530

    slantlinejoinarcview: adding SlantLineJoinArcView class

--- slantlinejoinarcview/src/main/java/com/example/slantlinejoinarcview/SlantLineJoinArcView.kt
+++ slantlinejoinarcview/src/main/java/com/example/slantlinejoinarcview/SlantLineJoinArcView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawSLJANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSlantLineJoinArc(scale, w, h, paint)
 }
+
+class SlantLineJoinArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
