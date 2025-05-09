7c229a2fcf8e30a8f1abf4050afd1922dcc79c48
Anwesh43 <anweshthecool0@gmail.com>
2025-05-09 07:32:26 +0530

    stepquarterarcleftview: adding SQALNode class

--- stepquarterarcvleftview/src/main/java/com/example/stepquarterarcvleftview/StepQuarterArcLeftView.kt
+++ stepquarterarcvleftview/src/main/java/com/example/stepquarterarcvleftview/StepQuarterArcLeftView.kt
@@ -120,4 +120,45 @@ class StepQuarterArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SQALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SQALNode? = null
+        private var prev : SQALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SQALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSQALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SQALNode {
+            var curr : SQALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
