043ec10a003fd69e28652e9403b0feee889adc71
Anwesh43 <anweshthecool0@gmail.com>
2025-05-11 10:56:57 +0530

    dividelinearcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biarcdividemoveview.BiArcDivideMoveView
+import com.example.dividelinearcjoinview.DivideLineArcJoinView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcDivideMoveView.create(this)
+        DivideLineArcJoinView.create(this)
     }
 }
