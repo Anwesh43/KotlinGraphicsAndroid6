4b63209984a7fae4cc943aa4a7740defce5e7e3a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-11 10:34:38 +0530

    dividelinearcjoinview: adding Canvas extension methods

--- dividelinearcjoinview/src/main/java/com/example/dividelinearcjoinview/DivideLineArcJoinView.kt
+++ dividelinearcjoinview/src/main/java/com/example/dividelinearcjoinview/DivideLineArcJoinView.kt
@@ -25,7 +25,41 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 180f
 val sweep : Float = 45f
 val start : Float = 135f
+val arcSweep : Float = 270f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDivideLineArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate((1 + dsc(0)) * sweep * (1f - 2 * j))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), start + sweep * dsc(2), arcSweep * dsc(1) - 2 * sweep * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawDLAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDivideLineArcJoin(scale, w, h, paint)
+}
