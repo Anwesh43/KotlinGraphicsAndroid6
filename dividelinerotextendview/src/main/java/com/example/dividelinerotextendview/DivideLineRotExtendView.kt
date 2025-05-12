85747a71ee41bbbb64e17f53d2098c8c24d64ace
Anwesh43 <anweshthecool0@gmail.com>
2025-05-12 08:31:26 +0530

    dividelinerotextendview: adding DLRENode class

--- dividelinerotextendview/src/main/java/com/example/dividelinerotextendview/DivideLineRotExtendView.kt
+++ dividelinerotextendview/src/main/java/com/example/dividelinerotextendview/DivideLineRotExtendView.kt
@@ -126,4 +126,45 @@ class DivideLineRotExtendView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DLRENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DLRENode? = null
+        private var prev : DLRENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DLRENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDLRENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DLRENode {
+            var curr : DLRENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
