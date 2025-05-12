c7bf770e443e5c99050941417409920e8381c823
Anwesh43 <anweshthecool0@gmail.com>
2025-05-13 02:28:42 +0530

    caparclinecircleview: adding Canvas extension methods

--- caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
+++ caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
@@ -28,3 +28,38 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCapArcLineCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(-size, 0f) {
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(0), false, paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, size * dsc(1).divideScale(0, 2), 0f, paint)
+            drawXY(size * (1 - dsc(2)), 0f) {
+                drawArc(RectF(0f, -size / 2, size, 0f), 180f, 180f * dsc(1).divideScale(1, 2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawCALCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCapArcLineCircle(scale, w, h, paint)
+}
