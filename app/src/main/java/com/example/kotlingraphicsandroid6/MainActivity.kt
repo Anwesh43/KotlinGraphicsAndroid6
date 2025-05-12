846b1d166e848c22bc54d58eb7f8bb57061503f2
Anwesh43 <anweshthecool0@gmail.com>
2025-05-12 08:40:09 +0530

    dividelinerotextendview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dividelinearcjoinview.DivideLineArcJoinView
+import com.example.dividelinerotextendview.DivideLineRotExtendView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DivideLineArcJoinView.create(this)
+        DivideLineRotExtendView.create(this)
     }
 }
