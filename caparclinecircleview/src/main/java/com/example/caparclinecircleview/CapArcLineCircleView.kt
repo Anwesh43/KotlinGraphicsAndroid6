4ec2ec27e7bf140fd79e69f23218aca04ca771e3
Anwesh43 <anweshthecool0@gmail.com>
2025-05-13 02:40:51 +0530

    caparclinecircleview: adding static create method

--- caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
+++ caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
@@ -215,4 +215,13 @@ class CapArcLineCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : CapArcLineCircleView {
+            val view : CapArcLineCircleView = CapArcLineCircleView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
