9d3d63372657a7828ea8ce21d186206507316b65
Anwesh43 <anweshthecool0@gmail.com>
2025-05-13 02:41:21 +0530

    caparclinecircleview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dividelinerotextendview.DivideLineRotExtendView
+import com.example.caparclinecircleview.CapArcLineCircleView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DivideLineRotExtendView.create(this)
+        CapArcLineCircleView.create(this)
     }
 }
