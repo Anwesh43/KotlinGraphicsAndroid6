30e9bc9849a522bf5ecc5bc2506fed76ebc22d25
Anwesh43 <anweshthecool0@gmail.com>
2025-05-13 02:36:04 +0530

    caparclinecircleview: adding CALCNode class

--- caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
+++ caparclinecircleview/src/main/java/com/example/caparclinecircleview/CapArcLineCircleView.kt
@@ -126,4 +126,45 @@ class CapArcLineCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CALCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CALCNode? = null
+        private var prev : CALCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CALCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCALCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CALCNode {
+            var curr : CALCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
