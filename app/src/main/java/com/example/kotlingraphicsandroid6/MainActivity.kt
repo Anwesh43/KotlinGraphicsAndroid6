268e3515623e6494f679f785761a1f8bc55476a3
Anwesh43 <anweshthecool0@gmail.com>
2025-05-16 04:28:27 +0530

    rightanglebentdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.lineslantarcjoinview.LineSlantArcJoinView
+import com.example.rightanglebentdownview.RightAngleBentDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineSlantArcJoinView.create(this)
+        RightAngleBentDownView.create(this)
     }
 }
