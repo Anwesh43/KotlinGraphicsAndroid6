a191e4684e80bffc958b35dfa4b0b47c29ed5b2e
Anwesh43 <anweshthecool0@gmail.com>
2025-05-16 03:56:42 +0530

    rightanglebentdownview: adding RightAngleBentDownView class

--- rightanglebentdownview/src/main/java/com/example/rightanglebentdownview/RightAngleBentDownView.kt
+++ rightanglebentdownview/src/main/java/com/example/rightanglebentdownview/RightAngleBentDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawRABDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightAngleBentDown(scale, w, h, paint)
 }
+
+class RightAngleBentDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
