0221aeeca9a6b5031bda59f6f3222377b5a19972
Anwesh43 <anweshthecool0@gmail.com>
2025-05-16 04:26:53 +0530

    rightanglebentdownview: adding Renderer class

--- rightanglebentdownview/src/main/java/com/example/rightanglebentdownview/RightAngleBentDownView.kt
+++ rightanglebentdownview/src/main/java/com/example/rightanglebentdownview/RightAngleBentDownView.kt
@@ -190,4 +190,27 @@ class RightAngleBentDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RightAngleBentDownView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val rabd : RightAngleBentDown = RightAngleBentDown(0)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rabd.draw(canvas, paint)
+            animator.animate {
+                rabd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rabd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
