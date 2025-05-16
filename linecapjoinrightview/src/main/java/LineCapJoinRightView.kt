a9d21227ed39ac11cb0ea5c7e7aacf48f0e079d2
Anwesh43 <anweshthecool0@gmail.com>
2025-05-17 02:59:30 +0530

    linecapjoinrightview: adding LineCapJoinRight class

--- linecapjoinrightview/src/main/java/LineCapJoinRightView.kt
+++ linecapjoinrightview/src/main/java/LineCapJoinRightView.kt
@@ -163,4 +163,27 @@ class LineCapJoinRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineCapJoinRight(var i : Int = 0, val state : State = State()) {
+
+        private var curr : LCJRNode = LCJRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
