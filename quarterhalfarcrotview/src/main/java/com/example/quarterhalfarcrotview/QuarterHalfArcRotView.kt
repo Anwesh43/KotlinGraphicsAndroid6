f7d3a506da0bfb74d2f433b5fa17c07cdf5e6f9d
Anwesh43 <anweshthecool0@gmail.com>
2025-05-19 10:19:32 +0530

    quarterhalfarcrotview: adding QHARNode class

--- quarterhalfarcrotview/src/main/java/com/example/quarterhalfarcrotview/QuarterHalfArcRotView.kt
+++ quarterhalfarcrotview/src/main/java/com/example/quarterhalfarcrotview/QuarterHalfArcRotView.kt
@@ -119,4 +119,45 @@ class QuarterHalfArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QHARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : QHARNode? = null
+        private var prev : QHARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QHARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQHARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QHARNode {
+            var curr : QHARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
