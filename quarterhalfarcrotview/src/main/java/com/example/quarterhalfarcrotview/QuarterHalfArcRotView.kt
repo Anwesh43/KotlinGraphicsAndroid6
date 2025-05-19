b4747bfd0f07ae1298cf8d1f32ad9c23297f08c5
Anwesh43 <anweshthecool0@gmail.com>
2025-05-19 09:59:45 +0530

    quarterhalfarcrotview: adding QuarterHalfArcRotView class

--- quarterhalfarcrotview/src/main/java/com/example/quarterhalfarcrotview/QuarterHalfArcRotView.kt
+++ quarterhalfarcrotview/src/main/java/com/example/quarterhalfarcrotview/QuarterHalfArcRotView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawQHARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawQuarterHalfArcRot(scale, w, h, paint)
 }
+
+class QuarterHalfArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
