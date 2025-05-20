9b593836d69893a675892ba60d2d8695ccf4248f
Anwesh43 <anweshthecool0@gmail.com>
2025-05-21 02:25:33 +0530

    linejoinclosearcview: adding LineJoinCloseArcView class

--- linejoinclosearview/src/main/java/com/example/linejoinclosearview/LineJoinCloseArcView.kt
+++ linejoinclosearview/src/main/java/com/example/linejoinclosearview/LineJoinCloseArcView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLJCANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineJoinCloseArc(scale, w, h, paint)
 }
+
+class LineJoinCloseArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
