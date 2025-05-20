e74d878ba11bea66af0602554323e145b34351ae
Anwesh43 <anweshthecool0@gmail.com>
2025-05-20 07:25:38 +0530

    arclinecloserightview: adding ArcLineCloseRightView class

--- arclinecloserightview/src/main/java/com/example/arclinecloserightview/ArcLineCloseRightView.kt
+++ arclinecloserightview/src/main/java/com/example/arclinecloserightview/ArcLineCloseRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawALCRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcLineCloseRight(scale, w, h, paint)
 }
+
+class ArcLineCloseRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
