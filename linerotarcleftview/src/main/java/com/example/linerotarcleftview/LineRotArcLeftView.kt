509aca1d7ca0677af7d71b7a379b141e01b9fbbb
Anwesh43 <anweshthecool0@gmail.com>
2025-05-22 02:24:36 +0530

    linerotarcleftview: adding Canvas extension methods

--- linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineRotArcLeftView.kt
+++ linerotarcleftview/src/main/java/com/example/linerotarcleftview/LineRotArcLeftView.kt
@@ -29,3 +29,27 @@ val sweep : Float = 60f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(sweep * 0.5f * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), 90f - sweep * 0.5f * dsc(1), sweep * dsc(1), false, paint)
+    }
+}
