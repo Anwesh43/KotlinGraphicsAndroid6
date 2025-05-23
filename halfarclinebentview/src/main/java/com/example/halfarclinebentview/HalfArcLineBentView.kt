7da2bce35b73cca55cd33fdf78ceccc612b9617f
Anwesh43 <anweshthecool0@gmail.com>
2025-05-23 09:45:51 +0530

    halfarclinebentview: adding HALBNode class

--- halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
+++ halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
@@ -126,4 +126,45 @@ class HalfArcLineBentView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HALBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HALBNode? = null
+        private var prev : HALBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HALBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHALBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HALBNode {
+            var curr : HALBNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
