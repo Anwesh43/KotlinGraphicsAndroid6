0e3825299eda291381e6b30863500ddaf57dc211
Anwesh43 <anweshthecool0@gmail.com>
2025-05-23 09:49:55 +0530

    halfarclinebentview: adding Renderer class

--- halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
+++ halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
@@ -190,4 +190,27 @@ class HalfArcLineBentView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfArcLineBentView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val halb : HalfArcLineBent = HalfArcLineBent(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            halb.draw(canvas, paint)
+            animator.animate {
+                halb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            halb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
