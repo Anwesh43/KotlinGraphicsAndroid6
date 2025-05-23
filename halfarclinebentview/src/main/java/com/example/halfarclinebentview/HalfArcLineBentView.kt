12105013fce92f5a85689005266b5de27bafa5bc
Anwesh43 <anweshthecool0@gmail.com>
2025-05-23 09:27:36 +0530

    halfarclinebentview: adding Canvas extension methods

--- halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
+++ halfarclinebentview/src/main/java/com/example/halfarclinebentview/HalfArcLineBentView.kt
@@ -28,3 +28,38 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcLineBent(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawLine(size * (1f - dsc(0)), 0f, size, 0f, paint)
+        drawArc(RectF(0f, -size / 4, size / 2, size / 4), 180f, 180f * dsc(1), false, paint)
+        drawXY(size / 2, 0f) {
+            drawLine(0f, 0f, size * 0.5f * dsc(2), -size * 0.5f * dsc(2), paint)
+        }
+        drawXY(size, -size / 2) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(3), paint)
+        }
+    }
+}
+
+fun Canvas.drawHALBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawHalfArcLineBent(scale, w, h, paint)
+}
