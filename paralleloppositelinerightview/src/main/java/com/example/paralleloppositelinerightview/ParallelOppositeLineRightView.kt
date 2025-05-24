348ed8ab5eee9373a06e1b603edb46f8c7a4d84a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-24 08:34:42 +0530

    paralleloppositelinerightview: adding ParallelOppositeLineRightView class

--- paralleloppositelinerightview/src/main/java/com/example/paralleloppositelinerightview/ParallelOppositeLineRightView.kt
+++ paralleloppositelinerightview/src/main/java/com/example/paralleloppositelinerightview/ParallelOppositeLineRightView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawPOLRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     paint.strokeCap = Paint.Cap.ROUND
 }
+
+class ParallelOppositeLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
