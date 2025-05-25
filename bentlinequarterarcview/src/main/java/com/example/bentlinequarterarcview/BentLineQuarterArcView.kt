248b626c073b82cc44530e7c4bf4da9ea3572b71
Anwesh43 <anweshthecool0@gmail.com>
2025-05-25 10:00:29 +0530

    bentlinequarterarcview: adding Canvas extension methods

--- bentlinequarterarcview/src/main/java/com/example/bentlinequarterarcview/BentLineQuarterArcView.kt
+++ bentlinequarterarcview/src/main/java/com/example/bentlinequarterarcview/BentLineQuarterArcView.kt
@@ -29,3 +29,35 @@ val gapDeg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentLineQuarter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-gapDeg * j + rot * (1f - 2 * j) * (1 - dsc(1)))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -gapDeg, gapDeg * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawBLQANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentLineQuarter(scale, w, h, paint)
+}
