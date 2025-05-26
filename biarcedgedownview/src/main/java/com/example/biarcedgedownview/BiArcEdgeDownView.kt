6ea3f9042713e484edd29f57027125513685cf44
Anwesh43 <anweshthecool0@gmail.com>
2025-05-27 00:38:07 +0530

    biarcedgedownview: adding BAEDNode class

--- biarcedgedownview/src/main/java/com/example/biarcedgedownview/BiArcEdgeDownView.kt
+++ biarcedgedownview/src/main/java/com/example/biarcedgedownview/BiArcEdgeDownView.kt
@@ -132,4 +132,45 @@ class BiArcEdgeDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAEDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BAEDNode? = null
+        private var prev : BAEDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BAEDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAEDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAEDNode {
+            var curr : BAEDNode? = prev
+            if (dir === 1) {
+                curr = this
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
