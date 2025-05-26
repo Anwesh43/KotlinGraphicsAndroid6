ad3f6f96d5a0ca5e6d14033d1864f3ab806bcb59
Anwesh43 <anweshthecool0@gmail.com>
2025-05-27 00:43:11 +0530

    biarcedgedownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.biarcedgedownview.BiArcEdgeDownView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.trilinearcbisectview.TriLineArcBisectView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TriLineArcBisectView.create(this)
+        BiArcEdgeDownView.create(this)
     }
 }
