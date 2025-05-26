801748f2b75b34390efd741ed85981578ec3a8cb
Anwesh43 <anweshthecool0@gmail.com>
2025-05-27 00:41:06 +0530

    biarcedgedownview: adding Renderer

--- biarcedgedownview/src/main/java/com/example/biarcedgedownview/BiArcEdgeDownView.kt
+++ biarcedgedownview/src/main/java/com/example/biarcedgedownview/BiArcEdgeDownView.kt
@@ -196,4 +196,27 @@ class BiArcEdgeDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiArcEdgeDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val baed : BiArcEdgeDown = BiArcEdgeDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            baed.draw(canvas, paint)
+            animator.animate {
+                baed.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            baed.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
