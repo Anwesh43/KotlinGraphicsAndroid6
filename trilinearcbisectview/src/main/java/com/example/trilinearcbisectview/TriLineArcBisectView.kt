3cf3c6c8125c52a6cfac8fd563e7ac7192a3f962
Anwesh43 <anweshthecool0@gmail.com>
2025-05-26 10:55:11 +0530

    trilinearcbisectview: adding Canvas extension methods

--- trilinearcbisectview/src/main/java/com/example/trilinearcbisectview/TriLineArcBisectView.kt
+++ trilinearcbisectview/src/main/java/com/example/trilinearcbisectview/TriLineArcBisectView.kt
@@ -1,12 +1,8 @@
 package com.example.trilinearcbisectview
 
-import android.view.View
-import android.view.MotionEvent
-import android.app.Activity
-import android.content.Context
 import android.graphics.Paint
-import android.graphics.Color
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.RectF
 import androidx.core.graphics.toColorInt
 
@@ -28,3 +24,43 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriLineArcBisect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val dsk : (Int, Int, Int) -> Float =  { i: Int, i1: Int, i2: Int ->
+        dsc(i).divideScale(i1 , i2)
+    }
+
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, -size * 0.5f * dsk(0, 0, 2), size * 0.5f * dsk(0, 0, 2), paint)
+            drawXY(-size / 2, size / 2) {
+                drawLine(0f, 0f, -size * 0.5f * dsk(0, 1, 2), -size * 0.5f * dsk(0, 1, 2), paint)
+            }
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawTLABNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriLineArcBisect(scale, w, h, paint)
+}
