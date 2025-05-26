82271cda8ededbd909c2b38420e73d9db698f05a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-26 11:05:13 +0530

    trilinearcbisectview: adding TLABNode class

--- trilinearcbisectview/src/main/java/com/example/trilinearcbisectview/TriLineArcBisectView.kt
+++ trilinearcbisectview/src/main/java/com/example/trilinearcbisectview/TriLineArcBisectView.kt
@@ -131,4 +131,45 @@ class TriLineArcBisectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLABNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TLABNode? = null
+        private var prev : TLABNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLABNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLABNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLABNode {
+            var curr : TLABNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
