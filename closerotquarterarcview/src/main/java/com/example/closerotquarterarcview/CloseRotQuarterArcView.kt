ebe97b0ccf3a6eefbd3bcbd6f9bbf61e0a697a3d
Anwesh43 <anweshthecool0@gmail.com>
2025-05-28 06:01:52 +0530

    closerotquarterarcview: adding CloseRotQuarterArcView class

--- closerotquarterarcview/src/main/java/com/example/closerotquarterarcview/CloseRotQuarterArcView.kt
+++ closerotquarterarcview/src/main/java/com/example/closerotquarterarcview/CloseRotQuarterArcView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawCRQANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawCloseRotQuarterArc(scale, w, h, paint)
 }
+
+class CloseRotQuarterArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
