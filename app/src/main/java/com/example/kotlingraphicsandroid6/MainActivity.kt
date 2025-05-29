501cd7a9686a867a49b4a13c0f47689b9d0f9d11
Anwesh43 <anweshthecool0@gmail.com>
2025-05-29 12:58:37 +0530

    rottrapeziumlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.closerotquarterarcview.CloseRotQuarterArcView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
+import com.example.rottrapeziumlineview.RotTrapeziumLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CloseRotQuarterArcView.create(this)
+        RotTrapeziumLineView.create(this)
     }
 }
