79ef845db9a25c1ae8594532410496bea00f2510
Anwesh43 <anweshthecool0@gmail.com>
2025-05-29 12:38:32 +0530

    rottrapeziumlineview: adding RTLNode class

--- rottrapeziumlineview/src/main/java/com/example/rottrapeziumlineview/RotTrapeziumLineView.kt
+++ rottrapeziumlineview/src/main/java/com/example/rottrapeziumlineview/RotTrapeziumLineView.kt
@@ -127,4 +127,45 @@ class RotTrapeziumLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RTLNode? = null
+        private var prev : RTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTLNode {
+            var curr : RTLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
