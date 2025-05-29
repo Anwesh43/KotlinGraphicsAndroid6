3c5ba5ee48507463eb33011bf0f7fc2bca3700ff
Anwesh43 <anweshthecool0@gmail.com>
2025-05-29 12:12:12 +0530

    rottrapeziumlineview: adding Canvas extension methods

--- rottrapeziumlineview/src/main/java/com/example/rottrapeziumlineview/RotTrapeziumLineView.kt
+++ rottrapeziumlineview/src/main/java/com/example/rottrapeziumlineview/RotTrapeziumLineView.kt
@@ -28,3 +28,39 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotTrapeziumLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawXY(0f, -size) {
+            drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        }
+        drawLine(0f, 0f, -size * dsc(1), 0f, paint)
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, 0f, -size * 0.5f * dsc(2), paint)
+        }
+        drawXY(-size, -size / 2) {
+            drawLine(0f, 0f, size * dsc(3), -size * 0.5f * dsc(3), paint)
+        }
+    }
+}
+
+fun Canvas.drawRTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotTrapeziumLine(scale, w, h, paint)
+}
