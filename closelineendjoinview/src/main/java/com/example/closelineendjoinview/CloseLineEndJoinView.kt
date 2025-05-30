312546e9aab04600e9101c803fa96f32efe9737c
Anwesh43 <anweshthecool0@gmail.com>
2025-05-30 21:07:12 +0530

    closelineendjoinview: adding Renderer class

--- closelineendjoinview/src/main/java/com/example/closelineendjoinview/CloseLineEndJoinView.kt
+++ closelineendjoinview/src/main/java/com/example/closelineendjoinview/CloseLineEndJoinView.kt
@@ -190,4 +190,27 @@ class CloseLineEndJoinView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CloseLineEndJoinView) {
+
+        private val animator : Animator = Animator(view)
+        private val clej : CloseLineEndJoin = CloseLineEndJoin(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            clej.draw(canvas, paint)
+            animator.animate {
+                clej.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            clej.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
