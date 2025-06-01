a8182842db526ce09a7ef66716eccecf4260a033
Anwesh43 <anweshthecool0@gmail.com>
2025-06-01 11:45:22 +0530

    linerottricapview: adding LRTCNode class

--- linerottricapview/src/main/java/com/example/linerottricapview/LineRotTriCapView.kt
+++ linerottricapview/src/main/java/com/example/linerottricapview/LineRotTriCapView.kt
@@ -123,4 +123,45 @@ class LineRotTriCapView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRTCNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LRTCNode? = null
+        private var next : LRTCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRTCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRTCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRTCNode {
+            var curr : LRTCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
