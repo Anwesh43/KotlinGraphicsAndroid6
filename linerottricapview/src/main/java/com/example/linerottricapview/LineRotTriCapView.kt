56c0533309935844f1ab704b5d8c2c5a4e99534c
Anwesh43 <anweshthecool0@gmail.com>
2025-06-01 11:30:03 +0530

    linerottricapview: adding Canvas extension methods

--- linerottricapview/src/main/java/com/example/linerottricapview/LineRotTriCapView.kt
+++ linerottricapview/src/main/java/com/example/linerottricapview/LineRotTriCapView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
@@ -27,4 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotTriCap(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawLine(0f, 0f, size * dsc(0), -size * dsc(0), paint)
+        drawXY(size, -size) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+            drawXY(0f, size / 2) {
+                rotate(rot * dsc(3))
+                drawLine(0f, 0f, size * 0.5f * dsc(2), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRTCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotTriCap(scale, w, h, paint)
+}
 
