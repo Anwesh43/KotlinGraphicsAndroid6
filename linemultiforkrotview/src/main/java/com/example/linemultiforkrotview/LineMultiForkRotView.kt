07b60970231f7b447d0a622e1f8712a2cf0fd46b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-02 11:16:35 +0530

    linemultiforkrotview: adding static create method

--- linemultiforkrotview/src/main/java/com/example/linemultiforkrotview/LineMultiForkRotView.kt
+++ linemultiforkrotview/src/main/java/com/example/linemultiforkrotview/LineMultiForkRotView.kt
@@ -215,4 +215,12 @@ class LineMultiForkRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : LineMultiForkRotView {
+            val view : LineMultiForkRotView = LineMultiForkRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
