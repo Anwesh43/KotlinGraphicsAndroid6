26847b8854ba26effbcca3f3af34cd2efa6bdd9e
Anwesh43 <anweshthecool0@gmail.com>
2025-06-02 10:59:45 +0530

    linemultiforkrotview: adding LMFRNode class

--- linemultiforkrotview/src/main/java/com/example/linemultiforkrotview/LineMultiForkRotView.kt
+++ linemultiforkrotview/src/main/java/com/example/linemultiforkrotview/LineMultiForkRotView.kt
@@ -126,4 +126,45 @@ class LineMultiForkRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LMFRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LMFRNode? = null
+        private var prev : LMFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LMFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLMFRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LMFRNode {
+            var curr : LMFRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
