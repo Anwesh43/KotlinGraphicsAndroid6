a5207a0ecf075d0d3217eb144267c87c2d137aaa
Anwesh43 <anweshthecool0@gmail.com>
2025-06-04 01:36:08 +0530

    zigzaglinerightview: adding Canvas extension methods

--- zigzaglinerightview/src/main/java/com/example/zigzaglinerightview/ZigZagLineRightView.kt
+++ zigzaglinerightview/src/main/java/com/example/zigzaglinerightview/ZigZagLineRightView.kt
@@ -27,3 +27,38 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawZigZagLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        drawXY(-size, size) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawXY(0f, size) {
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(3))
+            drawLine(0f, 0f, size * dsc(2), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawZZLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawZigZagLineRight(scale, w, h, paint)
+}
