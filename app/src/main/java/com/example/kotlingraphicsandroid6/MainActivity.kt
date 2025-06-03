50fd620eec090a931be146f78cf517b7c476e889
Anwesh43 <anweshthecool0@gmail.com>
2025-06-04 01:50:50 +0530

    zigzaglinerightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.linemultiforkrotview.LineMultiForkRotView
+import com.example.zigzaglinerightview.ZigZagLineRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineMultiForkRotView.create(this)
+        ZigZagLineRightView.create(this)
     }
 }
