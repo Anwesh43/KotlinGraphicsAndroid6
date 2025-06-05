be5a03c7aac892c6cc1966083b341fc8218c84bf
Anwesh43 <anweshthecool0@gmail.com>
2025-06-06 02:22:04 +0530

    strokeinnerarcrotview: adding Renderer class

--- strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
+++ strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
@@ -188,4 +188,27 @@ class StrokeInnerArcRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : StrokeInnerArcRotView) {
+
+        private val siar : StrokeInnerArcRot = StrokeInnerArcRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            siar.draw(canvas, paint)
+            animator.animate {
+                siar.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            siar.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
