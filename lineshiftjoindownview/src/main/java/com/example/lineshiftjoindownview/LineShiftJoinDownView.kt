b5cb20b32d38a191a028213b9f27a5cb851f5bae
Anwesh43 <anweshthecool0@gmail.com>
2025-06-05 08:11:03 +0530

    lineshiftjoindownview: adding Canvas extension methods

--- lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
+++ lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
@@ -7,6 +7,7 @@ import android.graphics.Canvas
 import androidx.core.graphics.toColorInt
 import android.content.Context
 import android.app.Activity
+import android.graphics.Color
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -27,3 +28,36 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineShiftJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        drawXY(0f, 0f) {
+            rotate(rot2 * dsc(3))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawXY(0f, -size * (1 - dsc(2))) {
+            rotate(rot1 * dsc(3))
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLSJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineShiftJoinDown(scale, w, h, paint)
+}
