4f8eb792685b5610900d9ea0478fb7769b6093f5
Anwesh43 <anweshthecool0@gmail.com>
2025-06-05 08:18:39 +0530

    lineshiftjoindownview: adding LineShifJoinDown class

--- lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
+++ lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
@@ -166,4 +166,26 @@ class LineShiftJoinDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineShiftJoinDown(var i : Int) {
+
+        private var curr : LSJDNode = LSJDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
