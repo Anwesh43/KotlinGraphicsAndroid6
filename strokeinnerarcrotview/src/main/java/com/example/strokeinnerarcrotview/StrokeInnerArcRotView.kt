4ee78a3c2975157ac2eeff75fbe8c1b3bb967b87
Anwesh43 <anweshthecool0@gmail.com>
2025-06-06 02:11:54 +0530

    strokeinnerarcrotview: adding SIARNode class

--- strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
+++ strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
@@ -124,4 +124,45 @@ class StrokeInnerArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SIARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SIARNode? = null
+        private var prev : SIARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SIARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSIARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SIARNode {
+            var curr : SIARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
