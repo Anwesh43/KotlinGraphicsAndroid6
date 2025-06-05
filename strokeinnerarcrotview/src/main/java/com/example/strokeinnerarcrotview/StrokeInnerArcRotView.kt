ccf7b7520bccfaa76b721ff68798349c21e8e6d3
Anwesh43 <anweshthecool0@gmail.com>
2025-06-06 01:47:41 +0530

    strokeinnerarcrotview: adding StrokeInnerArcRotView class

--- strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
+++ strokeinnerarcrotview/src/main/java/com/example/strokeinnerarcrotview/StrokeInnerArcRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSIARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawStrokeInnerArcRot(scale, w, h, paint)
 }
+
+class StrokeInnerArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
