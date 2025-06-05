a0ac54f46c3f01f73be87b6c06aacaad4e593a24
Anwesh43 <anweshthecool0@gmail.com>
2025-06-05 08:13:09 +0530

    lineshiftjoindownview: adding LineShiftJoinDownView class

--- lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
+++ lineshiftjoindownview/src/main/java/com/example/lineshiftjoindownview/LineShiftJoinDownView.kt
@@ -8,6 +8,7 @@ import androidx.core.graphics.toColorInt
 import android.content.Context
 import android.app.Activity
 import android.graphics.Color
+import androidx.constraintlayout.widget.ConstraintSet.Motion
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -60,4 +61,20 @@ fun Canvas.drawLSJDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineShiftJoinDown(scale, w, h, paint)
+}
+
+class LineShiftJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
 }
