ebd3bcc5a7c60ee0be18faf8f7423ba19ce570ba
Anwesh43 <anweshthecool0@gmail.com>
2025-06-07 09:07:25 +0530

    sideconcarcdownview: adding constants and librreis

---
+++ sideconcarcdownview/src/main/java/com/example/sideconcarcdownview/SideConcArcDownView.kt
@@ -0,0 +1,28 @@
+package com.example.sideconcarcdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val arc : Int = 4
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val startDeg : Float = -90f
+val deg : Float = 30f
+val backColor : Int = "#BDBDBD".toColorInt()
