b4f4c717d6fda29316a2c4b35f3037377513ce55
Anwesh43 <anweshthecool0@gmail.com>
2025-06-07 11:26:32 +0530

    sideconcarcdownview: adding SCADNode class

--- sideconcarcdownview/src/main/java/com/example/sideconcarcdownview/SideConcArcDownView.kt
+++ sideconcarcdownview/src/main/java/com/example/sideconcarcdownview/SideConcArcDownView.kt
@@ -137,4 +137,45 @@ class SideConcArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SCADNode? = null
+        private var prev : SCADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SCADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir: Int, cb : () -> Unit) : SCADNode {
+            var curr : SCADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
