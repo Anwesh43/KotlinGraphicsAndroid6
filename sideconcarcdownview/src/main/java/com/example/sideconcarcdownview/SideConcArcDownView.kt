de35a5acd493211bdb01c568ede9289ef5ea9b58
Anwesh43 <anweshthecool0@gmail.com>
2025-06-07 09:24:06 +0530

    sideconcarcdownview: adding Canvas exension methods

--- sideconcarcdownview/src/main/java/com/example/sideconcarcdownview/SideConcArcDownView.kt
+++ sideconcarcdownview/src/main/java/com/example/sideconcarcdownview/SideConcArcDownView.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.app.Activity
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.RectF
 import androidx.core.graphics.toColorInt
 
@@ -22,7 +23,7 @@ val arc : Int = 4
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = 180f
 val startDeg : Float = -90f
 val deg : Float = 30f
 val backColor : Int = "#BDBDBD".toColorInt()
@@ -30,3 +31,46 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSideConcArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+        for (j in 1..arc) {
+            val r : Float = (size / arc) * j
+            drawArc(
+                RectF(-r, -r, r, r),
+                startDeg - deg,
+                2 * deg * dsc(2).divideScale(j, arc),
+                false,
+                paint
+            )
+        }
+    }
+}
+
+fun Canvas.drawSCADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSideConcArcDown(scale, w, h, paint)
+}
