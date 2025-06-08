0037ce3fce0514ffd4f38196c693414c7075c866
Anwesh43 <anweshthecool0@gmail.com>
2025-06-08 07:31:26 +0530

    linerightleftpieview: adding Canvas extension methods

--- linerightleftpieview/src/main/java/com/example/linerightleftpieview/LineRightLeftPieView.kt
+++ linerightleftpieview/src/main/java/com/example/linerightleftpieview/LineRightLeftPieView.kt
@@ -27,3 +27,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightLeftPie(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(-w * 0.5f * (1 - dsc(1)) + size * 0.5f * (1 - j) * dsc(3), 0f) {
+                rotate(rot * j * dsc(2))
+                drawLine(0f, 0f, -size, 0f, paint)
+            }
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(2))) {
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRLPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightLeftPie(scale, w, h, paint)
+}
+
