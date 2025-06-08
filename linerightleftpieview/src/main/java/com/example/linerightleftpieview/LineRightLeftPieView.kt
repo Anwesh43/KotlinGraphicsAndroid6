f38dab1f03274caa9e359d395870f74bee356d9d
Anwesh43 <anweshthecool0@gmail.com>
2025-06-08 07:35:22 +0530

    linerightleftpieview: adding LineRightLeftPieView class

--- linerightleftpieview/src/main/java/com/example/linerightleftpieview/LineRightLeftPieView.kt
+++ linerightleftpieview/src/main/java/com/example/linerightleftpieview/LineRightLeftPieView.kt
@@ -61,3 +61,18 @@ fun Canvas.drawLRLPNode(i : Int, scale : Float, paint : Paint) {
     drawLineRightLeftPie(scale, w, h, paint)
 }
 
+class LineRightLeftPieView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
