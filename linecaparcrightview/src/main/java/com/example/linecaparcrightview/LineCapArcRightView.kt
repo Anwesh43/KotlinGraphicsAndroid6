63c007fd683f2f609ce9c3536e6da9a9a372169b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-10 07:37:32 +0530

    linecaparcrightviw: adding stroke arc fix

--- linecaparcrightview/src/main/java/com/example/linecaparcrightview/LineCapArcRightView.kt
+++ linecaparcrightview/src/main/java/com/example/linecaparcrightview/LineCapArcRightView.kt
@@ -56,6 +56,7 @@ fun Canvas.drawLCARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineCapArcRight(scale, w, h, paint)
 }
 
