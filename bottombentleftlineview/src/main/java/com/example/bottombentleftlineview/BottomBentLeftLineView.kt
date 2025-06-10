c637dfff0ce864c9f0b62c65e861ada628669246
Anwesh43 <anweshthecool0@gmail.com>
2025-06-11 01:05:10 +0530

    bottombentleftview: adding Canvas extension methods

--- bottombentleftlineview/src/main/java/com/example/bottombentleftlineview/BottomBentLeftLineView.kt
+++ bottombentleftlineview/src/main/java/com/example/bottombentleftlineview/BottomBentLeftLineView.kt
@@ -7,6 +7,7 @@ import android.graphics.Canvas
 import androidx.core.graphics.toColorInt
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -27,3 +28,32 @@ val rot2 : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i. n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBottomBentLeftLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 * (1f - dsc(3)), h / 2) {
+        for (j in 0..1) {
+            rotate(rot1 * (1 - dsc(1)) * j + rot2 * dsc(2) * (1 - j))
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawBBLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBottomBentLeftLine(scale, w, h, paint)
+}
