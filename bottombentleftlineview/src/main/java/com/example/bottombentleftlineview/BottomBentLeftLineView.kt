fe8e9c0f4648523617dfdd5cc380749a7562e19c
Anwesh43 <anweshthecool0@gmail.com>
2025-06-11 01:11:47 +0530

    bottombentleftview: adding BBLLNode class

--- bottombentleftlineview/src/main/java/com/example/bottombentleftlineview/BottomBentLeftLineView.kt
+++ bottombentleftlineview/src/main/java/com/example/bottombentleftlineview/BottomBentLeftLineView.kt
@@ -120,4 +120,45 @@ class BottomBentLeftLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BBLLNode? = null
+        private var next : BBLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBLLNode {
+            var curr : BBLLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
