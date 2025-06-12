db7ad1be428cc74b9a687be5d61cd846bcfea2da
Anwesh43 <anweshthecool0@gmail.com>
2025-06-13 01:47:52 +0530

    multidiagdownlineview: adding MultiDiagDownLineView class

--- multidiagdownlineview/src/main/java/com/example/multidiagdownlineview/MultiDiagDownLineView.kt
+++ multidiagdownlineview/src/main/java/com/example/multidiagdownlineview/MultiDiagDownLineView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawMDDLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMultiDiagDownLine(scale, w, h, paint)
 }
+
+class MultiDiagDownLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
