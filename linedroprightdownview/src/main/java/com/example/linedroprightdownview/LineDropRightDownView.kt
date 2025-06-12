67908ed276e9374958c730cd58ae4fd47f830f7e
Anwesh43 <anweshthecool0@gmail.com>
2025-06-12 09:24:21 +0530

    linedroprightdownview: adding Canvas extension methods

--- linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
+++ linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
@@ -22,9 +22,39 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 180f
 val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDropRightDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(size * dsc(1) * (1 - j), -h * 0.5f * (1 - dsc(j))) {
+                rotate(deg * (1 + j) * dsc(2))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLDRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDropRightDown(scale, w, h, paint)
+}
