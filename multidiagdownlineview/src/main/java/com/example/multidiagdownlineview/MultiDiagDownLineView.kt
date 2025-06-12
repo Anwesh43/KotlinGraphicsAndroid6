720e6ee065607d0e4d23b6284cf7bd537f236ff8
Anwesh43 <anweshthecool0@gmail.com>
2025-06-13 01:57:31 +0530

    multidiagdownlineview: adding MDDLNode class

--- multidiagdownlineview/src/main/java/com/example/multidiagdownlineview/MultiDiagDownLineView.kt
+++ multidiagdownlineview/src/main/java/com/example/multidiagdownlineview/MultiDiagDownLineView.kt
@@ -126,4 +126,45 @@ class MultiDiagDownLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MDDLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MDDLNode? = null
+        private var prev : MDDLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MDDLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMDDLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MDDLNode {
+            var curr : MDDLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
