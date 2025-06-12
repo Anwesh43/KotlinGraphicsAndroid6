c6037801839df0baeb3b7a2d85c04e8467d5cef7
Anwesh43 <anweshthecool0@gmail.com>
2025-06-12 09:33:47 +0530

    linedroprightdownview: adding LDRDNode class

--- linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
+++ linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
@@ -121,4 +121,45 @@ class LineDropRightDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDRDNode? = null
+        private var prev : LDRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDRDNode {
+            var curr : LDRDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
