afe013dac698de71c73f34cca5ed453f195b34a4
Anwesh43 <anweshthecool0@gmail.com>
2025-06-12 09:38:11 +0530

    linedroprightdownview: adding Renderer class

--- linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
+++ linedroprightdownview/src/main/java/com/example/linedroprightdownview/LineDropRightDownView.kt
@@ -185,4 +185,27 @@ class LineDropRightDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineDropRightDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val ldrd : LineDropRightDown = LineDropRightDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ldrd.draw(canvas, paint)
+            animator.animate {
+                ldrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ldrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
