9a71c043c79fad8566ba23c316a1fba3bc347516
Anwesh43 <anweshthecool0@gmail.com>
2025-06-15 02:09:09 +0530

    linerotcompletequareview: adding Canvas extension methods

--- linerotcompletesquareview/src/main/java/com/example/linerotcompletesquareview/LineRotCompleteSquareView.kt
+++ linerotcompletesquareview/src/main/java/com/example/linerotcompletesquareview/LineRotCompleteSquareView.kt
@@ -27,3 +27,45 @@ val times : Int = 4
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRot(j : Int, size : Float, scale : Float, paint : Paint) {
+    drawXY(-size / 2, -size / 2) {
+        rotate(rot * j)
+        drawXY(size / 2, size / 2) {
+            drawLine(0f, 0f, -size * scale.divideScale(0, 2), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLineRotCompleteSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2 - (h / 2) * dsc(4)) {
+        var currRot : Float = 0f
+        for (j in 1..times) {
+            currRot = rot * dsc(j).divideScale(1, 2)
+        }
+        rotate(currRot)
+        for (j in 0..(times - 1)) {
+            drawLineRot(j, size, dsc(j), paint)
+        }
+    }
+}
+
+fun Canvas.drawLRCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotCompleteSquare(scale, w, h, paint)
+}
