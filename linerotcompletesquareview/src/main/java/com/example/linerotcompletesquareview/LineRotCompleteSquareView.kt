e689a9317a779f89afc18d8b5706e16b9cc4c917
Anwesh43 <anweshthecool0@gmail.com>
2025-06-15 01:55:54 +0530

    linerotcompletequareview: adding constants and libraries

---
+++ linerotcompletesquareview/src/main/java/com/example/linerotcompletesquareview/LineRotCompleteSquareView.kt
@@ -0,0 +1,29 @@
+package com.example.linerotcompletesquareview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
+val times : Int = 4
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
