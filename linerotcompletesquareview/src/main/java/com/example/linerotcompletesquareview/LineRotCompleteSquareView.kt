5bc2a31fe6a3a8fe245f84492ecfa818131036fb
Anwesh43 <anweshthecool0@gmail.com>
2025-06-15 02:22:16 +0530

    linerotcompletequareview: adding LineRotCompleteSquare class

--- linerotcompletesquareview/src/main/java/com/example/linerotcompletesquareview/LineRotCompleteSquareView.kt
+++ linerotcompletesquareview/src/main/java/com/example/linerotcompletesquareview/LineRotCompleteSquareView.kt
@@ -132,4 +132,68 @@ class LineRotCompleteSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRCSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRCSNode? = null
+        private var prev : LRCSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRCSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRCSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRCSNode {
+            var curr : LRCSNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
+    data class LineRotCompleteSquare(var i : Int) {
+
+        private var curr : LRCSNode = LRCSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
