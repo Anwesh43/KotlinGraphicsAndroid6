5189d1d56abb6693268c86f0d193ab6337b324cc
Anwesh43 <anweshthecool0@gmail.com>
2025-06-16 01:50:46 +0530

    arclineupdiagview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arclineupdiagview.ArcLineUpDiagView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.linerotcompletesquareview.LineRotCompleteSquareView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotCompleteSquareView.create(this)
+        ArcLineUpDiagView.create(this)
     }
 }
