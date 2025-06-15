d9b320097c34d1992f1f6b3c027ab7d75502ec36
Anwesh43 <anweshthecool0@gmail.com>
2025-06-16 01:38:17 +0530

    arclineupdiagview: adding ALUDNode class

--- arclineupdiagview/src/main/java/com/example/arclineupdiagview/ArcLineUpDiagView.kt
+++ arclineupdiagview/src/main/java/com/example/arclineupdiagview/ArcLineUpDiagView.kt
@@ -127,4 +127,45 @@ class ArcLineUpDiagView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALUDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALUDNode? = null
+        private var prev : ALUDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALUDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALUDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALUDNode {
+            var curr : ALUDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
