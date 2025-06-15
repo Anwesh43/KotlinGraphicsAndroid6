76fb9873c172078e2ef4bc944ffd2d704eac9009
Anwesh43 <anweshthecool0@gmail.com>
2025-06-16 00:24:17 +0530

    arclineupdiagview: adding Int and Float extension methods

--- arclineupdiagview/src/main/java/com/example/arclineupdiagview/ArcLineUpDiagView.kt
+++ arclineupdiagview/src/main/java/com/example/arclineupdiagview/ArcLineUpDiagView.kt
@@ -23,3 +23,7 @@ val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
 val scGap : Float = 0.04f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
