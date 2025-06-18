02aa2b952647e8de1bc7312795dd77cc85690648
Anwesh43 <anweshthecool0@gmail.com>
2025-06-18 09:55:18 +0530

    enclosesqdiagview: adding Canvas extension methods

--- enclosesqdiagview/src/main/java/com/example/enclosesqdiagview/EncloseSqDiagView.kt
+++ enclosesqdiagview/src/main/java/com/example/enclosesqdiagview/EncloseSqDiagView.kt
@@ -6,6 +6,7 @@ import android.app.Activity
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 import androidx.core.graphics.toColorInt
 
 val colors : Array<String> = arrayOf(
@@ -26,3 +27,39 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawEncloseSqDiag(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2 + (h / 2) * dsc(4)) {
+        for (i in 0..3) {
+            val j = i % 2
+            val k = i / 2
+            val dsi : Int = (1 - j) * k + 2 * k
+            val dsij : Int = dsi % 2
+            drawXY(size * j, size * k - ((h * 0.5f - size * k) * (1 - 2 * k) * dsc(dsi).divideScale(0, 2))) {
+                rotate(rot * dsc(dsi).divideScale(1, 2) * (1 + dsij))
+                drawLine(0f, 0f, 0f, -size * (1 - 2 * k), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawESDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEncloseSqDiag(scale, w, h, paint)
+}
+
