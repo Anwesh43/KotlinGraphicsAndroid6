02d1bd734c3544eb61654d31d47ebb9d140c9c98
Anwesh43 <anweshthecool0@gmail.com>
2025-06-18 10:25:42 +0530

    enclosesqdiagview: adding ESDNode class

--- enclosesqdiagview/src/main/java/com/example/enclosesqdiagview/EncloseSqDiagView.kt
+++ enclosesqdiagview/src/main/java/com/example/enclosesqdiagview/EncloseSqDiagView.kt
@@ -124,4 +124,45 @@ class EncloseSqDiagView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ESDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ESDNode? = null
+        private var prev : ESDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ESDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawESDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ESDNode {
+            var curr : ESDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
