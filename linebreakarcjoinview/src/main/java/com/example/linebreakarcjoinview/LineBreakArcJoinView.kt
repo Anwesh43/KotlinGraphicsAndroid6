6862a4f9adbb39fa3945c312a5e020900415e87a
Anwesh43 <anweshthecool0@gmail.com>
2025-06-19 10:18:59 +0530

    linebreakarcjoinview: adding LineBreakArcJoinView class

--- linebreakarcjoinview/src/main/java/com/example/linebreakarcjoinview/LineBreakArcJoinView.kt
+++ linebreakarcjoinview/src/main/java/com/example/linebreakarcjoinview/LineBreakArcJoinView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLBAJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBreakArcJoin(scale, w, h, paint)
 }
+
+class LineBreakArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
