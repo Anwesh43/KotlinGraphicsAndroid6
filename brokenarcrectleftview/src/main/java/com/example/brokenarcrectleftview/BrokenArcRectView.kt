9d204b886c9e7b91543011512bac49cf98029d69
Anwesh43 <anweshthecool0@gmail.com>
2025-06-20 15:40:11 +0530

    brokenarcrectleftview: adding BrokenArcRectLeftView class

--- brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
+++ brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawBARLNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBrokenArcRectLeft(scale, w, h, paint)
 }
+
+class BrokenArcRectLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
