11a37ef8e0bab5e76494262537afeb48511c37c2
Anwesh43 <anweshthecool0@gmail.com>
2025-06-20 15:39:09 +0530

    brokenarcrectleftview: adding Canvas  extension methods

--- brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
+++ brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
@@ -17,8 +17,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -28,3 +28,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBrokenArcRectLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        drawXY(-size / 2, size / 2) {
+            drawLine(0f, 0f, 0f, -size * 0.5f * dsc(0), paint)
+        }
+        drawXY(size / 2, 0f) {
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 90f * dsc(2), false, paint)
+        drawXY(0f, size / 2) {
+            drawLine(0f, 0f, size * 0.5f * dsc(3), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawBARLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBrokenArcRectLeft(scale, w, h, paint)
+}
