0c6ffa730424da753e3309ef13c362702c3da38b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-20 15:54:43 +0530

    brokenarcrectleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.brokenarcrectleftview.BrokenArcRectLeftView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.linebreakarcjoinview.LineBreakArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBreakArcJoinView.create(this)
+        BrokenArcRectLeftView.create(this)
     }
 }
