9a3d41cab11bcc9313f66538bea715ff7e5d5fb2
Anwesh43 <anweshthecool0@gmail.com>
2025-06-20 15:47:33 +0530

    brokenarcrectleftview: adding BARLNode class

--- brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
+++ brokenarcrectleftview/src/main/java/com/example/brokenarcrectleftview/BrokenArcRectView.kt
@@ -128,4 +128,45 @@ class BrokenArcRectLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BARLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BARLNode? = null
+        private var prev : BARLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BARLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBARLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BARLNode {
+            var curr : BARLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
