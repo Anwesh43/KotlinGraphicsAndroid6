964283b0096b99184395d1045a121db89e0c0e0e
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 11:17:36 +0530

    linebentarcjoinview: adding static create method

--- linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentArcJoinView.kt
+++ linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentArcJoinView.kt
@@ -214,4 +214,12 @@ class LineBentArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : LineBentArcJoinView {
+            val view : LineBentArcJoinView = LineBentArcJoinView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
