4573b64e3c8a723aea1d8eb7dc20ab4a42eca0ad
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 10:59:16 +0530

    linebentarcjoinview: adding LineBentArcJoinView class

--- linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentArcJoinView.kt
+++ linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentArcJoinView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLBAJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcJoin(scale, w, h, paint)
 }
+
+class LineBentArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
