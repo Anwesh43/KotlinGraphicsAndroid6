8ce1be1d2dcf9d9caf7d9edc299fa36da06f2dde
Anwesh43 <anweshthecool0@gmail.com>
2025-06-23 02:19:52 +0530

    altbarseparaterotview: adding Canvas extension methods

--- altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
+++ altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
@@ -27,3 +27,37 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawAltBarSeparateRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val barW : Float = Math.min(w, h) / wFactor
+    val barH : Float = Math.min(w, h) / hFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY((w / 2 - barW) * dsc(1), h * 0.5f * dsc(3)) {
+                    rotate(rot * dsc(2))
+                    drawRect(RectF(0f, 0f, barW, barH), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawABSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    drawAltBarSeparateRot(scale, w, h, paint)
+}
+
