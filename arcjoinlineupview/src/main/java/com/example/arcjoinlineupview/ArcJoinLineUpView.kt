3d2aca1976e7542b82126fd15d7c0b2b1a9b4792
Anwesh43 <anweshthecool0@gmail.com>
2025-06-22 07:30:34 +0530

    arcjoinlineupview: adding ArcJoinLineUpView class

--- arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
+++ arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawAJLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcJoinLineUp(scale, w, h, paint)
 }
+
+class ArcJoinLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
