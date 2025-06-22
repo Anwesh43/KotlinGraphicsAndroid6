7180602d3b0a912e2c0d695aa8b7acfb0a0d7bde
Anwesh43 <anweshthecool0@gmail.com>
2025-06-22 07:45:40 +0530

    arcjoinlineupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcjoinlineupview.ArcJoinLineUpView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.linebentarcjoinview.LineBentArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentArcJoinView.create(this)
+        ArcJoinLineUpView.create(this)
     }
 }
