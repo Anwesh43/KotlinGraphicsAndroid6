328666f835971137dcd59e74d9d938126fc523cf
Anwesh43 <anweshthecool0@gmail.com>
2025-06-22 07:37:21 +0530

    arcjoinlineupview: adding AJLUNode class

--- arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
+++ arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
@@ -123,4 +123,45 @@ class ArcJoinLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AJLUNode(var i : Int, val state : State = State()) {
+
+        private var next : AJLUNode? = null
+        private var prev : AJLUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AJLUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAJLUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AJLUNode {
+            var curr : AJLUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
