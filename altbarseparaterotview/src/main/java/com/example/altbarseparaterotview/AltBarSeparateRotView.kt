35ac0594cfc11506ac00892ee7afe1a696c73a3b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-23 02:32:26 +0530

    altbarseparaterotview: adding ABSRNode class

--- altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
+++ altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
@@ -123,5 +123,46 @@ class AltBarSeparateRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ABSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ABSRNode? = null
+        private var prev : ABSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ABSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawABSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) : ABSRNode {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ABSRNode {
+            var curr : ABSRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
