9a05882a8396f83f427c3eac205ce78994405a0b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-22 07:47:35 +0530

    arcjoinlineupview: adding fix 2

--- arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
+++ arcjoinlineupview/src/main/java/com/example/arcjoinlineupview/ArcJoinLineUpView.kt
@@ -58,6 +58,7 @@ fun Canvas.drawAJLUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawArcJoinLineUp(scale, w, h, paint)
 }
 
