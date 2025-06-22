891e66f8759b2e8c0bd49d87b908942e9b76cdbb
Anwesh43 <anweshthecool0@gmail.com>
2025-06-23 02:22:30 +0530

    altbarseparaterotview: adding AltBarSeparateRotView class

--- altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
+++ altbarseparaterotview/src/main/java/com/example/altbarseparaterotview/AltBarSeparateRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawABSRNode(i : Int, scale : Float, paint : Paint) {
     drawAltBarSeparateRot(scale, w, h, paint)
 }
 
+class AltBarSeparateRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
