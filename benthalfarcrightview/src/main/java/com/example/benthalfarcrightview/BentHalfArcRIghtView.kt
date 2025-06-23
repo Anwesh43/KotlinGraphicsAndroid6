6d77ef9aa33b5d0a9f36eaf67bf3c4fe99a4a6d0
Anwesh43 <anweshthecool0@gmail.com>
2025-06-24 02:47:37 +0530

    benthalfarcrightview: adding fix

--- benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
+++ benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
@@ -22,8 +22,8 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 135f
-val sweep : Float = -90f
+val rot : Float = 45f
+val sweep : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
