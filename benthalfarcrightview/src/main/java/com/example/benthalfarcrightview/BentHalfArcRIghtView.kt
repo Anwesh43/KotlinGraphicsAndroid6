6359977787bc4c844e00e2d224fbb21e46fb9ef3
Anwesh43 <anweshthecool0@gmail.com>
2025-06-24 02:31:18 +0530

    benthalfarcrightview: adding BentHalfArcRightView class

--- benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
+++ benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBHARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBentHalfArcRight(scale, w, h, paint)
 }
+
+class BentHalfArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
