0c805f00d973afd56b667ac0e3ca2bb0fec87d5a
Anwesh43 <anweshthecool0@gmail.com>
2025-06-24 02:44:57 +0530

    benthalfarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.altbarseparaterotview.AltBarSeparateRotView
+import com.example.benthalfarcrightview.BentHalfArcRightView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AltBarSeparateRotView.create(this)
+        BentHalfArcRightView.create(this)
     }
 }
