ab1e6ae6c7ca712b2e75ef6ac747703921fe1879
Anwesh43 <anweshthecool0@gmail.com>
2025-06-24 02:36:51 +0530

    benthalfarcrightview: adding BHARNode class

--- benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
+++ benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
@@ -124,4 +124,45 @@ class BentHalfArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BHARNode? = null
+        private var prev : BHARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHARNode {
+            var curr : BHARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
