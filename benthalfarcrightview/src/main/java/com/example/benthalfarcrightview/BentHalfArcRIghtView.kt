b3befdec441e4d90e7eeb0c2a8a4a9a1927270dc
Anwesh43 <anweshthecool0@gmail.com>
2025-06-24 02:29:54 +0530

    benthalfarcrightview: adding Canvas extension methods

--- benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
+++ benthalfarcrightview/src/main/java/com/example/benthalfarcrightview/BentHalfArcRIghtView.kt
@@ -28,3 +28,36 @@ val sweep : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentHalfArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2 + (h / 2) * dsc(3)) {
+        drawXY(size, 0f) {
+            rotate(rot - sweep * dsc(2))
+            drawXY(0f, -size) {
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawBHARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBentHalfArcRight(scale, w, h, paint)
+}
