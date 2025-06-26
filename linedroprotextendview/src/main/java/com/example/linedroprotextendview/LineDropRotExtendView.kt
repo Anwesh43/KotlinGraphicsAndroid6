372714ad8a4b458a83a79c3cc166b4cae7d80cde
Anwesh43 <anweshthecool0@gmail.com>
2025-06-26 10:03:14 +0530

    linedroprotextendview: adding LineDropRotExtendView class

--- linedroprotextendview/src/main/java/com/example/linedroprotextendview/LineDropRotExtendView.kt
+++ linedroprotextendview/src/main/java/com/example/linedroprotextendview/LineDropRotExtendView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLDRENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineDropRotExtend(scale, w, h, paint)
 }
+
+class LineDropRotExtendView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
