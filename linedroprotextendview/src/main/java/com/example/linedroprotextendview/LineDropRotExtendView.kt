04a59441f2e3a8bc5c36d730cba89f89451294b6
Anwesh43 <anweshthecool0@gmail.com>
2025-06-26 10:24:39 +0530

    linedroprotextendview: adding Renderer class

--- linedroprotextendview/src/main/java/com/example/linedroprotextendview/LineDropRotExtendView.kt
+++ linedroprotextendview/src/main/java/com/example/linedroprotextendview/LineDropRotExtendView.kt
@@ -183,4 +183,27 @@ class LineDropRotExtendView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineDropRotExtendView) {
+
+        private val animator : Animator = Animator(view)
+        private val ldre : LineDropRotExtend = LineDropRotExtend(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ldre.draw(canvas, paint)
+            animator.animate {
+                ldre.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ldre.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
