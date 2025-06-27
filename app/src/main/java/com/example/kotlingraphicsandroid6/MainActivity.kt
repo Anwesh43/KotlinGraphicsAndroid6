486cf4f2395a1050a07bc1e89973554f0007dc0f
Anwesh43 <anweshthecool0@gmail.com>
2025-06-28 04:39:12 +0530

    linedownrotrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.linedroprotextendview.LineDropRotExtendView
+import com.example.linedownrotrightview.LineDownRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineDropRotExtendView.create(this)
+        LineDownRotRightView.create(this)
     }
 }
