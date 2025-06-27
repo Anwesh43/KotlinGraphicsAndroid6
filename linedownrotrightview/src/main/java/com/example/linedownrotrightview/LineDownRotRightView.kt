6574537f334afbf79efbc996346ff54b70e8e847
Anwesh43 <anweshthecool0@gmail.com>
2025-06-28 03:56:35 +0530

    linedownrotrightview: adding LineDownRotRightView class

--- linedownrotrightview/src/main/java/com/example/linedownrotrightview/LineDownRotRightView.kt
+++ linedownrotrightview/src/main/java/com/example/linedownrotrightview/LineDownRotRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLDRRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineDownRotRight(scale, w, h, paint)
 }
+
+class LineDownRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
