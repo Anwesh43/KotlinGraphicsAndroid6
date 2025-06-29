92ee473595bfc058e6091fbd2d4bbbd4ce503d16
Anwesh43 <anweshthecool0@gmail.com>
2025-06-30 00:51:25 +0530

    rothalfarcendview: adding Canvas extension methods

--- rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
+++ rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
@@ -27,3 +27,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotHalfArcEnd(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, -size * j) {
+                rotate(rot * dsc(2) * j)
+                drawArc(RectF(-size / 2, -size, size / 2, 0f), 90f + 180f * dsc(3 + j), 180f * (dsc(j) - dsc(3 + j)), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRHAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRotHalfArcEnd(scale, w, h, paint)
+}
