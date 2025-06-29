28e3e57c9162b448e45f8cc51ed3bb6ca99dba78
Anwesh43 <anweshthecool0@gmail.com>
2025-06-29 13:43:58 +0530

    linealtdownrotview: adding LADRNode class

--- linealtdownrotview/src/main/java/com/example/linealtdownrotview/LineAltDownRotView.kt
+++ linealtdownrotview/src/main/java/com/example/linealtdownrotview/LineAltDownRotView.kt
@@ -122,4 +122,45 @@ class LineAltDownRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LADRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LADRNode? = null
+        private var prev : LADRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LADRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLADRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LADRNode {
+            var curr : LADRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
