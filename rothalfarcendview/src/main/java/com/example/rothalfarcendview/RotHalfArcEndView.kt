856ed07f1a9d61fe0725e5eef19eb8dd7fc91718
Anwesh43 <anweshthecool0@gmail.com>
2025-06-30 00:54:24 +0530

    rothalfarcendview: adding RotHalfArcEndView class

--- rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
+++ rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawRHAENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRotHalfArcEnd(scale, w, h, paint)
 }
+
+class RotHalfArcEndView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
