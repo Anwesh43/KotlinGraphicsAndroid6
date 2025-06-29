66b4a57a75e10f04136991da6ceb61f83de5cbc2
Anwesh43 <anweshthecool0@gmail.com>
2025-06-29 13:13:14 +0530

    linealtdownrotview: adding Canvas extension methods

--- linealtdownrotview/src/main/java/com/example/linealtdownrotview/LineAltDownRotView.kt
+++ linealtdownrotview/src/main/java/com/example/linealtdownrotview/LineAltDownRotView.kt
@@ -15,14 +15,47 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val rot : Float = 90f
 val strokeFactor : Float = 90f
 val backColor : Int = "#BDBDBD".toColorInt()
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineAltDownRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(deg * dsc(4))
+        for (j in 0..1) {
+            drawXY(-w * 0.5f * (1 - dsc(2 * j)), 0f) {
+                rotate((rot + deg * (1 - j)) * dsc(2 * j + 1))
+                drawLine(0f, 0f, size * 0.5f, 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLADRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineAltDownRot(scale, w, h, paint)
+}
