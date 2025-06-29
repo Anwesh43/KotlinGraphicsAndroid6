8338e4ac120b21aeb44d57b283f3f24af5ee128e
Anwesh43 <anweshthecool0@gmail.com>
2025-06-30 01:09:44 +0530

    rothalfarcendview: adding RotHalfArcEnd class

--- rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
+++ rothalfarcendview/src/main/java/com/example/rothalfarcendview/RotHalfArcEndView.kt
@@ -164,4 +164,27 @@ class RotHalfArcEndView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotHalfArcEnd(var i : Int) {
+
+        private var curr : RHAENode = RHAENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUdpating(cb)
+        }
+    }
 }
