4ef804c4b79a4f613ec83347eba2ba225f719800
Anwesh43 <anweshthecool0@gmail.com>
2025-07-02 04:57:25 +0530

    upsidedowntriview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.rothalfarcendview.RotHalfArcEndView
+import com.example.upsidedowntriview.UpSideDownTriView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotHalfArcEndView.create(this)
+        UpSideDownTriView.create(this)
     }
 }
