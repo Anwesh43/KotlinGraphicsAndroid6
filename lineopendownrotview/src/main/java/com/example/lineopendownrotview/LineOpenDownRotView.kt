70688bbe891ca2b4ed3d1dfd66bfdb08c1675aea
Anwesh43 <anweshthecool0@gmail.com>
2025-07-03 04:03:33 +0530

    lineopendownrotview: adding Renderer class

--- lineopendownrotview/src/main/java/com/example/lineopendownrotview/LineOpenDownRotView.kt
+++ lineopendownrotview/src/main/java/com/example/lineopendownrotview/LineOpenDownRotView.kt
@@ -188,4 +188,27 @@ class LineOpenDownRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineOpenDownRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lodr : LineOpenDownRot = LineOpenDownRot(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lodr.draw(canvas, paint)
+            animator.animate {
+                lodr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lodr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
