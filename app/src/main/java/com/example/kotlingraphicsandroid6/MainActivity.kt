1999b4ce97e9e11eddeea6b4b3cc02faea718e88
Anwesh43 <anweshthecool0@gmail.com>
2025-07-03 04:08:45 +0530

    lineopendownrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
-import com.example.upsidedowntriview.UpSideDownTriView
+import com.example.lineopendownrotview.LineOpenDownRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        UpSideDownTriView.create(this)
+        LineOpenDownRotView.create(this)
     }
 }
