ce652edeadf94637c61f6e39ea4cd97d2a8a58de
Anwesh43 <anweshthecool0@gmail.com>
2025-07-03 03:21:38 +0530

    lineopendownrotview: adding constants and libraries

---
+++ lineopendownrotview/src/main/java/com/example/lineopendownrotview/LineOpenDownRotView.kt
@@ -0,0 +1,26 @@
+package com.example.lineopendownrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.content.Context
+import android.app.Activity
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val rot : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val deg : Float = 180f
+val backColor : Int = "#BDBDBD".toColorInt()
