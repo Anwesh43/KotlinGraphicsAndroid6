6f37e01851120f0d53e5e6b4ab7f521df336a688
Anwesh43 <anweshthecool0@gmail.com>
2025-07-04 03:08:25 +0530

    bentclosearcrightview: adding BCARNode class

--- bentclosearcrightview/src/main/java/com/example/bentclosearcrightview/BentCloseArcRightView.kt
+++ bentclosearcrightview/src/main/java/com/example/bentclosearcrightview/BentCloseArcRightView.kt
@@ -121,4 +121,45 @@ class BentCloseArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BCARNode? = null
+        private var prev : BCARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCARNode {
+            var curr : BCARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
