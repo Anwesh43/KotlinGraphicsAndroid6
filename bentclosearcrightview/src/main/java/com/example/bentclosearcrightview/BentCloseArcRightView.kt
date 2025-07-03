e9b0819477b9ae8b0f852db6a2c09f4d6997f776
Anwesh43 <anweshthecool0@gmail.com>
2025-07-04 03:03:38 +0530

    bentclosearcrightview: adding BentCloseArcRightView class

--- bentclosearcrightview/src/main/java/com/example/bentclosearcrightview/BentCloseArcRightView.kt
+++ bentclosearcrightview/src/main/java/com/example/bentclosearcrightview/BentCloseArcRightView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBCARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentCloseArcRight(scale, w, h, paint)
 }
+
+class BentCloseArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
