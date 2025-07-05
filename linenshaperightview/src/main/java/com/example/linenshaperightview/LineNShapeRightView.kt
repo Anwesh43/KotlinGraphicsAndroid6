cd5b3247d6c7819627540fb7ecc0215de7e1ff22
Anwesh43 <anweshthecool0@gmail.com>
2025-07-05 10:00:22 +0530

    linenshaperightview: adding fix

--- linenshaperightview/src/main/java/com/example/linenshaperightview/LineNShapeRightView.kt
+++ linenshaperightview/src/main/java/com/example/linenshaperightview/LineNShapeRightView.kt
@@ -46,7 +46,7 @@ fun Canvas.drawLineNShapeRight(scale : Float, w : Float, h : Float, paint : Pain
                 drawLine(0f, 0f, 0f, -size, paint)
             }
         }
-        drawXY((w / 2) * 0.5f * (1 - dsc(1)), 0f) {
+        drawXY((w / 2) * (1 - dsc(1)), -size) {
             drawLine(0f, 0f, size, 0f, paint)
         }
 
