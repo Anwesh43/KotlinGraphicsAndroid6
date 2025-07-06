9f28fd251466f680cbdccd7bf7f63e9012e34492
Anwesh43 <anweshthecool0@gmail.com>
2025-07-06 10:28:41 +0530

    multilinerotaltview: adding MultiLineRotAlt class

--- multilinerotaltview/src/main/java/com/example/multilinerotaltview/MultiLineRotAltView.kt
+++ multilinerotaltview/src/main/java/com/example/multilinerotaltview/MultiLineRotAltView.kt
@@ -161,4 +161,27 @@ class MultiLineRotAltView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class MultiLineRotAlt(var i : Int) {
+
+        private var curr : MLRANode = MLRANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
