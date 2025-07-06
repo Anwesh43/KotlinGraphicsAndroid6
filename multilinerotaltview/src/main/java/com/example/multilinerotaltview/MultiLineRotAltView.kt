dfbf569870875cccce861ddb86bf62eff419ed87
Anwesh43 <anweshthecool0@gmail.com>
2025-07-06 10:26:43 +0530

    multilinerotaltview: adding MLRANode class

--- multilinerotaltview/src/main/java/com/example/multilinerotaltview/MultiLineRotAltView.kt
+++ multilinerotaltview/src/main/java/com/example/multilinerotaltview/MultiLineRotAltView.kt
@@ -120,4 +120,45 @@ class MultiLineRotAltView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLRANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MLRANode? = null
+        private var prev : MLRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLRANode {
+            var curr : MLRANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
