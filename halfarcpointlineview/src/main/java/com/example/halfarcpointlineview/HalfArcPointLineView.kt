3d398b99b1e3c021805d6bcb7d985b0a4d04a62d
Anwesh43 <anweshthecool0@gmail.com>
2025-07-08 08:26:58 +0530

    halfarcpointlineview: adding Canvas extension methods

--- halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
+++ halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
@@ -28,3 +28,38 @@ val lines : Int = 6
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcPointLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(0), false, paint)
+        for (j in 0..(lines - 1)) {
+            drawXY(0f, 0f) {
+                rotate((rot / (lines - 1)) * j)
+                drawXY(size / 2, 0f) {
+                    drawLine(0f, 0f, size * 0.5f * dsc(1).divideScale(j, lines), 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawHAPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcPointLine(scale, w, h, paint)
+}
