5f20950147ca443c5b5cae813f6f18ce2b9aa290
Anwesh43 <anweshthecool0@gmail.com>
2025-07-08 08:46:39 +0530

    halfarcpointlineview: adding Renderer in View

--- halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
+++ halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
@@ -66,14 +66,16 @@ fun Canvas.drawHAPLNode(i : Int, scale : Float, paint : Paint) {
 
 class HalfArcPointLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
