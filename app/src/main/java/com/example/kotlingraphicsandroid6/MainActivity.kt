1805150ef9b169253b36e9c33de6e62751565b78
Anwesh43 <anweshthecool0@gmail.com>
2025-07-08 08:48:17 +0530

    halfarcpointlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bentlinedownrotview.BentLineDownRotView
+import com.example.halfarcpointlineview.HalfArcPointLineView
 import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineDownRotView.create(this)
+        HalfArcPointLineView.create(this)
     }
 }
