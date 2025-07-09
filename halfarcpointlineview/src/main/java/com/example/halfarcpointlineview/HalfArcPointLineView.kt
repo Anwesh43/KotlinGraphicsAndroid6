87e633234f2bd813ad9e67459736f814f911cdf1
Anwesh43 <anweshthecool0@gmail.com>
2025-07-09 05:53:31 +0530

    halfarcpointlineview: adding fix

--- halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
+++ halfarcpointlineview/src/main/java/com/example/halfarcpointlineview/HalfArcPointLineView.kt
@@ -61,6 +61,7 @@ fun Canvas.drawHAPLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawHalfArcPointLine(scale, w, h, paint)
 }
 
