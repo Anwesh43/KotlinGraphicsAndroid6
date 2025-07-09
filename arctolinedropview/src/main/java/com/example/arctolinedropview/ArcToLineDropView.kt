b92608196259a90e7ee81574dd38cc4ab062b8ac
Anwesh43 <anweshthecool0@gmail.com>
2025-07-09 06:11:21 +0530

    arctolinedropview: adding ArcToLineDropView class

--- arctolinedropview/src/main/java/com/example/arctolinedropview/ArcToLineDropView.kt
+++ arctolinedropview/src/main/java/com/example/arctolinedropview/ArcToLineDropView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawATLDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcToLineDrop(scale, w, h, paint)
 }
+
+class ArcToLineDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
