0ee4db8bba3193b54a137d8a1883b1d025f258ab
Anwesh43 <anweshthecool0@gmail.com>
2025-07-10 10:08:47 +0530

    conclineopenrightview: adding view in acitivty

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,21 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.arctolinedropview.ArcToLineDropView
-import com.example.kotlingraphicsandroid6.ui.theme.KotlinGraphicsAndroid6Theme
+import com.example.conclineopenrightview.ConcLineOpenRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcToLineDropView.create(this)
+        ConcLineOpenRightView.create(this)
     }
 }
