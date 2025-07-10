ec40b2e3097b1bbdba440c6725e77ac7c372d2e4
Anwesh43 <anweshthecool0@gmail.com>
2025-07-10 09:52:59 +0530

    conclineopenrightview: adding CLORNode class

--- conclineopenrightview/src/main/java/com/example/conclineopenrightview/ConcLineOpenRightView.kt
+++ conclineopenrightview/src/main/java/com/example/conclineopenrightview/ConcLineOpenRightView.kt
@@ -125,4 +125,45 @@ class ConcLineOpenRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CLORNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CLORNode? = null
+        private var prev : CLORNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CLORNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCLORNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CLORNode {
+            var curr : CLORNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
