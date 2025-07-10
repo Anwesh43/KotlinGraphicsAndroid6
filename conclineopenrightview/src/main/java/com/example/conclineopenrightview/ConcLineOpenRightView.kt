59036c18a1cdc12b043d6becf03399ed2c43e28f
Anwesh43 <anweshthecool0@gmail.com>
2025-07-10 09:57:36 +0530

    conclineopenrightview: adding ConeLineOpenRight class

--- conclineopenrightview/src/main/java/com/example/conclineopenrightview/ConcLineOpenRightView.kt
+++ conclineopenrightview/src/main/java/com/example/conclineopenrightview/ConcLineOpenRightView.kt
@@ -166,4 +166,27 @@ class ConcLineOpenRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ConcLineOpenRight(var i : Int) {
+
+        private var curr : CLORNode = CLORNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
