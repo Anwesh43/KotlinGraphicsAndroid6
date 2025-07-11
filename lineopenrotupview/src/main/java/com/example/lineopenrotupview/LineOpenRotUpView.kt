e4d6c70f0f28707c654cf493e85e21b829bdebbf
Anwesh43 <anweshthecool0@gmail.com>
2025-07-11 10:22:32 +0530

    lineopenrotupview: adding LineOpenRotUpView class

--- lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
+++ lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLORUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineOpenRotUp(scale, w, h, paint)
 }
+
+class LineOpenRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
