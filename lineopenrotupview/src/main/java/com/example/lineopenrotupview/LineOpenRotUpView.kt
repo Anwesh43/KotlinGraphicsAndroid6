b9434756aec8610f6601d49ed8dd6f9a3a2b6056
Anwesh43 <anweshthecool0@gmail.com>
2025-07-14 23:31:29 +0530

    lineopenrotupview: adding static create method

--- lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
+++ lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
@@ -214,4 +214,12 @@ class LineOpenRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : LineOpenRotUpView {
+            val view : LineOpenRotUpView = LineOpenRotUpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
