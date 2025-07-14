ea3ffb624391afbb6602eb89e33983edea61e1c5
Anwesh43 <anweshthecool0@gmail.com>
2025-07-14 23:30:28 +0530

    lineopenrotupview: adding Renderer class

--- lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
+++ lineopenrotupview/src/main/java/com/example/lineopenrotupview/LineOpenRotUpView.kt
@@ -189,4 +189,27 @@ class LineOpenRotUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineOpenRotUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val loru : LineOpenRotUp = LineOpenRotUp(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            loru.draw(canvas, paint)
+            animator.animate {
+                loru.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            loru.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
