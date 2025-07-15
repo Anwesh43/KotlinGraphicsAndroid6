04be5777df7dcdbdf33c9b464e7e291e510e3a2d
Anwesh43 <anweshthecool0@gmail.com>
2025-07-15 22:48:08 +0530

    linesicklerotdownview: adding Renderer class

--- linesicklerotdownview/src/main/java/com/example/linesicklerotdownview/LineSickleRotDownView.kt
+++ linesicklerotdownview/src/main/java/com/example/linesicklerotdownview/LineSickleRotDownView.kt
@@ -184,4 +184,27 @@ class LineSickleRotDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineSickleRotDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lsrd : LineSickleRotDown = LineSickleRotDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lsrd.draw(canvas, paint)
+            animator.animate {
+                lsrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lsrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
