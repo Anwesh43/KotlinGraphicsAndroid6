8e34e14402c41bd04e6406f51a819cab134fb217
Anwesh43 <anweshthecool0@gmail.com>
2025-07-16 14:51:44 +0530

    linerotdroprightview: adding LineRotDropRightView class

--- linerotdroprightview/src/main/java/com/example/linerotdroprightview/LineRotDropRightView.kt
+++ linerotdroprightview/src/main/java/com/example/linerotdroprightview/LineRotDropRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRDRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotDropRight(scale, w, h, paint)
 }
+
+class LineRotDropRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
