c2af0eee5219d91d43b90cfe72797673ce0f2647
Anwesh43 <anweshthecool0@gmail.com>
2025-07-18 18:20:58 +0530

    linearccaprightview: adding Canvas extension methods

--- linearccaprightview/src/main/java/com/example/linearccaprightview/LineArcCapRightView.kt
+++ linearccaprightview/src/main/java/com/example/linearccaprightview/LineArcCapRightView.kt
@@ -28,3 +28,32 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCapRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * (dsc(1) + dsc(3)))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), sweep, sweep * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcCapRight(scale, w, h, paint)
+}
