e771f8a6b5dbbb062663cd5bb3569e6be9b5910f
Anwesh43 <anweshthecool0@gmail.com>
2025-07-19 13:40:50 +0530

    seqarcjoindownview: adding SAJDNode class

--- seqarcjoindownview/src/main/java/com/example/seqarcjoindownview/SeqArcJoinDownView.kt
+++ seqarcjoindownview/src/main/java/com/example/seqarcjoindownview/SeqArcJoinDownView.kt
@@ -122,4 +122,45 @@ class SeqArcJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAJDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SAJDNode? = null
+        private var prev : SAJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SAJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAJDNode {
+            var curr : SAJDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
