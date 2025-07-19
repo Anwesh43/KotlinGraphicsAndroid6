37e933a04c7a3a611c954c0b2eebd839b5f38451
Anwesh43 <anweshthecool0@gmail.com>
2025-07-19 13:18:17 +0530

    seqarcjoindownview: adding SeqArcJoinDownView class

--- seqarcjoindownview/src/main/java/com/example/seqarcjoindownview/SeqArcJoinDownView.kt
+++ seqarcjoindownview/src/main/java/com/example/seqarcjoindownview/SeqArcJoinDownView.kt
@@ -60,3 +60,18 @@ fun Canvas.drawSAJDNode(i : Int, scale : Float, paint : Paint) {
     drawSeqArcJoinDown(scale, w, h, paint)
 }
 
+class SeqArcJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
