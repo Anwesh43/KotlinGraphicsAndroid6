8dbb44ada3103641d70c85b419e31baefae79c37
Anwesh43 <anweshthecool0@gmail.com>
2025-07-20 08:13:01 +0530

    linequarterarcleftview: adding LQALNode class

--- linequarterarcleftview/src/main/java/com/example/linequarterarcleftview/LineQuarterArcLeftView.kt
+++ linequarterarcleftview/src/main/java/com/example/linequarterarcleftview/LineQuarterArcLeftView.kt
@@ -126,4 +126,45 @@ class LineQuarterArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LQALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LQALNode? = null
+        private var prev : LQALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LQALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLQALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LQALNode {
+            var curr : LQALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
