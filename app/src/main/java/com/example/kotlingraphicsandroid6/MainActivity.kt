144f5ebf8501556f7558e16767fdbd9fadbd3372
Anwesh43 <anweshthecool0@gmail.com>
2025-07-20 08:18:01 +0530

    linequarterarcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.seqarcjoindownview.SeqArcJoinDownView
+import com.example.linequarterarcleftview.LineQuarterArcLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SeqArcJoinDownView.create(this)
+        LineQuarterArcLeftView.create(this)
     }
 }
