dfd4dbfb6903e066527e53eb54cf72f770205d71
Anwesh43 <anweshthecool0@gmail.com>
2025-07-20 07:56:15 +0530

    linequarterarcleftview: adding module setup

---
+++ linequarterarcleftview/src/main/java/com/example/linequarterarcleftview/LineQuarterArcLeftView.kt
@@ -0,0 +1,26 @@
+package com.example.linequarterarcleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val sizeFactor : Float = 6.9f
+val strokeFactor : Float = 90f
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+
