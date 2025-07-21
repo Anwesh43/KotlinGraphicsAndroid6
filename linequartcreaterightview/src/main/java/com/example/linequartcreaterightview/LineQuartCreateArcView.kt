64e77f8258c386c0b3ce606372f1eb20382d7755
Anwesh43 <anweshthecool0@gmail.com>
2025-07-21 08:59:33 +0530

    linequartcreaterightview: adding LineQuartCreateArcView class

--- linequartcreaterightview/src/main/java/com/example/linequartcreaterightview/LineQuartCreateArcView.kt
+++ linequartcreaterightview/src/main/java/com/example/linequartcreaterightview/LineQuartCreateArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLQCANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineQuartCreateArc(scale, w, h, paint)
 }
+
+class LineQuartCreateArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
