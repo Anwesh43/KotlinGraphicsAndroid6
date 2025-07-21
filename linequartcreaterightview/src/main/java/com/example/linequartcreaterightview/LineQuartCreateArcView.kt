bbeea0cb482774a2a5bcab9034947b40dfc35a51
Anwesh43 <anweshthecool0@gmail.com>
2025-07-21 08:58:18 +0530

    linequartcreaterightview: adding Canvas extension methods

--- linequartcreaterightview/src/main/java/com/example/linequartcreaterightview/LineQuartCreateArcView.kt
+++ linequartcreaterightview/src/main/java/com/example/linequartcreaterightview/LineQuartCreateArcView.kt
@@ -18,7 +18,7 @@ val colors : Array<String> = arrayOf(
 )
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
-val rot : Float = 90f
+val rot : Float = 180f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val sizeFactor : Float = 5.9f
@@ -28,3 +28,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineQuartCreateArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+            drawArc(RectF(-size, -size, size, size), -90f - 90f * dsc(1), 90f * dsc(1), false, paint)
+            drawXY(-size, 0f) {
+                drawLine(0f, 0f, size * dsc(2), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLQCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineQuartCreateArc(scale, w, h, paint)
+}
