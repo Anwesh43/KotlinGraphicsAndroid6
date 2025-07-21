4c9843dc223ca953ffcfacb1654472ba039bf03d
Anwesh43 <anweshthecool0@gmail.com>
2025-07-21 09:10:53 +0530

    linequartcreaterightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linequarterarcleftview.LineQuarterArcLeftView
+import com.example.linequartcreaterightview.LineQuartCreateArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineQuarterArcLeftView.create(this)
+        LineQuartCreateArcView.create(this)
     }
 }
