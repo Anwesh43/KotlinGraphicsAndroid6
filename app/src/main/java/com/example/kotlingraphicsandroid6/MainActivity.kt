4ca8f4bf8b395d272b2bcdb1295137aee35ecd76
Anwesh43 <anweshthecool0@gmail.com>
2025-07-22 07:50:46 +0530

    altbentlinearcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linequartcreaterightview.LineQuartCreateArcView
+import com.example.altbentlinearcview.AltBentLineArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineQuartCreateArcView.create(this)
+        AltBentLineArcView.create(this)
     }
 }
