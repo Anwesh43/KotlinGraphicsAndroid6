2bb73d56fde70fafeeb519a5cf74b6bf026a36ad
Anwesh43 <anweshthecool0@gmail.com>
2025-07-22 07:31:06 +0530

    altbentlinearcview: adding Canvas extension methods

--- altbentlinearcview/src/main/java/com/example/altbentlinearcview/AltBentLineArcView.kt
+++ altbentlinearcview/src/main/java/com/example/altbentlinearcview/AltBentLineArcView.kt
@@ -23,7 +23,44 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
+val deg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawAltBentLineArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY((w / 2 - size) * (1 - dsc(1)), 0f) {
+                    rotate(-deg * (1 - dsc(2)))
+                    drawLine(0f, 0f, size * dsc(0), 0f, paint)
+                }
+                drawArc(RectF(-size, -size, size, size), 0f, rot * dsc(3), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawABLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAltBentLineArc(scale, w, h, paint)
+}
