f0aacf9d6697348c52e5daf47496841a2b58bc90
Anwesh43 <anweshthecool0@gmail.com>
2025-07-23 06:51:44 +0530

    bentlinestraightjoinview: adding BentLineStraightJoin class

--- bentlinestraightjoinview/src/main/java/com/example/bentlinestraightjoinview/BentLineStraightJoinView.kt
+++ bentlinestraightjoinview/src/main/java/com/example/bentlinestraightjoinview/BentLineStraightJoinView.kt
@@ -164,5 +164,28 @@ class BentLineStraightJoinView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+
+        data class BentLineStraightJoin(var i : Int) {
+
+            private var curr : BLSJNode = BLSJNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
