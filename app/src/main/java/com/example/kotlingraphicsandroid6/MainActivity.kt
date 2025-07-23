eea31741fb227d705177505b978c2e0e75805072
Anwesh43 <anweshthecool0@gmail.com>
2025-07-23 07:00:06 +0530

    bentlinestraightjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.altbentlinearcview.AltBentLineArcView
+import com.example.bentlinestraightjoinview.BentLineStraightJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AltBentLineArcView.create(this)
+        BentLineStraightJoinView.create(this)
     }
 }
