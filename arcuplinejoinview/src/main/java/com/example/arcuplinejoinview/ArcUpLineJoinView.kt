9b754f1d3e3b09c215cd09377980a750eb22f717
Anwesh43 <anweshthecool0@gmail.com>
2025-07-24 14:40:25 +0530

    arcuplinejoinview: adding ArcUpLineJoin class

--- arcuplinejoinview/src/main/java/com/example/arcuplinejoinview/ArcUpLineJoinView.kt
+++ arcuplinejoinview/src/main/java/com/example/arcuplinejoinview/ArcUpLineJoinView.kt
@@ -103,6 +103,8 @@ class ArcUpLineJoinView(ctx : Context) : View(ctx) {
                 try {
                     Thread.sleep(delay)
                     view.invalidate()
+                } catch(ex : Exception) {
+
                 }
             }
         }
@@ -159,6 +161,29 @@ class ArcUpLineJoinView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class ArcUpLineJoin(var i : Int) {
+
+        private var curr : AULJNode = AULJNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
