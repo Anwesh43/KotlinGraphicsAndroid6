1b31d9960f114b7fd5cf9b02483c24890651498c
Anwesh43 <anweshthecool0@gmail.com>
2025-07-24 14:37:14 +0530

    arcuplinejoinview: adding AULJNode class

--- arcuplinejoinview/src/main/java/com/example/arcuplinejoinview/ArcUpLineJoinView.kt
+++ arcuplinejoinview/src/main/java/com/example/arcuplinejoinview/ArcUpLineJoinView.kt
@@ -120,4 +120,45 @@ class ArcUpLineJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AULJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AULJNode? = null
+        private var prev : AULJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AULJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAULJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AULJNode {
+            var curr : AULJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
