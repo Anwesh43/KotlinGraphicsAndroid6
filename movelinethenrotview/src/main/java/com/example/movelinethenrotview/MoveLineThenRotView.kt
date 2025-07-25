6cfac031f2927d118e9863e766d5547784e2a11d
Anwesh43 <anweshthecool0@gmail.com>
2025-07-25 08:48:22 +0530

    movelinethenrotview: adding MoveLineThenRotView class

--- movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
+++ movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawMLTRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMoveLineThenRot(scale, w, h, paint)
 }
+
+class MoveLineThenRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
