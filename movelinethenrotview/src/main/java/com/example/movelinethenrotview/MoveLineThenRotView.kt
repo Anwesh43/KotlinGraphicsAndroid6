b20e0007c87985f7ec9aeff4a5355fbd035990b4
Anwesh43 <anweshthecool0@gmail.com>
2025-07-25 08:45:37 +0530

    movelinethenrotview: adding Canvas extension methods

--- movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
+++ movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
@@ -15,7 +15,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val rot : Float = -90f
 val strokeFactor : Float = 90f
@@ -26,3 +26,34 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMoveLineThenRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawLine(0f, 0f, 0f, -size * (dsc(0) - dsc(1)), paint)
+        drawLine(size * dsc(2), 0f, size * dsc(1), 0f, paint)
+        drawXY(size, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size * dsc(2), paint)
+        }
+    }
+}
+
+fun Canvas.drawMLTRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMoveLineThenRot(scale, w, h, paint)
+}
