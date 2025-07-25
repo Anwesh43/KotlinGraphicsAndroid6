f0c50f4c1ca7e9c30839ccdc7e43bb71220cdad1
Anwesh43 <anweshthecool0@gmail.com>
2025-07-25 10:06:16 +0530

    movelinethenrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arcuplinejoinview.ArcUpLineJoinView
+import com.example.movelinethenrotview.MoveLineThenRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcUpLineJoinView.create(this)
+        MoveLineThenRotView.create(this)
     }
 }
