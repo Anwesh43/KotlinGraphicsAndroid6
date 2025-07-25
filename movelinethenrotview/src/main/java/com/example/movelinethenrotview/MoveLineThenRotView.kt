967f2cf4ca7a3b59c33a1d64edea94e07ee446e5
Anwesh43 <anweshthecool0@gmail.com>
2025-07-25 09:14:12 +0530

    movelinethenrotview: adding MLTRNode class

--- movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
+++ movelinethenrotview/src/main/java/com/example/movelinethenrotview/MoveLineThenRotView.kt
@@ -120,4 +120,45 @@ class MoveLineThenRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLTRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MLTRNode? = null
+        private var prev : MLTRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLTRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLTRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLTRNode {
+            var curr : MLTRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
