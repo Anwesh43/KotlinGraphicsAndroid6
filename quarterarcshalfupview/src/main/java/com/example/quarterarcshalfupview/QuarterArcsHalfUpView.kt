7a0cb20ee1643be844eb9a1685ee16358d872bf4
Anwesh43 <anweshthecool0@gmail.com>
2025-07-26 10:12:35 +0530

    quarterarcshalfupview: adding QAHUNode class

--- quarterarcshalfupview/src/main/java/com/example/quarterarcshalfupview/QuarterArcsHalfUpView.kt
+++ quarterarcshalfupview/src/main/java/com/example/quarterarcshalfupview/QuarterArcsHalfUpView.kt
@@ -124,4 +124,45 @@ class QuarterArcsHalfUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QAHUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : QAHUNode? = null
+        private var prev : QAHUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QAHUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQAHUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QAHUNode {
+            var curr : QAHUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
