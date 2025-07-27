a59ca302bbe52110d0a93651bf6860c563111928
Anwesh43 <anweshthecool0@gmail.com>
2025-07-27 08:07:11 +0530

    linearcsweepdownview: adding LineArcSweepDown class

--- linearcsweepdownview/src/main/java/com/example/linearcsweepdownview/LineArcSweepDownView.kt
+++ linearcsweepdownview/src/main/java/com/example/linearcsweepdownview/LineArcSweepDownView.kt
@@ -164,4 +164,27 @@ class LineArcSweepDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcSweepDown(var i : Int) {
+
+        private var curr : LASDNode = LASDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
