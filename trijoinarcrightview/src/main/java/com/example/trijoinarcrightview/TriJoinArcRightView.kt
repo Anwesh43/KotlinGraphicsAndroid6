63ec2338d61257521b416f7d7a9856c4ef326ebe
Anwesh43 <anweshthecool0@gmail.com>
2025-07-28 10:52:36 +0530

    trijoinarcrightview: adding Canvas extension methods

--- trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
+++ trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
@@ -20,10 +20,52 @@ val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 90f
-val rot : Float = 4.9f
+val rot : Float = -90f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriJoinArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawLine(0f, 0f, size * dsc(0), -size * dsc(0), paint)
+        drawXY(size, -size) {
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+        }
+        drawXY(size, 0f) {
+            rotate(rot * dsc(3))
+            drawArc(
+                RectF(
+                    0f, -size / 2, size, size / 2
+                ),
+                180f * (1 - dsc(2)),
+                180f * dsc(2),
+                false,
+                paint
+            )
+        }
+    }
+}
+
+fun Canvas.drawTJARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawTriJoinArcRight(scale, w, h, paint)
+}
