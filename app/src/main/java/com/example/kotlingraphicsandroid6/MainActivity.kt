af20c00facb5a51fd20e5677b8a6412cbbde1f98
Anwesh43 <anweshthecool0@gmail.com>
2025-07-28 11:21:11 +0530

    trijoinarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcsweepdownview.LineArcSweepDownView
+import com.example.trijoinarcrightview.TriJoinArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcSweepDownView.create(this)
+        TriJoinArcRightView.create(this)
     }
 }
