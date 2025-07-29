0b9c34f4c2c96273aecca68debcd422c0943932b
Anwesh43 <anweshthecool0@gmail.com>
2025-07-29 09:00:18 +0530

    linebenthorizintoview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.trijoinarcrightview.TriJoinArcRightView
+import com.example.linebenthorizintoview.LineBentHorizIntoView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TriJoinArcRightView.create(this)
+        LineBentHorizIntoView.create(this)
     }
 }
