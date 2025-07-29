4e2ac251f08ae427d2bd750cae0b537f3b540710
Anwesh43 <anweshthecool0@gmail.com>
2025-07-29 08:44:40 +0530

    linebenthorizintoview: adding Canvas extension method

--- linebenthorizintoview/src/main/java/com/example/linebenthorizintoview/LineBentHorizIntoView.kt
+++ linebenthorizintoview/src/main/java/com/example/linebenthorizintoview/LineBentHorizIntoView.kt
@@ -17,7 +17,7 @@ val colors : Array<String> = arrayOf(
 )
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
-val rot : Float = 90f
+val rot : Float = 270f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
@@ -26,3 +26,35 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentHorizInto(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        drawXY(-w * 0.5f * (1 - dsc(1)), -size) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+        drawXY(-size, 0f) {
+            drawLine(size * dsc(3), -size * dsc(3), size * dsc(0), -size * dsc(0), paint)
+        }
+    }
+}
+
+fun Canvas.drawLBHINode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentHorizInto(scale, w, h, paint)
+}
