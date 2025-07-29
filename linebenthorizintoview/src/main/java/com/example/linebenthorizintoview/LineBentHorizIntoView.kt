f4b6164ed7d8da85259008cef39e744966d75fe3
Anwesh43 <anweshthecool0@gmail.com>
2025-07-29 08:45:51 +0530

    linebenthorizintoview: adding LineBentHorizIntoView class

--- linebenthorizintoview/src/main/java/com/example/linebenthorizintoview/LineBentHorizIntoView.kt
+++ linebenthorizintoview/src/main/java/com/example/linebenthorizintoview/LineBentHorizIntoView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLBHINode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentHorizInto(scale, w, h, paint)
 }
+
+class LineBentHorizIntoView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
