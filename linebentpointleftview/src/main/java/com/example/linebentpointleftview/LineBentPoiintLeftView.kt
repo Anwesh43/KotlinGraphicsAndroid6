2a3b566b87e53dff53b5d7c37b2ca8c623c656f3
Anwesh43 <anweshthecool0@gmail.com>
2025-07-30 09:21:47 +0530

    linebentpointleftview: adding Canvas extension methods

--- linebentpointleftview/src/main/java/com/example/linebentpointleftview/LineBentPoiintLeftView.kt
+++ linebentpointleftview/src/main/java/com/example/linebentpointleftview/LineBentPoiintLeftView.kt
@@ -26,3 +26,36 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentPointLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            val upSize : Float = -size * ((1 - j) * (1 - dsc(1)) + j * dsc(1))
+            val downSize : Float = -size * (1 - dsc(0)) * (1 - j)
+            drawXY(0f, 0f) {
+                rotate(rot * (1 - dsc(2)) * (1 - 2 * j))
+                drawLine(0f, 0f, 0f, upSize, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentPointLeft(scale, w, h, paint)
+}
