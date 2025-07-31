ce5cca77545c3526f7d3ea79180322884f6cf173
Anwesh43 <anweshthecool0@gmail.com>
2025-07-31 12:16:41 +0530

    linebentfromleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentpointleftview.LineBentPointLeftView
+import com.example.linebentfromleftview.LineBentFromLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentPointLeftView.create(this)
+        LineBentFromLeftView.create(this)
     }
 }
