25dddab5d48ff349e7c2582e0e0c2891a72a22c1
Anwesh43 <anweshthecool0@gmail.com>
2025-07-31 10:11:06 +0530

    linebentfromleftview: adding LBFLNode class

--- linebentfromleftview/src/main/java/com/example/linebentfromleftview/LineBentFromLeftView.kt
+++ linebentfromleftview/src/main/java/com/example/linebentfromleftview/LineBentFromLeftView.kt
@@ -116,4 +116,45 @@ class LineBentFromLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBFLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBFLNode? = null
+        private var prev : LBFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBFLNode {
+            var curr : LBFLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
