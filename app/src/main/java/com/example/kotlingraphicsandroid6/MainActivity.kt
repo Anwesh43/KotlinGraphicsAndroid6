fc49407464921b916070289ae939202623fa2ba6
Anwesh43 <anweshthecool0@gmail.com>
2025-08-01 07:58:31 +0530

    concseqarcdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentfromleftview.LineBentFromLeftView
+import com.example.concseqarcdownview.ConcArcSeqDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentFromLeftView.create(this)
+        ConcArcSeqDownView.create(this)
     }
 }
