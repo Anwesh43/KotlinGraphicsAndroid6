4fde37232e26ba07c8b3c63d7dce587052c6d12b
Anwesh43 <anweshthecool0@gmail.com>
2025-08-02 11:47:30 +0530

    linerotarcsideview: adding LineRotArcSideView class

--- linerotarcsideview/src/main/java/com/example/linerotarcsideview/LineRotArcSideView.kt
+++ linerotarcsideview/src/main/java/com/example/linerotarcsideview/LineRotArcSideView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLRASNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotArcSide(scale, w, h, paint)
 }
+
+class LineRotArcSideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
