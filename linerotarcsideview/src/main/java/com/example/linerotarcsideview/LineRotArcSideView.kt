0f77db6bbdf7641cd03246f0f529b6eaad80ccfc
Anwesh43 <anweshthecool0@gmail.com>
2025-08-02 12:24:53 +0530

    linerotarcsideview: adding fix

--- linerotarcsideview/src/main/java/com/example/linerotarcsideview/LineRotArcSideView.kt
+++ linerotarcsideview/src/main/java/com/example/linerotarcsideview/LineRotArcSideView.kt
@@ -55,6 +55,7 @@ fun Canvas.drawLRASNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineRotArcSide(scale, w, h, paint)
 }
 
