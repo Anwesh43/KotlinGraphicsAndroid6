6a1506491cce71b9c2a5bcb271a09ff043b5008c
Anwesh43 <anweshthecool0@gmail.com>
2025-08-02 12:11:45 +0530

    linerotarcsideview: adding view in acitivty

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.concseqarcdownview.ConcArcSeqDownView
+import com.example.linerotarcsideview.LineRotArcSideView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ConcArcSeqDownView.create(this)
+        LineRotArcSideView.create(this)
     }
 }
