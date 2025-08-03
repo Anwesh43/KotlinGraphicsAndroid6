d0e094aac5b0be280fa77b457c48572095ab9299
Anwesh43 <anweshthecool0@gmail.com>
2025-08-03 08:30:20 +0530

    rightlinedividearcview: adding Canvas extension methods

--- rightlinedividearcview/src/main/java/com/example/rightlinedividearcview/RightLineDivideArcView.kt
+++ rightlinedividearcview/src/main/java/com/example/rightlinedividearcview/RightLineDivideArcView.kt
@@ -19,7 +19,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 5.9f
+val sizeFactor : Float = 7.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
@@ -28,3 +28,35 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineDivideArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size + (w / 2 - size) * (1 - dsc(1)), 0f) {
+            drawLine(0f, 0f, size, 0f, paint)
+            drawArc(RectF(-size, -size, size, size), -deg * 0.5f * dsc(2), deg * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawRLDANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRightLineDivideArc(scale, w, h, paint)
+}
