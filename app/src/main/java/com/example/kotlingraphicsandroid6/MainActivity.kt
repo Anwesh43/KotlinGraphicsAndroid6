62458728c959378af45090e3b6be689d04827446
Anwesh43 <anweshthecool0@gmail.com>
2025-08-04 10:23:30 +0530

    linebentarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.rightlinedividearcview.RightLineDivideArcView
+import com.example.linebentarcrightview.LineBentArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RightLineDivideArcView.create(this)
+        LineBentArcRightView.create(this)
     }
 }
