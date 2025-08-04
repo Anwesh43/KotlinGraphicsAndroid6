b9f655202417b7a80d9edbdf643db369ee1a3c0b
Anwesh43 <anweshthecool0@gmail.com>
2025-08-04 09:57:29 +0530

    linebentarcrightview: adding LineBentArcRightView class

--- linebentarcrightview/src/main/java/com/example/linebentarcrightview/LineBentArcRightView.kt
+++ linebentarcrightview/src/main/java/com/example/linebentarcrightview/LineBentArcRightView.kt
@@ -74,3 +74,19 @@ fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcRight(scale, w, h, paint)
 }
+
+class LineBentArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
