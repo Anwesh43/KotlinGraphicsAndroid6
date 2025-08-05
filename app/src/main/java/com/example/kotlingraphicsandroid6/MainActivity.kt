c45c02b4e79786d262a722124f578f4a7fc81ed4
Anwesh43 <anweshthecool0@gmail.com>
2025-08-05 08:18:55 +0530

    midlinerotupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentarcrightview.LineBentArcRightView
+import com.example.midlinerotupview.MidLineRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentArcRightView.create( this)
+        MidLineRotUpView.create( this)
     }
 }
