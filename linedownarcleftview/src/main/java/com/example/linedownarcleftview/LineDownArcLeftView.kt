55c43908e545b9886aab090f2aeb6fe55084c619
Anwesh43 <anweshthecool0@gmail.com>
2025-08-06 10:43:36 +0530

    linedownarcleftview: adding Renderer class

--- linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
+++ linedownarcleftview/src/main/java/com/example/linedownarcleftview/LineDownArcLeftView.kt
@@ -187,4 +187,27 @@ class LineDownArcLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineDownArcLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val ldal : LineDownArcLeft = LineDownArcLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ldal.draw(canvas, paint)
+            animator.animate {
+                ldal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ldal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
