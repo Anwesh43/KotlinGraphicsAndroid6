42040b4a78af19bc3b2f3d6b95a9d44c6a5b2bbc
Anwesh43 <anweshthecool0@gmail.com>
2025-08-06 10:48:32 +0530

    linedownarcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.midlinerotupview.MidLineRotUpView
+import com.example.linedownarcleftview.LineDownArcLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MidLineRotUpView.create( this)
+        LineDownArcLeftView.create( this)
     }
 }
