e479766dc2073291fa265698af840bd4993f3602
Anwesh43 <anweshthecool0@gmail.com>
2025-08-07 10:26:14 +0530

    linejoinperprotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linedownarcleftview.LineDownArcLeftView
+import com.example.linejoinperprotview.LineJoinPerpRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineDownArcLeftView.create( this)
+        LineJoinPerpRotView.create( this)
     }
 }
