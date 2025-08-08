4c0a150cc80019256ba3244fcd7f1d44da24a8f5
Anwesh43 <anweshthecool0@gmail.com>
2025-08-08 08:35:53 +0530

    linebentrotupview: adding LineBentRotUpView class

--- linebentrotupview/src/main/java/com/example/linebentrotupview/LineBentRotUpView.kt
+++ linebentrotupview/src/main/java/com/example/linebentrotupview/LineBentRotUpView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLBRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentRotUp(scale, w, h, paint)
 }
+
+class LineBentRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
