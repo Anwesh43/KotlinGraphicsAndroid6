2bee25b8093deac8abc92864a87abb5bb6a47b22
Anwesh43 <anweshthecool0@gmail.com>
2025-08-08 08:49:39 +0530

    linebentrotupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linejoinperprotview.LineJoinPerpRotView
+import com.example.linebentrotupview.LineBentRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineJoinPerpRotView.create( this)
+        LineBentRotUpView.create( this)
     }
 }
