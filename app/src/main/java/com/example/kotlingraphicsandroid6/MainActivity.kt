d0c4977edb9280be70cf95898d1b15aefa94a43f
Anwesh43 <anweshthecool0@gmail.com>
2025-08-09 10:10:52 +0530

    rightarccreateview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentrotupview.LineBentRotUpView
+import com.example.rightarccreateview.RightArcCreateView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentRotUpView.create( this)
+        RightArcCreateView.create( this)
     }
 }
