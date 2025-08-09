cef15ab53d5de539576af4b00773846f1f05ca7d
Anwesh43 <anweshthecool0@gmail.com>
2025-08-09 09:49:39 +0530

    rightarccreateview: adding Canvas extension methods

--- rightarccreateview/src/main/java/com/example/rightarccreateview/RightArcCreateView.kt
+++ rightarccreateview/src/main/java/com/example/rightarccreateview/RightArcCreateView.kt
@@ -27,3 +27,30 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightArcCreate(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 - size) * (1 - dsc(1)) - w * 0.5f * dsc(4), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size, -size, size, size), 0f, rot * (dsc(0) + dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawRACNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightArcCreate(scale, w, h, paint)
+}
