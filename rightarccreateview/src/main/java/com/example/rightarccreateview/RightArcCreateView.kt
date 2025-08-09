b819a4e85abdf18b810631c1a7db5c1651025cb5
Anwesh43 <anweshthecool0@gmail.com>
2025-08-09 10:05:15 +0530

    rightarccreateview: adding RACNode class

--- rightarccreateview/src/main/java/com/example/rightarccreateview/RightArcCreateView.kt
+++ rightarccreateview/src/main/java/com/example/rightarccreateview/RightArcCreateView.kt
@@ -116,4 +116,45 @@ class RightArcCreateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RACNode? = null
+        private var prev : RACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RACNode {
+            var curr : RACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
