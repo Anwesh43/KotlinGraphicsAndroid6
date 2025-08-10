0c48bae6590253f2031f5961df22643be1407bf8
Anwesh43 <anweshthecool0@gmail.com>
2025-08-10 12:03:32 +0530

    linearcencloserightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.rightarccreateview.RightArcCreateView
+import com.example.linearcencloserightview.LineArcEncloseRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RightArcCreateView.create( this)
+        LineArcEncloseRightView.create( this)
     }
 }
