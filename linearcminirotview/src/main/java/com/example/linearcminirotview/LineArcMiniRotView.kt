cc5a4ae634d984a96e58493070f55615b7b2e693
Anwesh43 <anweshthecool0@gmail.com>
2025-08-12 03:14:20 +0530

    linearcminirotview: adding Renderer class

--- linearcminirotview/src/main/java/com/example/linearcminirotview/LineArcMiniRotView.kt
+++ linearcminirotview/src/main/java/com/example/linearcminirotview/LineArcMiniRotView.kt
@@ -189,4 +189,27 @@ class LineArcMiniRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcMiniRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lamr : LineArcMiniRot = LineArcMiniRot(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lamr.draw(canvas, paint)
+            animator.animate {
+                lamr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lamr.startUpdaitng {
+                animator.start()
+            }
+        }
+    }
 }
