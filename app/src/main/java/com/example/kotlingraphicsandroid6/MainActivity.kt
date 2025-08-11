5eab9c78f94194ed45d1f577d6bfa44273a7f733
Anwesh43 <anweshthecool0@gmail.com>
2025-08-11 12:18:22 +0530

    rotlinestraightdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcencloserightview.LineArcEncloseRightView
+import com.example.rotlinesraightdownview.RotLineStraightDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcEncloseRightView.create( this)
+        RotLineStraightDownView.create( this)
     }
 }
