54fd632b6f8e10f1e2fc69a356d06bb858e87cc3
Anwesh43 <anweshthecool0@gmail.com>
2025-08-12 03:15:19 +0530

    linearcminirotview: adding static create method

--- linearcminirotview/src/main/java/com/example/linearcminirotview/LineArcMiniRotView.kt
+++ linearcminirotview/src/main/java/com/example/linearcminirotview/LineArcMiniRotView.kt
@@ -214,4 +214,13 @@ class LineArcMiniRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : LineArcMiniRotView {
+            val view : LineArcMiniRotView = LineArcMiniRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
