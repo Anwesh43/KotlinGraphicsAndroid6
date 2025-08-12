cc1326645aeef7c04462ec38b1cf31eb0142d543
Anwesh43 <anweshthecool0@gmail.com>
2025-08-13 01:14:34 +0530

    linearcminirotview: adding BiBentFoldLine class

--- bibentfoldlineview/src/main/java/com/example/bibentfoldlineview/BiBentFoldLineView.kt
+++ bibentfoldlineview/src/main/java/com/example/bibentfoldlineview/BiBentFoldLineView.kt
@@ -164,4 +164,27 @@ class BiBentFoldLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiBentFoldLine(var i : Int) {
+
+        private var curr : BBFLNode = BBFLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
