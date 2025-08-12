1e127ded3072f8e078462f99d12527af5be93c0c
Anwesh43 <anweshthecool0@gmail.com>
2025-08-13 01:19:27 +0530

    linearcminirotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcminirotview.LineArcMiniRotView
+import com.example.bibentfoldlineview.BiBentFoldLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcMiniRotView.create( this)
+        BiBentFoldLineView.create( this)
     }
 }
