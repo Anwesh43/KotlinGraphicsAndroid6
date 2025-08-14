8fa10e70dcdf132224968c2200dce01f522d6c42
Anwesh43 <anweshthecool0@gmail.com>
2025-08-14 13:42:25 +0530

    steplinerotrightview: adding StepLineRotRightView class

--- steplinerotrightview/src/main/java/com/example/steplinerotrightview/StepLineRotRightView.kt
+++ steplinerotrightview/src/main/java/com/example/steplinerotrightview/StepLineRotRightView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawSLRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawStepLineRotRight(scale, w, h, paint)
 }
+
+class StepLineRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
