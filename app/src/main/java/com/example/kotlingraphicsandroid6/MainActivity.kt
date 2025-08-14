c59439215ea6f3634fdc3c2f6df6d652ee53699d
Anwesh43 <anweshthecool0@gmail.com>
2025-08-14 16:46:35 +0530

    steplinerotrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bibentfoldlineview.BiBentFoldLineView
+import com.example.steplinerotrightview.StepLineRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiBentFoldLineView.create( this)
+        StepLineRotRightView.create( this)
     }
 }
