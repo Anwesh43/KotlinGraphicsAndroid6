b3bcbdc91a6b3d44c73e4def6e531294cd602a9e
Anwesh43 <anweshthecool0@gmail.com>
2025-08-14 15:17:55 +0530

    steplinerotrightview: adding SLRRNode class

--- steplinerotrightview/src/main/java/com/example/steplinerotrightview/StepLineRotRightView.kt
+++ steplinerotrightview/src/main/java/com/example/steplinerotrightview/StepLineRotRightView.kt
@@ -120,4 +120,45 @@ class StepLineRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLRRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLRRNode? = null
+        private var prev : SLRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLRRNode {
+            var curr : SLRRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
