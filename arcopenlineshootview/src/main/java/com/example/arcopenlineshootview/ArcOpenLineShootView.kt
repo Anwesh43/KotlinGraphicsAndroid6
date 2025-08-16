1772773cf532bd7091910bfd965dbbf02e80b1b3
Anwesh43 <anweshthecool0@gmail.com>
2025-08-16 12:35:34 +0530

    arcopenlineshootview: adding AOLSNode class

--- arcopenlineshootview/src/main/java/com/example/arcopenlineshootview/ArcOpenLineShootView.kt
+++ arcopenlineshootview/src/main/java/com/example/arcopenlineshootview/ArcOpenLineShootView.kt
@@ -122,4 +122,45 @@ class ArcOpenLineShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AOLSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AOLSNode? = null
+        private var prev : AOLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AOLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAOLSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AOLSNode {
+            var curr : AOLSNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
