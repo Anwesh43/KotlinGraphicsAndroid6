cb20afd5558a8c3432604dceced7da4c27e5b54d
Anwesh43 <anweshthecool0@gmail.com>
2025-08-18 09:10:27 +0530

    halfarcrotrightview: adding static create method

--- halfarcrotrightview/src/main/java/com/example/halfarcrotrightview/HalfArcRotRightView.kt
+++ halfarcrotrightview/src/main/java/com/example/halfarcrotrightview/HalfArcRotRightView.kt
@@ -212,4 +212,12 @@ class HalfArcRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : HalfArcRotRightView {
+            val view : HalfArcRotRightView = HalfArcRotRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
