206e1103f1c3f2c33e2869bdcc1425fed83f4430
Anwesh43 <anweshthecool0@gmail.com>
2025-08-18 09:20:22 +0530

    halfarcrotrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arcopenlineshootview.ArcOpenLineShootView
+import com.example.halfarcrotrightview.HalfArcRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcOpenLineShootView.create( this)
+        HalfArcRotRightView.create( this)
     }
 }
