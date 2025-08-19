0aa947b772d8ba7718ddf43fb208f1334696e11b
Anwesh43 <anweshthecool0@gmail.com>
2025-08-19 10:24:20 +0530

    bentlinetofshapeview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.halfarcrotrightview.HalfArcRotRightView
+import com.example.bentlinetofshapeview.BentLineToFShapeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HalfArcRotRightView.create( this)
+        BentLineToFShapeView.create( this)
     }
 }
