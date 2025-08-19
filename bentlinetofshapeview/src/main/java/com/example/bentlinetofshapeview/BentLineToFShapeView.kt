e86b91e06d98b117104b135c5a01e64ac5d96a65
Anwesh43 <anweshthecool0@gmail.com>
2025-08-19 09:48:17 +0530

    bentlinetofshapeview: adding Int and Float extension methods

--- bentlinetofshapeview/src/main/java/com/example/bentlinetofshapeview/BentLineToFShapeView.kt
+++ bentlinetofshapeview/src/main/java/com/example/bentlinetofshapeview/BentLineToFShapeView.kt
@@ -23,3 +23,7 @@ val rot : Float = 45f
 val lineSizeFactor : Float = 0.25f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
