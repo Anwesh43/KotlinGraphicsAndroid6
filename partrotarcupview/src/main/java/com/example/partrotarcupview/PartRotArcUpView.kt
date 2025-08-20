773a796acb4105be42609ede636bca855a84f8fc
Anwesh43 <anweshthecool0@gmail.com>
2025-08-20 10:19:43 +0530

    partrotarcupview: adding PRAUNode class

--- partrotarcupview/src/main/java/com/example/partrotarcupview/PartRotArcUpView.kt
+++ partrotarcupview/src/main/java/com/example/partrotarcupview/PartRotArcUpView.kt
@@ -117,4 +117,45 @@ class PartRotArcUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PRAUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : PRAUNode? = null
+        private var prev : PRAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PRAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPRAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PRAUNode {
+            var curr : PRAUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
