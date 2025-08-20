0595b89dff606ecfcb112b781d9707ede005d4ee
Anwesh43 <anweshthecool0@gmail.com>
2025-08-20 10:15:22 +0530

    partrotarcupview: adding PartRotArcUpView class

--- partrotarcupview/src/main/java/com/example/partrotarcupview/PartRotArcUpView.kt
+++ partrotarcupview/src/main/java/com/example/partrotarcupview/PartRotArcUpView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawPRAUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPartRotArcUp(scale, w, h, paint)
 }
+
+class PartRotArcUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
