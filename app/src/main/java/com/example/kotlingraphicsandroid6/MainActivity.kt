219390a10d8960b4625eef9c25189a5076949dde
Anwesh43 <anweshthecool0@gmail.com>
2025-08-20 10:36:57 +0530

    partrotarcupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bentlinetofshapeview.BentLineToFShapeView
+import com.example.partrotarcupview.PartRotArcUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineToFShapeView.create( this)
+        PartRotArcUpView.create( this)
     }
 }
