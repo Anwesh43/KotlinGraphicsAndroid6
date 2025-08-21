1dd246f5323917c65dfb7d06a85fc10c91f8d9ae
Anwesh43 <anweshthecool0@gmail.com>
2025-08-21 10:04:46 +0530

    rightlinebentmoveview: adding Canvas extension methods

--- rightlinebentmoveview/src/main/java/com/example/rightlinebentmoveview/RightLineBentRotView.kt
+++ rightlinebentmoveview/src/main/java/com/example/rightlinebentmoveview/RightLineBentRotView.kt
@@ -26,3 +26,34 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineBentRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(size - size * j - (w / 2) * dsc(4) * j, (h / 2) * dsc(4) * (1 - j)) {
+                rotate(rot * dsc(2 + j))
+                drawLine(0f, 0f, -size * dsc(j) * (1 - j), -size * dsc(j) * j, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRLBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineBentRot(scale, w, h, paint)
+}
