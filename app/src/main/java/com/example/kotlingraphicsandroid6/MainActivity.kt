dc83315c9c23eaca108c5764230a3fd4b887fa04
Anwesh43 <anweshthecool0@gmail.com>
2025-08-21 10:45:00 +0530

    rightlinebentmoveview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.partrotarcupview.PartRotArcUpView
+import com.example.rightlinebentmoveview.RightLineBentRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PartRotArcUpView.create( this)
+        RightLineBentRotView.create( this)
     }
 }
