cd15cfd81a4d59c1cf708bf83adc25c9ab748da0
Anwesh43 <anweshthecool0@gmail.com>
2025-08-23 15:29:56 +0530

    linevertjoinarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arcdownrotupview.ArcDownRotUpView
+import com.example.linevertjoinarcview.LineVertJoinArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcDownRotUpView.create( this)
+        LineVertJoinArcView.create( this)
     }
 }
