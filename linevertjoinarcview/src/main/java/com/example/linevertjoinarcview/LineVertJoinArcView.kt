64cc311b7616984c42d102a2e1e97770779d8b7e
Anwesh43 <anweshthecool0@gmail.com>
2025-08-23 15:24:59 +0530

    linevertjoinarcview: adding LVJANode class

--- linevertjoinarcview/src/main/java/com/example/linevertjoinarcview/LineVertJoinArcView.kt
+++ linevertjoinarcview/src/main/java/com/example/linevertjoinarcview/LineVertJoinArcView.kt
@@ -122,4 +122,45 @@ class LineVertJoinArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LVJANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LVJANode? = null
+        private var prev : LVJANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LVJANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLVJANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LVJANode {
+            var curr : LVJANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
