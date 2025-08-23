ad8ebf4efaa1855c1aaba3f4ffbdb6eb2c8321e8
Anwesh43 <anweshthecool0@gmail.com>
2025-08-23 15:28:11 +0530

    linevertjoinarcview: adding Renderer class

--- linevertjoinarcview/src/main/java/com/example/linevertjoinarcview/LineVertJoinArcView.kt
+++ linevertjoinarcview/src/main/java/com/example/linevertjoinarcview/LineVertJoinArcView.kt
@@ -186,4 +186,27 @@ class LineVertJoinArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineVertJoinArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lvja : LineVertJoinArc = LineVertJoinArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lvja.draw(canvas, paint)
+            animator.animate {
+                lvja.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lvja.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
