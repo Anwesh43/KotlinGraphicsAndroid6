1d840b8f6ae0bea0b1307ad50891605688522836
Anwesh43 <anweshthecool0@gmail.com>
2025-08-24 12:29:09 +0530

    arcuplinerotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linevertjoinarcview.LineVertJoinArcView
+import com.example.arcuplinerotview.ArcUpLineRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineVertJoinArcView.create( this)
+        ArcUpLineRotView.create( this)
     }
 }
