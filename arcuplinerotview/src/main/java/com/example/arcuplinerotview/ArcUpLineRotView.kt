de05900cbcba44a72fe0459748a59bcf6c5c0308
Anwesh43 <anweshthecool0@gmail.com>
2025-08-24 10:14:27 +0530

    arcuplinerotview: adding Canvas extension methods

--- arcuplinerotview/src/main/java/com/example/arcuplinerotview/ArcUpLineRotView.kt
+++ arcuplinerotview/src/main/java/com/example/arcuplinerotview/ArcUpLineRotView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val deg : Float = 180f
@@ -28,3 +28,40 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcUpLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 90f * dsc(0), false, paint)
+                drawXY(0f, -size / 2) {
+                    rotate(deg * dsc(2))
+                    drawLine(0f, 0f, 0f, -size * 0.5f * dsc(1), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawAULRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    drawArcUpLineRot(scale, w, h, paint)
+}
