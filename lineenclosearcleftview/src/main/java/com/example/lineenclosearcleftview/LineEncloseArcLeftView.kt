21bf6d59432916e8cc4074aeed09accb5b678f12
Anwesh43 <anweshthecool0@gmail.com>
2025-08-25 18:04:07 +0530

    lineenclosearcleftview: adding LineEncloseArcLeft class

--- lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
+++ lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
@@ -164,4 +164,27 @@ class LineEncloseArcLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineEncloseArcLeft(var i : Int) {
+
+        private var curr : LEALNode = LEALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
