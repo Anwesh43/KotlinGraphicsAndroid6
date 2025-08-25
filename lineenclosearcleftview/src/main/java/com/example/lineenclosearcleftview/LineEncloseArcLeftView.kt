f1a15b5562785b381b0dc90b7c681df3dd6d2e10
Anwesh43 <anweshthecool0@gmail.com>
2025-08-25 14:22:24 +0530

    lineenclosearcleftview: adding LineEncloseArcLeftView class

--- lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
+++ lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLEALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineEncloseArcLeft(scale, w, h, paint)
 }
+
+class LineEncloseArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
