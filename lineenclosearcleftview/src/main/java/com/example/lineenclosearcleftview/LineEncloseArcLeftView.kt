7169666d8167db28cda3718d36207f8eb10a04d8
Anwesh43 <anweshthecool0@gmail.com>
2025-08-25 18:02:43 +0530

    lineenclosearcleftview: adding LEALNode class

--- lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
+++ lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
@@ -123,4 +123,45 @@ class LineEncloseArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LEALNode? = null
+        private var prev : LEALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEALNode {
+            var curr : LEALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
