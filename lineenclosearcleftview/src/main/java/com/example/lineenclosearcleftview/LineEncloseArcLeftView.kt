52e41182ee6566f817d8dc04bcff55511dd984d0
Anwesh43 <anweshthecool0@gmail.com>
2025-08-25 10:32:07 +0530

    lineenclosearcleftview: adding Canvas extension methods

--- lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
+++ lineenclosearcleftview/src/main/java/com/example/lineenclosearcleftview/LineEncloseArcLeftView.kt
@@ -27,3 +27,36 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineEncloseArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(-size * j, 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(2))
+                drawLine(0f, 0f, 0f, -size * 0.5f * dsc(1), paint)
+            }
+        }
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f, 180f * dsc(0), false, paint)
+    }
+}
+
+fun Canvas.drawLEALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineEncloseArcLeft(scale, w, h, paint)
+}
