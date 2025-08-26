0a7c0b4af0c8379b9d109d67f0669a8af281a4ae
Anwesh43 <anweshthecool0@gmail.com>
2025-08-26 12:05:37 +0530

    joinlineperpleftview: adding Renderer class

--- joinlineperpleftview/src/main/java/com/example/joinlineperpleftview/JoinLinePerpLeftView.kt
+++ joinlineperpleftview/src/main/java/com/example/joinlineperpleftview/JoinLinePerpLeftView.kt
@@ -185,4 +185,27 @@ class JoinLinePerpLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : JoinLinePerpLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val jlpl : JoinLinePerpLeft = JoinLinePerpLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            jlpl.draw(canvas, paint)
+            animator.animate {
+                jlpl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            jlpl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
