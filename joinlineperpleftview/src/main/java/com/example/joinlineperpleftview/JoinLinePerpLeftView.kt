b7f4b0deed9175cf69856043682c28ff87869e5e
Anwesh43 <anweshthecool0@gmail.com>
2025-08-26 12:02:17 +0530

    joinlineperpleftview: adding JLPLNode class

--- joinlineperpleftview/src/main/java/com/example/joinlineperpleftview/JoinLinePerpLeftView.kt
+++ joinlineperpleftview/src/main/java/com/example/joinlineperpleftview/JoinLinePerpLeftView.kt
@@ -121,4 +121,45 @@ class JoinLinePerpLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JLPLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : JLPLNode? = null
+        private var prev : JLPLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JLPLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJLPLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JLPLNode {
+            var curr : JLPLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
