951558a3e95b9ff294ee1f20395669a6a62b6455
Anwesh43 <anweshthecool0@gmail.com>
2025-08-27 08:16:58 +0530

    linejoinarcdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.joinlineperpleftview.JoinLinePerpLeftView
+import com.example.linejoinarcdownview.LineJoinArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        JoinLinePerpLeftView.create( this)
+        LineJoinArcDownView.create( this)
     }
 }
