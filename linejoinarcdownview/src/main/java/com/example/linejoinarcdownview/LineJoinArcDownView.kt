8694bc8e18141436aa2e7c0216f67ba2ce88337d
Anwesh43 <anweshthecool0@gmail.com>
2025-08-27 08:13:04 +0530

    linejoinarcdownview: adding LineJoinArcDown class

--- linejoinarcdownview/src/main/java/com/example/linejoinarcdownview/LineJoinArcDownView.kt
+++ linejoinarcdownview/src/main/java/com/example/linejoinarcdownview/LineJoinArcDownView.kt
@@ -166,4 +166,27 @@ class LineJoinArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineJoinArcDown(var i : Int) {
+
+        private var curr : LJADNode = LJADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
