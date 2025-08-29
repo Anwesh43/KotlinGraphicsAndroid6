c99ad7c932c3f9676bc0aeb780b33b5ade17fb9f
Anwesh43 <anweshthecool0@gmail.com>
2025-08-30 01:11:14 +0530

    crossbentperplineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linejoinarcdownview.LineJoinArcDownView
+import com.example.crossbentperplineview.CrossBentPerpLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineJoinArcDownView.create( this)
+        CrossBentPerpLineView.create( this)
     }
 }
