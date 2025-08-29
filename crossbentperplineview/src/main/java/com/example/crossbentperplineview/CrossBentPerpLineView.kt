637297a6310905da86316e707aa41e5dd553e420
Anwesh43 <anweshthecool0@gmail.com>
2025-08-30 01:07:37 +0530

    crossbentperplineview: adding Renderer class

--- crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
+++ crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
@@ -187,4 +187,27 @@ class CrossBentPerpLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CrossBentPerpLineView) {
+
+        private val cbpl : CrossBentPerpLine = CrossBentPerpLine(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            cbpl.draw(canvas, paint)
+            animator.animate {
+                cbpl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            cbpl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
