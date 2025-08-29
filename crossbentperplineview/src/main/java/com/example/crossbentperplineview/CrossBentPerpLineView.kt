0c8369522f59b76d0573738f99250667eb60da24
Anwesh43 <anweshthecool0@gmail.com>
2025-08-30 00:44:15 +0530

    crossbentperplineview: adding Canvas extension methods

--- crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
+++ crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
@@ -22,8 +22,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 5.9f
+val rot : Float = 135f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCrossBentPerpLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(-size + size * dsc(2), 0f) {
+                rotate(-rot * (1f - 2 * j) * (1f - dsc(1)))
+                drawLine(0f, size * (1 - j) * (1 - dsc(0).divideScale(j, 2)), 0f, size * j * dsc(0).divideScale(j, 2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawCBPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawCrossBentPerpLine(scale, w, h, paint)
+}
