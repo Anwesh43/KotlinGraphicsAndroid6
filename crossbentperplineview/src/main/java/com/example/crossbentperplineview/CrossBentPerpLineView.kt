96345fa4722a657d8443002d82ea48ad26f1cbd4
Anwesh43 <anweshthecool0@gmail.com>
2025-08-30 00:58:56 +0530

    crossbentperplineview: adding CBPLNode class

--- crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
+++ crossbentperplineview/src/main/java/com/example/crossbentperplineview/CrossBentPerpLineView.kt
@@ -122,4 +122,45 @@ class CrossBentPerpLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CBPLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CBPLNode? = null
+        private var prev : CBPLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CBPLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCBPLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CBPLNode {
+            var curr : CBPLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
