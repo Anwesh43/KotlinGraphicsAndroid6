ceba5b9e4a923c555d2ca6624463c4f226b77224
Anwesh43 <anweshthecool0@gmail.com>
2025-09-01 02:04:32 +0530

    bentlinearcleftview: adding Renderer class

--- bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
+++ bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
@@ -189,4 +189,27 @@ class BentLineArcLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BentLineArcLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val blal : BentLineArcLeft = BentLineArcLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blal.draw(canvas, paint)
+            animator.animate {
+                blal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
