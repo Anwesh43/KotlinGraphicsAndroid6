79ada39a1a89f1d222cba933d6ef22587d171e06
Anwesh43 <anweshthecool0@gmail.com>
2025-09-01 02:11:58 +0530

    bentlinearcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arcbisectlinedownview.ArcBisectLineDownView
+import com.example.bentlinearcleftvew.BentLineArcLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcBisectLineDownView.create( this)
+        BentLineArcLeftView.create( this)
     }
 }
