106c4a9b1193343c6ab2fba75527a9634899a575
Anwesh43 <anweshthecool0@gmail.com>
2025-09-01 01:51:24 +0530

    bentlinearcleftview: adding BentLineArcLeftView class

--- bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
+++ bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBLALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentLineArcLeft(scale, w, h, paint)
 }
+
+class BentLineArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
