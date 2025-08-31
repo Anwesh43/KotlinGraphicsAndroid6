fda8e76e2d98400f3dc96b82b97b12cfef8a3a34
Anwesh43 <anweshthecool0@gmail.com>
2025-08-31 06:38:38 +0530

    arcbisectlinedownview: adding ABLDNode class

--- arcbisectlinedownview/src/main/java/com/example/arcbisectlinedownview/ArcBisectLineDownView.kt
+++ arcbisectlinedownview/src/main/java/com/example/arcbisectlinedownview/ArcBisectLineDownView.kt
@@ -125,4 +125,45 @@ class ArcBisectLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ABLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ABLDNode? = null
+        private var prev : ABLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ABLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawABLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ABLDNode {
+            var curr : ABLDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
