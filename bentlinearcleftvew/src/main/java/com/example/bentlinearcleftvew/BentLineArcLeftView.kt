20f100a2396cd37c4ddb90465dc953d7adf78b89
Anwesh43 <anweshthecool0@gmail.com>
2025-09-01 01:50:25 +0530

    bentlinearcleftview: adding Canvas extension methods

--- bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
+++ bentlinearcleftvew/src/main/java/com/example/bentlinearcleftvew/BentLineArcLeftView.kt
@@ -27,3 +27,38 @@ val sizeFactor : Float = 5.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentLineArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(-size, -size) {
+            drawLine(0f, 0f, size * dsc(0), size * dsc(0), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, 0f, -size * dsc(3), paint)
+        }
+    }
+}
+
+fun Canvas.drawBLALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentLineArcLeft(scale, w, h, paint)
+}
