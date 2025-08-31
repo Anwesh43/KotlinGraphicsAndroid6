cc0438d7e95f3564b39f1571379e2e0a7917c1af
Anwesh43 <anweshthecool0@gmail.com>
2025-08-31 06:20:10 +0530

    arcbisectlinedownview: adding Canvas extension methods

--- arcbisectlinedownview/src/main/java/com/example/arcbisectlinedownview/ArcBisectLineDownView.kt
+++ arcbisectlinedownview/src/main/java/com/example/arcbisectlinedownview/ArcBisectLineDownView.kt
@@ -23,7 +23,42 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 45f
-
+val arcSizeFactor : Float = 6f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcBisectLineDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val arcSize : Float = size / arcSizeFactor
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        drawArc(RectF(-arcSize / 2, 0f, arcSize / 2, arcSize), -90f, 360f * dsc(0), false, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(2))
+                drawXY(0f, arcSize) {
+                    drawLine(0f, 0f, 0f, (size - arcSize) * dsc(1), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawABLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcBisectLineDown(scale, w, h, paint)
+}
