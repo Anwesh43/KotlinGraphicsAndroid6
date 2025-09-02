9f2f1f76df7898dec1c8331247a7cb32612a9654
Anwesh43 <anweshthecool0@gmail.com>
2025-09-02 07:34:43 +0530

    trigaplineseparateview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bentlinearcleftvew.BentLineArcLeftView
+import com.example.trigaplineseparateview.TriGapLineSeparateView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineArcLeftView.create( this)
+        TriGapLineSeparateView.create( this)
     }
 }
