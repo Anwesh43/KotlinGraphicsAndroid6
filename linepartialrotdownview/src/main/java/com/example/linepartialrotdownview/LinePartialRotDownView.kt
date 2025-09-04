52510b3381919d000f5bd2edc80409cbb3e762d3
Anwesh43 <anweshthecool0@gmail.com>
2025-09-05 02:02:35 +0530

    linepartilarotdownview: adding LinePartialRotDown class

--- linepartialrotdownview/src/main/java/com/example/linepartialrotdownview/LinePartialRotDownView.kt
+++ linepartialrotdownview/src/main/java/com/example/linepartialrotdownview/LinePartialRotDownView.kt
@@ -164,4 +164,26 @@ class LinePartialRotDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinePartialRotDown(var i : Int) {
+
+        private var curr : LPRDNode = LPRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
