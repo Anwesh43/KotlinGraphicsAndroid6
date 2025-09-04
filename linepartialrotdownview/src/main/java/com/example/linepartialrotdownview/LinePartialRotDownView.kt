d13aec13cf966b98a3f7233ed2aa2407fd3e165d
Anwesh43 <anweshthecool0@gmail.com>
2025-09-05 01:47:58 +0530

    linepartilarotdownview: adding LinePartialRotDownView class

--- linepartialrotdownview/src/main/java/com/example/linepartialrotdownview/LinePartialRotDownView.kt
+++ linepartialrotdownview/src/main/java/com/example/linepartialrotdownview/LinePartialRotDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLPRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinePartialRotDown(scale, w, h, paint)
 }
+
+class LinePartialRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
