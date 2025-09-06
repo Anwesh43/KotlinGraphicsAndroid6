fcf6b8e4c63f35b863dbdc5465501d73366e53ba
Anwesh43 <anweshthecool0@gmail.com>
2025-09-06 08:38:46 +0530

    linebentdropperpview: LBDPNode class

--- linebentdropperpview/src/main/java/com/example/linebentdropperpview/LineBentDropPerpView.kt
+++ linebentdropperpview/src/main/java/com/example/linebentdropperpview/LineBentDropPerpView.kt
@@ -124,4 +124,45 @@ class LineBentDropPerpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBDPNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBDPNode? = null
+        private var prev : LBDPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBDPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBDPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBDPNode {
+            var curr : LBDPNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
