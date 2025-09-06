f01fcec7df652b889851b60a91eb04f1ba699bbe
Anwesh43 <anweshthecool0@gmail.com>
2025-09-06 08:22:03 +0530

    linebentdropperpview: adding LineBentDropPerpView class

--- linebentdropperpview/src/main/java/com/example/linebentdropperpview/LineBentDropPerpView.kt
+++ linebentdropperpview/src/main/java/com/example/linebentdropperpview/LineBentDropPerpView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLBDPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentDropPerp(scale, w, h, paint)
 }
+
+class LineBentDropPerpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
