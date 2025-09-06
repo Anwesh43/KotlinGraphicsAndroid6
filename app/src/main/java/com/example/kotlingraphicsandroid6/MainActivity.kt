d5fe55f247afb0b57b39468071d4782bbefb388a
Anwesh43 <anweshthecool0@gmail.com>
2025-09-06 08:56:08 +0530

    linebentdropperpview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linepartialrotdownview.LinePartialRotDownView
+import com.example.linebentdropperpview.LineBentDropPerpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinePartialRotDownView.create( this)
+        LineBentDropPerpView.create( this)
     }
 }
