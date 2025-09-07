b3695c2ed449195ae88afea0eb70174439a8b7a5
Anwesh43 <anweshthecool0@gmail.com>
2025-09-07 11:57:27 +0530

    arclidopenfillview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentdropperpview.LineBentDropPerpView
+import com.example.arclidopenfillview.ArcLidOpenFillView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentDropPerpView.create( this)
+        ArcLidOpenFillView.create( this)
     }
 }
