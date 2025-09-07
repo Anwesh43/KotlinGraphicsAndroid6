52b989cf7ad44bdc8cd9adfe8d4a7770f3e6f50b
Anwesh43 <anweshthecool0@gmail.com>
2025-09-07 11:43:37 +0530

    arclidopenfillview: adding Animaotr class

--- arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
+++ arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
@@ -136,4 +136,32 @@ class ArcLidOpenFillView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
