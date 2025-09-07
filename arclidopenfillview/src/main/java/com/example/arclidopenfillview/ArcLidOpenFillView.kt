f2d19d480dd6c283ff12fe18e627544cc30bd67b
Anwesh43 <anweshthecool0@gmail.com>
2025-09-07 11:34:11 +0530

    arclidopenfillview: adding ArcLideOpenFillView class

--- arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
+++ arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawALOFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcLidOpenFill(scale, w, h, paint)
 }
+
+class ArcLidOpenFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
