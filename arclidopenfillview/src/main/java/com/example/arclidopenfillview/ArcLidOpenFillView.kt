86a390f773c6e9ed202c0b9f2826568dca8bfa68
Anwesh43 <anweshthecool0@gmail.com>
2025-09-07 11:39:59 +0530

    arclidopenfillview: adding ALOFNode class

--- arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
+++ arclidopenfillview/src/main/java/com/example/arclidopenfillview/ArcLidOpenFillView.kt
@@ -95,4 +95,45 @@ class ArcLidOpenFillView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALOFNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : ALOFNode? = null
+        private var next : ALOFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALOFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALOFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALOFNode {
+            var curr : ALOFNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
