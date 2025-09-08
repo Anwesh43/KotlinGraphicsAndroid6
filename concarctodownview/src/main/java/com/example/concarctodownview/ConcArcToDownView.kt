811a76176da6d96adb1cdfffe9e653859a7f0a6f
Anwesh43 <anweshthecool0@gmail.com>
2025-09-08 12:34:39 +0530

    concarcrotdownview: adding ConcArcToDownView class

--- concarctodownview/src/main/java/com/example/concarctodownview/ConcArcToDownView.kt
+++ concarctodownview/src/main/java/com/example/concarctodownview/ConcArcToDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawCATDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawConcArcToDown(scale, w, h, paint)
 }
+
+class ConcArcToDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
