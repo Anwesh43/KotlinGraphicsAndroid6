de839d57535e47eaa89f00dc8fe453eda4665568
Anwesh43 <anweshthecool0@gmail.com>
2025-09-08 12:52:32 +0530

    concarcrotdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arclidopenfillview.ArcLidOpenFillView
+import com.example.concarctodownview.ConcArcToDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcLidOpenFillView.create( this)
+        ConcArcToDownView.create( this)
     }
 }
