3e18fdbee558bffa0983e9f5ff4ddfc150fc277e
Anwesh43 <anweshthecool0@gmail.com>
2025-09-10 02:04:53 +0530

    rightboxarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.partarcrotlineview.PartArcRotLineView
+import com.example.rightboxarcview.RightBoxArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PartArcRotLineView.create( this)
+        RightBoxArcView.create( this)
     }
 }
