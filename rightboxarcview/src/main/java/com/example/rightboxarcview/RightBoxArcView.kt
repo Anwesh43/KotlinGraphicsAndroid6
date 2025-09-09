f85b6836d71b8d667662e27592927cec53cc276c
Anwesh43 <anweshthecool0@gmail.com>
2025-09-10 01:52:20 +0530

    rightboxarcview: adding RightBoxArcView class

--- rightboxarcview/src/main/java/com/example/rightboxarcview/RightBoxArcView.kt
+++ rightboxarcview/src/main/java/com/example/rightboxarcview/RightBoxArcView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawRBANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRightBoxArc(scale, w, h, paint)
 }
+
+class RightBoxArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
