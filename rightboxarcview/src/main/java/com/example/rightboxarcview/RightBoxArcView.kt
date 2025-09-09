982adb242b958c6ec5e2d0f0e8ed9802ac766a7e
Anwesh43 <anweshthecool0@gmail.com>
2025-09-10 01:51:14 +0530

    rightboxarcview: adding Canvas extension methods

--- rightboxarcview/src/main/java/com/example/rightboxarcview/RightBoxArcView.kt
+++ rightboxarcview/src/main/java/com/example/rightboxarcview/RightBoxArcView.kt
@@ -27,3 +27,40 @@ val sizeFactor : Float = 5.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightBoxArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        drawArc(RectF(-size / 2, -size, size / 2, 0f), 180f, 180f * dsc(0), false, paint)
+        drawXY(size / 2, -size / 2) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+            drawXY(size / 2, 0f) {
+                drawLine(0f, 0f, -size * dsc(2), 0f, paint)
+            }
+            drawXY(-size / 2, 0f) {
+                drawLine(0f, 0f, 0f, -size * 0.5f * dsc(3), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRBANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRightBoxArc(scale, w, h, paint)
+}
