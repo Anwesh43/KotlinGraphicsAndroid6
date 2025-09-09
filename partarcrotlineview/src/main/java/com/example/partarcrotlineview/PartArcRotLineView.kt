7e7195e3abcb6a8ad383f10aee9862542df2f9a5
Anwesh43 <anweshthecool0@gmail.com>
2025-09-09 08:20:13 +0530

    partarcrotlineview: adding PartArcRotLineView class

--- partarcrotlineview/src/main/java/com/example/partarcrotlineview/PartArcRotLineView.kt
+++ partarcrotlineview/src/main/java/com/example/partarcrotlineview/PartArcRotLineView.kt
@@ -63,3 +63,18 @@ fun Canvas.drawPARLNode(i : Int, scale : Float, paint : Paint) {
     drawPartArcRotLine(scale, w, h, paint)
 }
 
+class PartArcRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
