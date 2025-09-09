e405b73876620b4fdac06c6fcc2ddfe21b79bea5
Anwesh43 <anweshthecool0@gmail.com>
2025-09-09 08:19:17 +0530

    partarcrotlineview: adding Canvas extension methods

--- partarcrotlineview/src/main/java/com/example/partarcrotlineview/PartArcRotLineView.kt
+++ partarcrotlineview/src/main/java/com/example/partarcrotlineview/PartArcRotLineView.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
 import androidx.core.graphics.toColorInt
+import android.graphics.RectF
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -15,14 +16,50 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val rot : Float = 90f
+val rot : Float = 45f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
 val parts : Int = 6
 val scGap : Float = 0.05f / parts
+val deg : Float = 180f
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPartArcRotLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawXY(0f, 0f) {
+            rotate(deg * dsc(2))
+            drawLine(-size * (1 - dsc(0)), 0f, -size, 0f, paint)
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        drawArc(RectF(-size, -size, size, size), 0f, 90f * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawPARLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawPartArcRotLine(scale, w, h, paint)
+}
+
