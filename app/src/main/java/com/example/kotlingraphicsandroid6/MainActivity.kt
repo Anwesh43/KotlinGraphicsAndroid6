7c9f1cb09730b58e09948051f17d51bc68983da4
Anwesh43 <anweshthecool0@gmail.com>
2025-09-09 08:53:02 +0530

    partarcrotlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.concarctodownview.ConcArcToDownView
+import com.example.partarcrotlineview.PartArcRotLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ConcArcToDownView.create( this)
+        PartArcRotLineView.create( this)
     }
 }
