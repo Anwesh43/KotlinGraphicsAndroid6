6c2016472fbaf36b78ff0ffd3a3f105fdb37d1d8
Anwesh43 <anweshthecool0@gmail.com>
2025-09-11 00:51:41 +0530

    lineconcarcrightview: adding LineConcArcRightView class

--- lineconcarcrightview/src/main/java/com/example/lineconcarcrightview/LineConcArcRightView.kt
+++ lineconcarcrightview/src/main/java/com/example/lineconcarcrightview/LineConcArcRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLCARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineConcArcRight(scale, w, h, paint)
 }
+
+class LineConcArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
