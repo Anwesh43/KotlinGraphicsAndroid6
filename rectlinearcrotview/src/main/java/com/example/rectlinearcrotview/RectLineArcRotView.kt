90627144c7d83d129254c15cada6c1a7ecb2efda
Anwesh43 <anweshthecool0@gmail.com>
2025-09-12 13:18:18 +0530

    rectlinearcrotview: adding Renderer class

--- rectlinearcrotview/src/main/java/com/example/rectlinearcrotview/RectLineArcRotView.kt
+++ rectlinearcrotview/src/main/java/com/example/rectlinearcrotview/RectLineArcRotView.kt
@@ -190,4 +190,27 @@ class RectLineArcRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RectLineArcRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val rlar : RectLineArcRot = RectLineArcRot(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rlar.draw(canvas, paint)
+            animator.animate {
+                rlar.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rlar.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
