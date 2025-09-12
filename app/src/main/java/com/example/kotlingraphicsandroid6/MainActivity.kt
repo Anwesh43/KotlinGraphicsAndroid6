ab666ffa4366fdc240b653ccf3e0aeab0bc0552e
Anwesh43 <anweshthecool0@gmail.com>
2025-09-12 13:24:01 +0530

    rectlinearcrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.lineconcarcrightview.LineConcArcRightView
+import com.example.rectlinearcrotview.RectLineArcRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineConcArcRightView.create( this)
+        RectLineArcRotView.create( this)
     }
 }
