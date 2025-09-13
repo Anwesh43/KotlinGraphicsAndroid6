6052ab65f6714b49ad5d97ec34aded5a9724e2e9
Anwesh43 <anweshthecool0@gmail.com>
2025-09-13 11:13:03 +0530

    arccloserotdownview: adding ArcCloseRotDownView class

--- arccloserotdownview/src/main/java/com/example/arccloserotdownview/ArcCloseRotDownView.kt
+++ arccloserotdownview/src/main/java/com/example/arccloserotdownview/ArcCloseRotDownView.kt
@@ -62,3 +62,18 @@ fun Canvas.drawACRDNode(i : Int, scale : Float, paint : Paint) {
     drawArcCloseRotDown(scale, w, h, paint)
 }
 
+class ArcCloseRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
