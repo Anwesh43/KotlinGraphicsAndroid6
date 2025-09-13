d1097901b331b488501e3588a45f14bce4ebad01
Anwesh43 <anweshthecool0@gmail.com>
2025-09-13 11:53:36 +0530

    arccloserotdownview: adding view in actvity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.rectlinearcrotview.RectLineArcRotView
+import com.example.arccloserotdownview.ArcCloseRotDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RectLineArcRotView.create( this)
+        ArcCloseRotDownView.create( this)
     }
 }
