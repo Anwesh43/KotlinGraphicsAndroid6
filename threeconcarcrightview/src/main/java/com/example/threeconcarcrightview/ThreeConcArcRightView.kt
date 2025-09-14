94f898c4266e4420db158bb5609e5dd9806bbf03
Anwesh43 <anweshthecool0@gmail.com>
2025-09-14 07:49:06 +0530

    threeconcarcrightview: adidng Canvas extension methods

--- threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
+++ threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
@@ -29,3 +29,36 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawThreeConcArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(arcs + 1), h / 2) {
+        for (j in 1..arcs) {
+            val upSize : Float = (size * j) / arcs
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(j - 1).divideScale(1, 2))
+                drawArc(RectF(-upSize / 2, -upSize, upSize / 2, upSize), 90f, sweep * (dsc(j - 1).divideScale(0, 2) + dsc(arcs)), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTCARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawThreeConcArcRight(scale, w, h, paint)
+}
