7d69f413324e54957e74b08f3dd4d1be524ecf32
Anwesh43 <anweshthecool0@gmail.com>
2025-09-14 07:56:12 +0530

    threeconcarcrightview: adidng ThreeConcArcRightNow class

--- threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
+++ threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawTCARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawThreeConcArcRight(scale, w, h, paint)
 }
+
+class ThreeConcArcRightNow(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
