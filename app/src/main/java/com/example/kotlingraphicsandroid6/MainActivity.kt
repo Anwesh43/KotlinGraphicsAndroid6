bf275e8464076cbae94ce69c7aa0f31f8da99837
Anwesh43 <anweshthecool0@gmail.com>
2025-09-14 08:50:43 +0530

    threeconcarcrightview: adidng view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arccloserotdownview.ArcCloseRotDownView
+import com.example.threeconcarcrightview.ThreeConcArcRightNow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcCloseRotDownView.create( this)
+        ThreeConcArcRightNow.create( this)
     }
 }
