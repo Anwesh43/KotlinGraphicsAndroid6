8bbbd0bd34285ade37bf81ada610c064f0faa606
Anwesh43 <anweshthecool0@gmail.com>
2025-09-14 08:42:19 +0530

    threeconcarcrightview: adidng TCARNode class

--- threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
+++ threeconcarcrightview/src/main/java/com/example/threeconcarcrightview/ThreeConcArcRightView.kt
@@ -125,4 +125,45 @@ class ThreeConcArcRightNow(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TCARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TCARNode? = null
+        private var prev : TCARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TCARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTCARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TCARNode {
+            var curr : TCARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
