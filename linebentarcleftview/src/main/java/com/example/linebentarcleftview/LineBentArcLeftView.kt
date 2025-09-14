cd5dcab221f789f0fd556b0b84ca5ec20f29f103
Anwesh43 <anweshthecool0@gmail.com>
2025-09-15 00:56:26 +0530

    linebentarcleftview: adding LineBentArcLeftView class

--- linebentarcleftview/src/main/java/com/example/linebentarcleftview/LineBentArcLeftView.kt
+++ linebentarcleftview/src/main/java/com/example/linebentarcleftview/LineBentArcLeftView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLBALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcLeft(scale, w, h, paint)
 }
+
+class LineBentArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
