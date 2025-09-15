dddabcdd56f798231b24a106e310a94de648ae10
Anwesh43 <anweshthecool0@gmail.com>
2025-09-16 01:41:51 +0530

    kayshapeshiftlineview: adding KayShapeLienView class

--- kayshapeshiftlineview/src/main/java/com/example/kayshapeshiftlineview/KayShapeShiftLineView.kt
+++ kayshapeshiftlineview/src/main/java/com/example/kayshapeshiftlineview/KayShapeShiftLineView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawKSSLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     drawKayShapeShift(scale, w, h, paint)
 }
+
+class KayShapeShiftLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
