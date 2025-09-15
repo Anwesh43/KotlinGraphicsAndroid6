e1fabce2fdc1db8fa3d0bd9f42627d343bfbcad7
Anwesh43 <anweshthecool0@gmail.com>
2025-09-16 01:40:37 +0530

    kayshapeshiftlineview: adding Canvas extension methods

--- kayshapeshiftlineview/src/main/java/com/example/kayshapeshiftlineview/KayShapeShiftLineView.kt
+++ kayshapeshiftlineview/src/main/java/com/example/kayshapeshiftlineview/KayShapeShiftLineView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
+val parts : Int = 6
 val scGap : Float = 0.04f / parts
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
@@ -29,3 +29,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawKayShapeShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(size * dsc(2) * j, 0f) {
+                for (k in 0..1) {
+                    drawXY(0f, -h * 0.5f * dsc(5) * (1 - 2 * k)) {
+                        rotate(deg * k * (1 - dsc(3)))
+                        drawLine(0f, 0f, 0f, -size * 0.5f * dsc(j), paint)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawKSSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = colors[i].toColorInt()
+    drawKayShapeShift(scale, w, h, paint)
+}
