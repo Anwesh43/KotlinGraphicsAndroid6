c7e29a9e234e07b23cfaa15464d037bed5707c06
Anwesh43 <anweshthecool0@gmail.com>
2025-09-16 07:46:59 +0530

    kayshapeshiftlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentarcleftview.LineBentArcLeftView
+import com.example.kayshapeshiftlineview.KayShapeShiftLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentArcLeftView.create( this)
+        KayShapeShiftLineView.create( this)
     }
 }
