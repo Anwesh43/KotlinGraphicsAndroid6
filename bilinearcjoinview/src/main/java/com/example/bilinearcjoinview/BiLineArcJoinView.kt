86d7855d406877b15fdf05b1d57afcd9ef85a22f
Anwesh43 <anweshthecool0@gmail.com>
2025-09-17 12:17:47 +0530

    bilibearcjoinview: adding static create method

--- bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
+++ bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
@@ -215,4 +215,13 @@ class BiLineArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : BiLineArcJoinView {
+            val view : BiLineArcJoinView = BiLineArcJoinView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
