f8abac6945d5850d2e437ee0a1de55bf2273a247
Anwesh43 <anweshthecool0@gmail.com>
2025-09-17 12:12:44 +0530

    bilibearcjoinview: adding BLAJNode class

--- bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
+++ bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
@@ -126,4 +126,45 @@ class BiLineArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLAJNode? = null
+        private var prev : BLAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLAJNode {
+            var curr : BLAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
