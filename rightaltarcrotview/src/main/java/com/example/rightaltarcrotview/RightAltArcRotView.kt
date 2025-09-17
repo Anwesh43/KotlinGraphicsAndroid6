86dbd2073c61bd5737b19c2e8deafce01a6d6033
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 01:28:43 +0530

    rightaltarcrotview: adding Canvas extension methods

--- rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
+++ rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
@@ -28,3 +28,38 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightAltArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+                    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), sweep, sweep * (dsc(0) - dsc(2) * j), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawRAARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRightAltArcRot(scale, w, h, paint)
+}
