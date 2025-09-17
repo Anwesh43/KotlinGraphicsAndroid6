3a92cbf6afc9f4b3c8eaa5c7768415c0c16de82a
Anwesh43 <anweshthecool0@gmail.com>
2025-09-17 12:02:53 +0530

    bilibearcjoinview: adding Canvas extension methods

--- bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
+++ bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
@@ -27,3 +27,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        for (k in 0..1) {
+            for (j in 0..1) {
+                val r : Float = size * 0.5f * (k + 1)
+                drawXY(0f, 0f) {
+                    scale(1f - 2 * j, 1f)
+                    drawArc(RectF(-r, -r, r, r), 180f, 90f * dsc(2 * j), false, paint)
+                }
+            }
+        }
+        drawLine(0f, -size / 2, 0f, -size / 2 - size * 0.5f * dsc(2), paint)
+    }
+}
+
+fun Canvas.drawBLAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiLineArcJoin(scale, w, h, paint)
+}
