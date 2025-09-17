25e641e2951c836508a81f7af2a3fdd64651ae9e
Anwesh43 <anweshthecool0@gmail.com>
2025-09-17 12:04:04 +0530

    bilibearcjoinview: adding BiLineArcJoinView class

--- bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
+++ bilinearcjoinview/src/main/java/com/example/bilinearcjoinview/BiLineArcJoinView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBLAJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiLineArcJoin(scale, w, h, paint)
 }
+
+class BiLineArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
