7d81fc25f63172c7fc30f468962579643444fd21
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 08:51:27 +0530

    rightaltarcrotview: adding RightAltArcRot class

--- rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
+++ rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
@@ -167,4 +167,27 @@ class RightAltArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightAltArcRot(var i : Int = 0, val state : State = State()) {
+
+        private var curr : RAARNode = RAARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
