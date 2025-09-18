eebdc878cac3ca3b92cc063017956338ab5577e0
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 08:45:34 +0530

    rightaltarcrotview: adding RAARNode class

--- rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
+++ rightaltarcrotview/src/main/java/com/example/rightaltarcrotview/RightAltArcRotView.kt
@@ -126,4 +126,45 @@ class RightAltArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RAARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RAARNode? = null
+        private var prev : RAARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RAARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRAARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RAARNode {
+            var curr : RAARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
