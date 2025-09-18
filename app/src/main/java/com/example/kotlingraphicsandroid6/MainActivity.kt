fd7b4f5788a3d9e0b657bb0d0d5f276f9c798091
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 09:23:42 +0530

    rightaltarcrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bilinearcjoinview.BiLineArcJoinView
+import com.example.rightaltarcrotview.RightAltArcRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiLineArcJoinView.create( this)
+        RightAltArcRotView.create( this)
     }
 }
