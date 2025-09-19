b3f4ad5a37ed2c521986046125c5f9a25cb8cb65
Anwesh43 <anweshthecool0@gmail.com>
2025-09-19 09:33:31 +0530

    ellipselinebreakview: adding EllipseLineBreakView class

--- ellipselinebreakview/src/main/java/com/example/ellipselinebreakview/EllipseLineBreakView.kt
+++ ellipselinebreakview/src/main/java/com/example/ellipselinebreakview/EllipseLineBreakView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawELBNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawEllipseLineBreak(scale, w, h, paint)
 }
+
+class EllipseLineBreakView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
