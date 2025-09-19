1d993d8546fbbb38808791ccd9fe531dc292a6ed
Anwesh43 <anweshthecool0@gmail.com>
2025-09-19 10:21:21 +0530

    ellipselinebreakview: adding ELBNode class

--- ellipselinebreakview/src/main/java/com/example/ellipselinebreakview/EllipseLineBreakView.kt
+++ ellipselinebreakview/src/main/java/com/example/ellipselinebreakview/EllipseLineBreakView.kt
@@ -128,4 +128,45 @@ class EllipseLineBreakView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELBNode(var i : Int= 0, val state : State = State()) {
+
+        private var next : ELBNode? = null
+        private var prev : ELBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ELBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELBNode {
+            var curr : ELBNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
