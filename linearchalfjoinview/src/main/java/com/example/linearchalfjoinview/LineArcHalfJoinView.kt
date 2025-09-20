79eaa7a2750801d2075809fd50dcb971977c0d57
Anwesh43 <anweshthecool0@gmail.com>
2025-09-20 10:18:32 +0530

    linearchalfjoinview: adding Canvas extension methods

--- linearchalfjoinview/src/main/java/com/example/linearchalfjoinview/LineArcHalfJoinView.kt
+++ linearchalfjoinview/src/main/java/com/example/linearchalfjoinview/LineArcHalfJoinView.kt
@@ -27,3 +27,39 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(),  maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcHalfJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        rotate(-rot * dsc(2))
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(1))
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -rot * dsc(1), rot * (dsc(1) + dsc(3)), false, paint)
+        drawXY(0f, 0f) {
+            rotate(-rot)
+            drawLine(size * (1 - dsc(4)), 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLAHJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineArcHalfJoin(scale, w, h, paint)
+}
