1475c854a78183563cee3d0a72ffbfc6afeaf54a
Anwesh43 <anweshthecool0@gmail.com>
2025-09-20 10:36:12 +0530

    linearchalfjoinview: adding Renderer class

--- linearchalfjoinview/src/main/java/com/example/linearchalfjoinview/LineArcHalfJoinView.kt
+++ linearchalfjoinview/src/main/java/com/example/linearchalfjoinview/LineArcHalfJoinView.kt
@@ -190,4 +190,27 @@ class LineArcHalfJoinView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcHalfJoinView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lahj : LineArcHalfJoin = LineArcHalfJoin(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lahj.draw(canvas, paint)
+            animator.animate {
+                lahj.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lahj.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
