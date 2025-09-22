810a46c30fa6ccf51733f2029273711a4112742d
Anwesh43 <anweshthecool0@gmail.com>
2025-09-23 01:30:45 +0530

    halfarcjoindownview: adding HalfArcJoinDownView class

--- halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
+++ halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawHAJDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcJoinDown(scale, w, h, paint)
 }
+
+class HalfArcJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
