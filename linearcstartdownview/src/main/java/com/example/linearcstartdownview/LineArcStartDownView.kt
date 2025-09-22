dcd8a0992849e5af241cc6f2b47892b2ee57d1f5
Anwesh43 <anweshthecool0@gmail.com>
2025-09-22 12:33:32 +0530

    lineacstartdownview: adding Renderer class

--- linearcstartdownview/src/main/java/com/example/linearcstartdownview/LineArcStartDownView.kt
+++ linearcstartdownview/src/main/java/com/example/linearcstartdownview/LineArcStartDownView.kt
@@ -190,4 +190,27 @@ class LineArcStartDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcStartDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lasd : LineArcStartDown = LineArcStartDown(0)
+
+        fun render(canvas : Canvas, paint : Paint) {
+            canvas.drawColor(backColor)
+            lasd.draw(canvas, paint)
+            animator.animate {
+                lasd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lasd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
