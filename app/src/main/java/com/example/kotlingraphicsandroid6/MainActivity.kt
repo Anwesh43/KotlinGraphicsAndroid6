f657f0e1709cb249a0eade1f012acd6395960d23
Anwesh43 <anweshthecool0@gmail.com>
2025-09-23 01:50:50 +0530

    halfarcjoindownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcstartdownview.LineArcStartDownView
+import com.example.halfarcjoindownview.HalfArcJoinDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcStartDownView.create( this)
+        HalfArcJoinDownView.create( this)
     }
 }
