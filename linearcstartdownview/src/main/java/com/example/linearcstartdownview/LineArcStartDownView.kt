747c1a54b19f42c3c2b818e572f728c654e2aaa3
Anwesh43 <anweshthecool0@gmail.com>
2025-09-22 12:29:29 +0530

    lineacstartdownview: adding LineArcStartDown class

--- linearcstartdownview/src/main/java/com/example/linearcstartdownview/LineArcStartDownView.kt
+++ linearcstartdownview/src/main/java/com/example/linearcstartdownview/LineArcStartDownView.kt
@@ -167,4 +167,27 @@ class LineArcStartDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcStartDown(var i : Int = 0, val state : State = State()) {
+
+        private var curr : LASDNode = LASDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
