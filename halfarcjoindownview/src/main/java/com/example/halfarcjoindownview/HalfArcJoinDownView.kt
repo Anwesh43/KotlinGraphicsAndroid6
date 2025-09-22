cf5bfa851bcc20f63cbfa117229f205713427535
Anwesh43 <anweshthecool0@gmail.com>
2025-09-23 02:05:10 +0530

    halfarcjoindownview: adding fix 4

--- halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
+++ halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
@@ -40,7 +40,7 @@ fun Canvas.drawHalfArcJoinDown(scale : Float, w : Float, h : Float, paint : Pain
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(4)) {
         rotate(rot * dsc(2))
         for (j in 0..1) {
             drawXY(0f, 0f) {
