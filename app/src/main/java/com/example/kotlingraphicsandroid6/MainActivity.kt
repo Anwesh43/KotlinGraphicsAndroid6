37cfbd9ff9366b336ae20981e01dde6325523520
Anwesh43 <anweshthecool0@gmail.com>
2025-09-25 03:12:40 +0530

    halfarcjoinfullview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearccreateleftview.LineArcCreateLeftView
+import com.example.halfarcjoinfullview.HalfArcJoinFullView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcCreateLeftView.create( this)
+        HalfArcJoinFullView.create( this)
     }
 }
