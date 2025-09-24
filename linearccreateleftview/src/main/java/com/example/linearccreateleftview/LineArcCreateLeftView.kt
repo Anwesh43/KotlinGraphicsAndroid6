04cf4cb6d9c7875f76f2f3ef1a881b13550385b9
Anwesh43 <anweshthecool0@gmail.com>
2025-09-24 10:33:55 +0530

    linearccreateleftview: adding Canvas extension methods

--- linearccreateleftview/src/main/java/com/example/linearccreateleftview/LineArcCreateLeftView.kt
+++ linearccreateleftview/src/main/java/com/example/linearccreateleftview/LineArcCreateLeftView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCreateLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(rot * dsc(2))
+        drawXY((w / 2) * (1 - dsc(0)), -size + size * 0.5f * dsc(3)) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+            val currRot : Float = 180f * dsc(1)
+            drawArc(RectF(-size / 2, -size, size / 2, 0f), 90f - currRot, currRot, false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLACLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineArcCreateLeft(scale, w, h, paint)
+}
