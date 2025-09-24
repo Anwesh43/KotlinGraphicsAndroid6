1bac2ef6ba2e172605ba403be3f5a71ab2a68aac
Anwesh43 <anweshthecool0@gmail.com>
2025-09-24 10:52:52 +0530

    linearccreateleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.halfarcjoindownview.HalfArcJoinDownView
+import com.example.linearccreateleftview.LineArcCreateLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HalfArcJoinDownView.create( this)
+        LineArcCreateLeftView.create( this)
     }
 }
