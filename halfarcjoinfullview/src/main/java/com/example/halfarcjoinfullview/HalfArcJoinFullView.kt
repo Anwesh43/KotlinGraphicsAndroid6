d88c2040bd8a8c854690f028f1b6cd83bad08e31
Anwesh43 <anweshthecool0@gmail.com>
2025-09-25 02:46:15 +0530

    halfarcjoinfullview: adding HalfArcJoinFullView class

--- halfarcjoinfullview/src/main/java/com/example/halfarcjoinfullview/HalfArcJoinFullView.kt
+++ halfarcjoinfullview/src/main/java/com/example/halfarcjoinfullview/HalfArcJoinFullView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawHAJFNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcJoinFull(scale, w, h, paint)
 }
+
+class HalfArcJoinFullView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
