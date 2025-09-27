f83e1069e660b4852eaf352706b6f96a48ed5e40
Anwesh43 <anweshthecool0@gmail.com>
2025-09-27 09:55:55 +0530

    triarcjoinrightview: adding Animator class

--- triarcjoinrightview/src/main/java/com/example/triarcjoinrightview/TriArcJoinRightView.kt
+++ triarcjoinrightview/src/main/java/com/example/triarcjoinrightview/TriArcJoinRightView.kt
@@ -101,4 +101,30 @@ class TriArcJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            animated = false
+        }
+    }
 }
