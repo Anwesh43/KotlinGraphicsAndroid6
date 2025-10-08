d2a8c92f459da0a44a66e677e511a25d28e17f71
Anwesh43 <anweshthecool0@gmail.com>
2025-10-08 10:52:05 +0530

    lineballrotmover: adding Canvas extension methods

--- lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
+++ lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
@@ -29,3 +29,40 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBallRotMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w /2 , h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawXY(size * (dsc(1) - dsc(3)), size * dsc(2)) {
+            drawArc(RectF(-r, -r, r, r), 0f, 360f * dsc(0), true, paint)
+        }
+        drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, 0f, size * dsc(2), paint)
+            drawXY(0f, size) {
+                drawLine(0f, 0f, -size * dsc(3), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBRMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBallRotMover(scale, w, h, paint)
+}
+
