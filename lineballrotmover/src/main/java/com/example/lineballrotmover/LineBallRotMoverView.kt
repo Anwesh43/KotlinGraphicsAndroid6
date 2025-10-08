0fe77e97c204c7a9337a36595b2f638817d70af2
Anwesh43 <anweshthecool0@gmail.com>
2025-10-08 10:39:00 +0530

    lineballrotmover: adding constants and libraries

---
+++ lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
@@ -0,0 +1,27 @@
+package com.example.lineballrotmover
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 6
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val rFactor : Float = 15.2f
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
+
