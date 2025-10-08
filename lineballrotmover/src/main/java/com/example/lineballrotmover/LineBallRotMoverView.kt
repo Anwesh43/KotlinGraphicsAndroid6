f649840fea280afc26b0edec353a3880356f1128
Anwesh43 <anweshthecool0@gmail.com>
2025-10-08 11:59:31 +0530

    lineballrotmover: adding Renderer class

--- lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
+++ lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
@@ -192,4 +192,27 @@ class LineBallRotMoverView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBallRotMoverView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lbrm : LineBallRotMover = LineBallRotMover(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbrm.draw(canvas, paint)
+            animator.animate {
+                lbrm.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbrm.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
