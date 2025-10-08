31250f43eab00390ee0991d136d455aab00ffad2
Anwesh43 <anweshthecool0@gmail.com>
2025-10-08 11:21:41 +0530

    lineballrotmover: adding LBRMNode class

--- lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
+++ lineballrotmover/src/main/java/com/example/lineballrotmover/LineBallRotMoverView.kt
@@ -128,4 +128,45 @@ class LineBallRotMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBRMNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBRMNode? = null
+        private var prev : LBRMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBRMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBRMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBRMNode {
+            var curr : LBRMNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
