18826c4efed3db2492cdbb11bd9ca0d96c573ff4
Anwesh43 <anweshthecool0@gmail.com>
2025-10-09 08:39:12 +0530

    bilineclosedownview: Adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.lineballrotmover.LineBallRotMoverView
+import com.example.bilineclosedownview.BiLineCloseDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBallRotMoverView.create( this)
+        BiLineCloseDownView.create( this)
     }
 }
