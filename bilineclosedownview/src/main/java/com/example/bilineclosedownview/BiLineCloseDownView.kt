74ae7313e04f5a6dc43e5ef5d3269f1d274643e0
Anwesh43 <anweshthecool0@gmail.com>
2025-10-09 08:18:32 +0530

    bilineclosedownview: adding Canvas extension methods

--- bilineclosedownview/src/main/java/com/example/bilineclosedownview/BiLineCloseDownView.kt
+++ bilineclosedownview/src/main/java/com/example/bilineclosedownview/BiLineCloseDownView.kt
@@ -28,3 +28,45 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this -  i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineCloseDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(deg * dsc(4))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(1), false, paint)
+        for (j in 0..1) {
+            drawXY(-size / 2 + size * j, 0f) {
+                rotate(-rot * (1f - 2 * j) * dsc(3))
+                drawXY(0f, size * 0.5f * (1 - j)) {
+                    drawLine(
+                        0f,
+                        size * 0.5f * (1 - j) * dsc(j * 2),
+                        0f,
+                        size * 0.5f * j * dsc(j * 2),
+                        paint
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiLineCloseDown(scale, w, h, paint)
+}
