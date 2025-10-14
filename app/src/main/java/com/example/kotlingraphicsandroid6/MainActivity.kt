b5ccc6a808caf4247f8f900851a8a70277f48ef3
Anwesh43 <anweshthecool0@gmail.com>
2025-10-15 02:08:05 +0530

    rshapetodshapeview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bilineclosedownview.BiLineCloseDownView
+import com.example.rshapetodshapeview.RShapeToDShapeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiLineCloseDownView.create( this)
+        RShapeToDShapeView.create( this)
     }
 }
