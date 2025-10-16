9194aa88ae8e66cbff024dd7ce7275c68e9c9d43
Anwesh43 <anweshthecool0@gmail.com>
2025-10-16 09:02:55 +0530

    biquarterseparatearcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.rshapetodshapeview.RShapeToDShapeView
+import com.example.biquarterseparatearcview.BiQuarterSeparateArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RShapeToDShapeView.create( this)
+        BiQuarterSeparateArcView.create( this)
     }
 }
