b8083181e898e71ebdc99e2cac52405d6f603611
Anwesh43 <anweshthecool0@gmail.com>
2025-10-17 01:11:42 +0530

    biendarrowlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biquarterseparatearcview.BiQuarterSeparateArcView
+import com.example.biendarrowlineview.BiEndArrowLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiQuarterSeparateArcView.create( this)
+        BiEndArrowLineView.create( this)
     }
 }
