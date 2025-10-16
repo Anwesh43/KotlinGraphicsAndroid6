513909fad9480055fe40227fff3cfac0a52dd1a7
Anwesh43 <anweshthecool0@gmail.com>
2025-10-17 00:52:42 +0530

    biendarrowlineview: adding BiEndArrowLineView class

--- biendarrowlineview/src/main/java/com/example/biendarrowlineview/BiEndArrowLineView.kt
+++ biendarrowlineview/src/main/java/com/example/biendarrowlineview/BiEndArrowLineView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBEALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiEndArrowLine(scale, w, h, paint)
 }
+
+class BiEndArrowLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
