9bf989cd502ad2fe32e2c002077d6f95845129aa
Anwesh43 <anweshthecool0@gmail.com>
2025-10-18 09:23:06 +0530

    polarextendlinerotview: adding PolarExtendLineRotView class

--- polarextendlinerotview/src/main/java/com/example/polarextendlinerotview/PolarExtendLineRotView.kt
+++ polarextendlinerotview/src/main/java/com/example/polarextendlinerotview/PolarExtendLineRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawPELRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPolarExtendLineRot(scale, w, h, paint)
 }
+
+class PolarExtendLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
