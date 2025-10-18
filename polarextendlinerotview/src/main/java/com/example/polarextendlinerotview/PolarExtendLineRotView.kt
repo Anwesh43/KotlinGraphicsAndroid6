8eb7c0352c27b8b31579149665a138a02aa37aa1
Anwesh43 <anweshthecool0@gmail.com>
2025-10-18 09:03:10 +0530

    polarextendlinerotview: adding constants and libraries

---
+++ polarextendlinerotview/src/main/java/com/example/polarextendlinerotview/PolarExtendLineRotView.kt
@@ -0,0 +1,24 @@
+package com.example.polarextendlinerotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import androidx.core.graphics.toColorInt
+import android.graphics.Canvas
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
