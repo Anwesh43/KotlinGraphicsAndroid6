113d82f4895bb8b08ac98523d26267c76c2d5618
Anwesh43 <anweshthecool0@gmail.com>
2025-10-18 09:20:15 +0530

    polarextendlinerotview: adding Canvas extension methods

--- polarextendlinerotview/src/main/java/com/example/polarextendlinerotview/PolarExtendLineRotView.kt
+++ polarextendlinerotview/src/main/java/com/example/polarextendlinerotview/PolarExtendLineRotView.kt
@@ -15,8 +15,9 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val lines : Int = 6
+val parts : Int = lines + 2
+val scGap : Float = 0.06f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -26,3 +27,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPolarExtendLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val deg : Float = 180f / (lines - 1)
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(lines + 1)) {
+        rotate(rot * dsc(lines))
+        for (j in 0..(lines - 1)) {
+            drawXY(0f, 0f) {
+                rotate(deg * j)
+                drawLine(0f, 0f, 0f, size * dsc(j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawPELRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPolarExtendLineRot(scale, w, h, paint)
+}
