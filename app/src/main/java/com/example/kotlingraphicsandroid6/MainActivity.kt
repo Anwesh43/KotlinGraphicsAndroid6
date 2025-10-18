8069e93d6e8cdbc4e8117052fbf695a4df07db6b
Anwesh43 <anweshthecool0@gmail.com>
2025-10-18 09:54:21 +0530

    polarextendlinerotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biendarrowlineview.BiEndArrowLineView
+import com.example.polarextendlinerotview.PolarExtendLineRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiEndArrowLineView.create( this)
+        PolarExtendLineRotView.create( this)
     }
 }
