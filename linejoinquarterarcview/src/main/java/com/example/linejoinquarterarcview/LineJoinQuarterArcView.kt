831f8dfc7eeeb90142ef65416f819a8f8de493d0
Anwesh43 <anweshthecool0@gmail.com>
2025-10-20 13:34:34 +0530

    linejoinquarterarcview: adding Renderer in View

--- linejoinquarterarcview/src/main/java/com/example/linejoinquarterarcview/LineJoinQuarterArcView.kt
+++ linejoinquarterarcview/src/main/java/com/example/linejoinquarterarcview/LineJoinQuarterArcView.kt
@@ -61,14 +61,16 @@ fun Canvas.drawLJQANode(i : Int, scale : Float, paint : Paint) {
 
 class LineJoinQuarterArcView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
