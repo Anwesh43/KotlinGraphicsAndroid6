f92440e3c0ff3b9ffa7cb0ebfe569c8689cac40d
Anwesh43 <anweshthecool0@gmail.com>
2025-10-20 13:25:06 +0530

    linejoinquarterarcview: adding Canvas extension methods

--- linejoinquarterarcview/src/main/java/com/example/linejoinquarterarcview/LineJoinQuarterArcView.kt
+++ linejoinquarterarcview/src/main/java/com/example/linejoinquarterarcview/LineJoinQuarterArcView.kt
@@ -27,3 +27,34 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineJoinQuarterArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size / 2) * dsc(4), h / 2 - (h / 2 + size / 2) * dsc(4)) {
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, rot * dsc(0), false, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, -size * 0.5f * dsc(1), 0f, paint)
+        }
+        drawLine(0f, 0f, 0f, size * 0.5f * dsc(2), paint)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 90f * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawLJQANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineJoinQuarterArc(scale, w, h, paint)
+}
