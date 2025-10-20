98457de4cda9e037ea6712d4755a435c3a73b153
Anwesh43 <anweshthecool0@gmail.com>
2025-10-20 13:36:06 +0530

    linejoinquarterarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.bentlinerotsingleview.BentLineRotSingleView
+import com.example.linejoinquarterarcview.LineJoinQuarterArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineRotSingleView.create( this)
+        LineJoinQuarterArcView.create( this)
     }
 }
