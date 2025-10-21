f2afd6f7e621d6712495cf304ad3ae22b16a51de
Anwesh43 <anweshthecool0@gmail.com>
2025-10-21 12:14:53 +0530

    diaglinearcjoinview: adding DLAJNode class

--- diaglinearcjoinview/src/main/java/com/example/diaglinearcjoinview/DiagLineArcJoinView.kt
+++ diaglinearcjoinview/src/main/java/com/example/diaglinearcjoinview/DiagLineArcJoinView.kt
@@ -124,4 +124,44 @@ class DiagLineArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DLAJNode(var i : Int = 0, val state : State = State()) {
+        private var next : DLAJNode? = null
+        private var prev : DLAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DLAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDLAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DLAJNode {
+            var curr : DLAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
