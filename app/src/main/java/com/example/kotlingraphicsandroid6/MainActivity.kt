4d5cc8c103bfbdbb8ef30e6846b7c96833cb6235
Anwesh43 <anweshthecool0@gmail.com>
2025-10-21 12:36:26 +0530

    diaglinearcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linejoinquarterarcview.LineJoinQuarterArcView
+import com.example.diaglinearcjoinview.DiagLineArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineJoinQuarterArcView.create( this)
+        DiagLineArcJoinView.create( this)
     }
 }
