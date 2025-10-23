a4c18e66a36a7a443034e9b8551dc004c27c0739
Anwesh43 <anweshthecool0@gmail.com>
2025-10-23 11:37:47 +0530

    linerightarcjoinview: adding Canvas extension methods

--- linerightarcjoinview/src/main/java/com/example/linerightarcjoinview/LineRightArcJoinView.kt
+++ linerightarcjoinview/src/main/java/com/example/linerightarcjoinview/LineRightArcJoinView.kt
@@ -27,3 +27,35 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawArc(RectF(-size, -size, size, size), -90f, 90f * dsc(1), false, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, size * dsc(0), -size * dsc(0), paint)
+        }
+    }
+}
+
+fun Canvas.drawLRAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineRightArcJoin(scale, w, h, paint)
+}
