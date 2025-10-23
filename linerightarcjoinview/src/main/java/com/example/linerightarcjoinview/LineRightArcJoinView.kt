8249a1addb91bcb596910a156637a90820709bdb
Anwesh43 <anweshthecool0@gmail.com>
2025-10-23 12:14:40 +0530

    linerightarcjoinview: adding LRAJNode class

--- linerightarcjoinview/src/main/java/com/example/linerightarcjoinview/LineRightArcJoinView.kt
+++ linerightarcjoinview/src/main/java/com/example/linerightarcjoinview/LineRightArcJoinView.kt
@@ -122,4 +122,45 @@ class LineRightArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRAJNode? = null
+        private var prev : LRAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRAJNode {
+            var curr : LRAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
