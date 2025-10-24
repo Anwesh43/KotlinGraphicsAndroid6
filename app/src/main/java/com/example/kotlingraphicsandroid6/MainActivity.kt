4c7ffe49f604d72a2f952910cfc4aaedb33d379c
Anwesh43 <anweshthecool0@gmail.com>
2025-10-24 13:10:25 +0530

    biarcexpandupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linerightarcjoinview.LineRightArcJoinView
+import com.example.biarcexpandupview.BiArcExpandUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRightArcJoinView.create( this)
+        BiArcExpandUpView.create( this)
     }
 }
