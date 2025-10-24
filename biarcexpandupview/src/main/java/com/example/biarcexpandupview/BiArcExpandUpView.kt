f1a131aef9b10826910f0f79d68548d93779aede
Anwesh43 <anweshthecool0@gmail.com>
2025-10-24 12:35:49 +0530

    biarcexpandupview: adding BiArcExpandUp class

--- biarcexpandupview/src/main/java/com/example/biarcexpandupview/BiArcExpandUpView.kt
+++ biarcexpandupview/src/main/java/com/example/biarcexpandupview/BiArcExpandUpView.kt
@@ -181,4 +181,27 @@ class BiArcExpandUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiArcExpandUp(var i : Int) {
+
+        private var curr : BAEUNode = BAEUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
