8840c05aec7870ee8170424fe1d502639283bea3
Anwesh43 <anweshthecool0@gmail.com>
2025-10-25 10:54:15 +0530

    arcshiftcircleupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biarcexpandupview.BiArcExpandUpView
+import com.example.arcshiftcircleupview.ArcShiftCircleUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcExpandUpView.create( this)
+        ArcShiftCircleUpView.create( this)
     }
 }
