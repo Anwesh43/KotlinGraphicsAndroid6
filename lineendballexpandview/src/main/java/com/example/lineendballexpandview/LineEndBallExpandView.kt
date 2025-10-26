d26d55d3553fb46b3233d27c53eac3900e8d9627
Anwesh43 <anweshthecool0@gmail.com>
2025-10-26 10:01:12 +0530

    lineendballexpandview: adding LEBENode class

--- lineendballexpandview/src/main/java/com/example/lineendballexpandview/LineEndBallExpandView.kt
+++ lineendballexpandview/src/main/java/com/example/lineendballexpandview/LineEndBallExpandView.kt
@@ -125,4 +125,45 @@ class LineEndBallExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEBENode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LEBENode? = null
+        private var next : LEBENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEBENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEBENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEBENode {
+            var curr : LEBENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
