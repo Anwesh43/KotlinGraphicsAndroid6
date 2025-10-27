3b0e2ee4975c710c86f8d788c4df3fb9d4602149
Anwesh43 <anweshthecool0@gmail.com>
2025-10-27 09:08:41 +0530

    lineintoquarterarcview: adding LineIntoQuarterArcView class

--- lineintoquarterarcview/src/main/java/com/example/lineintoquarterarcview/LineIntoQuarterArcView.kt
+++ lineintoquarterarcview/src/main/java/com/example/lineintoquarterarcview/LineIntoQuarterArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLIQCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineIntoQuarterArc(scale, w, h, paint)
 }
+
+class LineIntoQuarterArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
