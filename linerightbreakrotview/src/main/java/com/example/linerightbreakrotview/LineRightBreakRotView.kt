4ee08187eb11fc2b5b7abb83f3d188faed5aac1a
Anwesh43 <anweshthecool0@gmail.com>
2025-10-28 08:56:11 +0530

    linerightbreakrotview: adding LRBRNode class

--- linerightbreakrotview/src/main/java/com/example/linerightbreakrotview/LineRightBreakRotView.kt
+++ linerightbreakrotview/src/main/java/com/example/linerightbreakrotview/LineRightBreakRotView.kt
@@ -121,4 +121,45 @@ class LineRightBreakRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LRBRNode? = null
+        private var next : LRBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRBRNode {
+            var curr : LRBRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
