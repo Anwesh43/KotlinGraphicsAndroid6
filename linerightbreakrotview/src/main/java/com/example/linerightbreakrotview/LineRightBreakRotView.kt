d8f1344daed606348bc57eeb2d12ac8c61b6ebe3
Anwesh43 <anweshthecool0@gmail.com>
2025-10-28 08:46:32 +0530

    linerightbreakrotview: adding Canvas extension methods

--- linerightbreakrotview/src/main/java/com/example/linerightbreakrotview/LineRightBreakRotView.kt
+++ linerightbreakrotview/src/main/java/com/example/linerightbreakrotview/LineRightBreakRotView.kt
@@ -22,7 +22,39 @@ val sizeFactor : Float = 5.9f
 val rot : Float = 120f
 val backColor : Int  ="#BDBDBD".toColorInt()
 val delay : Long = 20
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightBreakRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY((w / 2) * j * dsc(2), (h / 2) * (1 - j) * dsc(3)) {
+                rotate(deg * dsc(1) * rot * dsc(3))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightBreakRot(scale, w, h, paint)
+}
