bb643047146abd2e7a61d84c382ce6239058cbe7
Anwesh43 <anweshthecool0@gmail.com>
2025-10-29 13:32:50 +0530

    lineumbrellaarcdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linerightbreakrotview.LineRightBreakRotView
+import com.example.lineumbrellaarcdownview.LineUmbrellaArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRightBreakRotView.create( this)
+        LineUmbrellaArcDownView.create( this)
     }
 }
