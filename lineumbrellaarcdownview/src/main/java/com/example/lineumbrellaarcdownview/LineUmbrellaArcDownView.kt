5127a74eb33b6fa1a9fb4532bf71f7e0b6442c76
Anwesh43 <anweshthecool0@gmail.com>
2025-10-29 11:38:00 +0530

    lineumbrellaarcdownview: adding Canvas extension methods

--- lineumbrellaarcdownview/src/main/java/com/example/lineumbrellaarcdownview/LineUmbrellaArcDownView.kt
+++ lineumbrellaarcdownview/src/main/java/com/example/lineumbrellaarcdownview/LineUmbrellaArcDownView.kt
@@ -28,3 +28,36 @@ val sweep : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *  n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineUmbrellaArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                drawArc(RectF(-size, -size, size, size), -90f, sweep * (dsc(1) + dsc(3)), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLUADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineUmbrellaArcDown(scale, w, h, paint)
+}
