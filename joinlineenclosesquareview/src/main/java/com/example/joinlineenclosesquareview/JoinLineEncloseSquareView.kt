399b51454413bbe8a48209eda4d3d590147208bb
Anwesh43 <anweshthecool0@gmail.com>
2025-10-30 18:16:26 +0530

    joinlineenclosesquareview: adding JLESNode class

--- joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
+++ joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
@@ -124,4 +124,45 @@ class JoinLineEncloseSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JLESNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : JLESNode? = null
+        private var prev : JLESNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JLESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJLESNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JLESNode {
+            var curr : JLESNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
