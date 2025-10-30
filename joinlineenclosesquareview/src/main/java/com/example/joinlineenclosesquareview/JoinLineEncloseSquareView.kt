7aedc098c0364a9e55e93fa4af39921945967ef5
Anwesh43 <anweshthecool0@gmail.com>
2025-10-30 13:48:45 +0530

    joinlineenclosesquareview: adding Canvas extension methods

--- joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
+++ joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawJoinLineEncloseSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (2 - j) * dsc(j + 2) * (1f - 2 * j))
+                drawLine(0f, 0f, 0f, -size * dsc(j).divideScale(0, 2), paint)
+                drawXY(0f, -size) {
+                    drawLine(0f, 0f, size * dsc(j).divideScale(1, 2) * (1f - 2 * j), 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawJLESNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawJoinLineEncloseSquare(scale, w, h, paint)
+}
