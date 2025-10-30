a7c105a4c0b9d93758ac67f7fcd2e30aa38f902a
Anwesh43 <anweshthecool0@gmail.com>
2025-10-30 13:49:53 +0530

    joinlineenclosesquareview: adding JoinLineEncloseSquareView class

--- joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
+++ joinlineenclosesquareview/src/main/java/com/example/joinlineenclosesquareview/JoinLineEncloseSquareView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawJLESNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawJoinLineEncloseSquare(scale, w, h, paint)
 }
+
+class JoinLineEncloseSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
