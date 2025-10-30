c9c15fbb280cc060ba971a3fc7e66367b807fd2b
Anwesh43 <anweshthecool0@gmail.com>
2025-10-30 18:27:33 +0530

    joinlineenclosesquareview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.lineumbrellaarcdownview.LineUmbrellaArcDownView
+import com.example.joinlineenclosesquareview.JoinLineEncloseSquareView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineUmbrellaArcDownView.create( this)
+        JoinLineEncloseSquareView.create( this)
     }
 }
