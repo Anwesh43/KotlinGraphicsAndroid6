3fc6eacd256aa6ebdbdf59619d8bce255f51f125
Anwesh43 <anweshthecool0@gmail.com>
2025-11-01 13:34:58 +0530

    orbitalballcollapseview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.halfarcshiftupview.HalfArcShiftUpView
+import com.example.orbitalballscollapseview.OrbitalBallsCollapseView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HalfArcShiftUpView.create( this)
+        OrbitalBallsCollapseView.create( this)
     }
 }
