997de1dc6990822cb90298b7ce6a58e3810b512e
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 02:35:10 +0530

    concaltarcdownview: adding Canvas extension methods

--- concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
+++ concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
@@ -28,3 +28,36 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawConcAltArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(1))
+        for (j in 0..1) {
+            drawXY(0f, h * 0.5f * dsc(3).divideScale(j, 2)) {
+                val r : Float = size * 0.5f * (2 - j)
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(-r, -r, r, r), 180f, deg * dsc(0) + (rot - deg) * dsc(2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawCAADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawConcAltArcDown(scale, w, h, paint)
+}
