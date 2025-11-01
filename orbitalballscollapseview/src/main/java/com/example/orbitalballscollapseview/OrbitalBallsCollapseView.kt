5c1913fdd8e2d860cf3115aa82ac01ea4a2ca71a
Anwesh43 <anweshthecool0@gmail.com>
2025-11-01 13:09:59 +0530

    orbitalballcollapseview: adding OrbitalBallsCollapseView class

--- orbitalballscollapseview/src/main/java/com/example/orbitalballscollapseview/OrbitalBallsCollapseView.kt
+++ orbitalballscollapseview/src/main/java/com/example/orbitalballscollapseview/OrbitalBallsCollapseView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawOBCNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i].toColorInt()
     drawOrbitalBallsCollapse(scale, w, h, paint)
 }
+
+class OrbitalBallsCollapseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
