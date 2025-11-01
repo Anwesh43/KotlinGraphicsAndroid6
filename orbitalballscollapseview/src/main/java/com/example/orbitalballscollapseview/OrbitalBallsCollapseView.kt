bdd0ada70c49b1fbe77a761306b2a8ca46a0b293
Anwesh43 <anweshthecool0@gmail.com>
2025-11-01 12:36:56 +0530

    orbitalballcollapseview: adding constants and libraries

---
+++ orbitalballscollapseview/src/main/java/com/example/orbitalballscollapseview/OrbitalBallsCollapseView.kt
@@ -0,0 +1,25 @@
+package com.example.orbitalballscollapseview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 90f
+val backColor : Int ="#BDBDBD".toColorInt()
+val rot : Float = 180f
+val delay : Long = 20
+val sweep : Float = 360f
