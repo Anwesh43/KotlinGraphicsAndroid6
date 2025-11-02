52e8a33fa1cd0a28b63817b2ffa7f23b64e217e9
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 09:07:25 +0530

    concaltarcdownview: adding Renderer in View

--- concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
+++ concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
@@ -64,13 +64,15 @@ fun Canvas.drawCAADNode(i : Int, scale : Float, paint : Paint) {
 
 class ConcAltArcDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
