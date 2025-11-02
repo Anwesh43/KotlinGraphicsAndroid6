3cc994c40f1906ad9a304eaa4c365e626b1e39aa
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 08:49:19 +0530

    concaltarcdownview: adding ConcAltArcDownView class

--- concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
+++ concaltarcdownview/src/main/java/com/example/concaltarcdownview/ConcAltArcDownView.kt
@@ -61,3 +61,18 @@ fun Canvas.drawCAADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawConcAltArcDown(scale, w, h, paint)
 }
+
+class ConcAltArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
