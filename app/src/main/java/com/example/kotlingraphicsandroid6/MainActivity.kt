583a05199b820a66276a47cedad834b4aa714327
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 09:18:05 +0530

    concaltarcdownview: adding static view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.orbitalballscollapseview.OrbitalBallsCollapseView
+import com.example.concaltarcdownview.ConcAltArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        OrbitalBallsCollapseView.create( this)
+        ConcAltArcDownView.create( this)
     }
 }
