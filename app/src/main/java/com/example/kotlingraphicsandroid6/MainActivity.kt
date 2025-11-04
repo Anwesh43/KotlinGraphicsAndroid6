9659d8c3308b057664e011680b504badbd9f0aaa
Anwesh43 <anweshthecool0@gmail.com>
2025-11-04 08:55:32 +0530

    linearcjoinrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.lineshiftpartialcompleteview.LineShiftPartialCompleteView
+import com.example.linearcjoinrightview.LineArcJoinRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineShiftPartialCompleteView.create( this)
+        LineArcJoinRightView.create( this)
     }
 }
