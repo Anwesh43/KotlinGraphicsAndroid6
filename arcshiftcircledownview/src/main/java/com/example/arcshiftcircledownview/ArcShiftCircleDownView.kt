3ceb78d8ac1b94e830ebbb78143d49941e6c3322
Anwesh43 <anweshthecool0@gmail.com>
2025-11-06 07:57:05 +0530

    arcshiftcircledownview: adding ArcShiftCircleDownView class

--- arcshiftcircledownview/src/main/java/com/example/arcshiftcircledownview/ArcShiftCircleDownView.kt
+++ arcshiftcircledownview/src/main/java/com/example/arcshiftcircledownview/ArcShiftCircleDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawASCDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcShiftCircleDown(scale, w, h, paint)
 }
+
+class ArcShiftCircleDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
