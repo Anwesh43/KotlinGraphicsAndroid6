03d19cf687677a33a88645a0fe47eca58902e302
Anwesh43 <anweshthecool0@gmail.com>
2025-11-06 08:14:46 +0530

    arcshiftcircledownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcjoinrightview.LineArcJoinRightView
+import com.example.arcshiftcircledownview.ArcShiftCircleDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcJoinRightView.create( this)
+        ArcShiftCircleDownView.create( this)
     }
 }
