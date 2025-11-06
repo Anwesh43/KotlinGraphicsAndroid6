905727c35c17ed19a2b09a56e0fddbab1fa742f8
Anwesh43 <anweshthecool0@gmail.com>
2025-11-07 01:31:09 +0530

    linearcencloseview: adding LineArcEnclose class

--- linearcencloseview/src/main/java/com/example/linearcencloseview/LineArcEncloseView.kt
+++ linearcencloseview/src/main/java/com/example/linearcencloseview/LineArcEncloseView.kt
@@ -165,4 +165,27 @@ class LineArcEncloseView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcEnclose(var i : Int) {
+
+        private var curr : LAENode = LAENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
