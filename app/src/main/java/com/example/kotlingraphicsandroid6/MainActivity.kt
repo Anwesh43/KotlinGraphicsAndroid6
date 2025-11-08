d16c9e5bbdad75dd457d1b975ac2513d7ca120ae
Anwesh43 <anweshthecool0@gmail.com>
2025-11-08 08:37:28 +0530

    umbrellamidlinedownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linearcencloseview.LineArcEncloseView
+import com.example.umbrellamidlinedownview.UmbrellaMidLineDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcEncloseView.create( this)
+        UmbrellaMidLineDownView.create( this)
     }
 }
