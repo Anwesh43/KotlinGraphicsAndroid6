5684844d8d034cbefec93fc7f9009caf7cd9a2f7
Anwesh43 <anweshthecool0@gmail.com>
2025-11-08 08:30:25 +0530

    umbrellamidlinedownview: adding UMLDNode class

--- umbrellamidlinedownview/src/main/java/com/example/umbrellamidlinedownview/UmbrellaMidLineDownView.kt
+++ umbrellamidlinedownview/src/main/java/com/example/umbrellamidlinedownview/UmbrellaMidLineDownView.kt
@@ -120,4 +120,45 @@ class UmbrellaMidLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class UMLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : UMLDNode? = null
+        private var next : UMLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = UMLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawUMLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : UMLDNode {
+            var curr : UMLDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
