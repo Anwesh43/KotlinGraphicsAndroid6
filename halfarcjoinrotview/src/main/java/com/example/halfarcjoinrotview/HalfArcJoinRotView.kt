bf20255acf5d41dd39a9bf0accc5160e11769838
Anwesh43 <anweshthecool0@gmail.com>
2025-11-09 14:01:27 +0530

    haalfarcjoinrotview: adding constants and libraries

---
+++ halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
@@ -0,0 +1,26 @@
+package com.example.halfarcjoinrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 180f
+val deg : Float = 45f
