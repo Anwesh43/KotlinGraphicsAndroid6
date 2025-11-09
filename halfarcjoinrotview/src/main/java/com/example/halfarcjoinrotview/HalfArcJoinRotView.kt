bac4bace99fe086b9f6132361fef3ce4aa3719a3
Anwesh43 <anweshthecool0@gmail.com>
2025-11-09 14:11:48 +0530

    haalfarcjoinrotview: adding HalfArcJoinRotView class

--- halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
+++ halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawHAJRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcJoinRot(scale, w, h, paint)
 }
+
+class HalfArcJoinRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
