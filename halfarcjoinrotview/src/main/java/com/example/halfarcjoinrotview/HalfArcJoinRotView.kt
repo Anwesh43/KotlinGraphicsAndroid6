725477a943a0d369c503b687e765bed0c52e9129
Anwesh43 <anweshthecool0@gmail.com>
2025-11-09 14:23:27 +0530

    haalfarcjoinrotview: adding HAJRNode class

--- halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
+++ halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
@@ -132,4 +132,45 @@ class HalfArcJoinRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HAJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HAJRNode? = null
+        private var prev : HAJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HAJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHAJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HAJRNode {
+            var curr : HAJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
