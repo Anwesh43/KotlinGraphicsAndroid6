bd077f2c44bf5d26332572ab949bc0a3b22da9f9
Anwesh43 <anweshthecool0@gmail.com>
2025-11-09 14:10:23 +0530

    haalfarcjoinrotview: adding Canvas extension methods

--- halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
+++ halfarcjoinrotview/src/main/java/com/example/halfarcjoinrotview/HalfArcJoinRotView.kt
@@ -28,3 +28,44 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcJoinRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawXY(-size * 0.5f * dsc(3), 0f) {
+            for (j in 0..1) {
+                drawXY(-size + 2 * size * j, 0f) {
+                    drawArc(
+                        RectF(-size, -size / 2, 0f, size / 2),
+                        -deg * (1 - j) + rot * j,
+                        deg * dsc(2 * j),
+                        false,
+                        paint
+                    )
+                }
+            }
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawHAJRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawHalfArcJoinRot(scale, w, h, paint)
+}
