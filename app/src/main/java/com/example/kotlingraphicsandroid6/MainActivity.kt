c77fdee82a7f01fc43b9fa47c09b150df20acc5d
Anwesh43 <anweshthecool0@gmail.com>
2025-11-10 08:06:32 +0530

    lineshiftbiarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.halfarcjoinrotview.HalfArcJoinRotView
+import com.example.lineshiftbiarcview.LineShiftBiArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HalfArcJoinRotView.create( this)
+        LineShiftBiArcView.create( this)
     }
 }
