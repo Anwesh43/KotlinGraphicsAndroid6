b25b495a59b39c1d1d281d565817446c2ca0e633
Anwesh43 <anweshthecool0@gmail.com>
2025-11-10 07:56:07 +0530

    lineshiftbiarcview: adding LSBANode class

--- lineshiftbiarcview/src/main/java/com/example/lineshiftbiarcview/LineShiftBiArcView.kt
+++ lineshiftbiarcview/src/main/java/com/example/lineshiftbiarcview/LineShiftBiArcView.kt
@@ -123,4 +123,45 @@ class LineShiftBiArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSBANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSBANode? = null
+        private var prev : LSBANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSBANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSBANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSBANode {
+            var curr : LSBANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
