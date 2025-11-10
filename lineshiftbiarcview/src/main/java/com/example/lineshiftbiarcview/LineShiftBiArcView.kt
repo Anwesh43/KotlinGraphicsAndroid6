d2c98bef8b0c4071a30bc52f3ee010d683f4848f
Anwesh43 <anweshthecool0@gmail.com>
2025-11-10 07:50:22 +0530

    lineshiftbiarcview: adding Canvas extension methods

--- lineshiftbiarcview/src/main/java/com/example/lineshiftbiarcview/LineShiftBiArcView.kt
+++ lineshiftbiarcview/src/main/java/com/example/lineshiftbiarcview/LineShiftBiArcView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.06f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.2f
 val delay : Long = 20
@@ -27,3 +27,37 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineShiftBiArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(4))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(2), false, paint)
+        }
+        drawXY(0f, -size * (1 - dsc(3))) {
+            drawArc(RectF(-size, -size /2 , 0f, size / 2), 180f, 180f * dsc(0), false, paint)
+        }
+        drawLine(0f, -size * (1 - dsc(2)), 0f, -size + size * dsc(1), paint)
+    }
+}
+
+fun Canvas.drawLSBANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineShiftBiArc(scale, w, h, paint)
+}
