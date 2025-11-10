626e0dec84227023880d12af4c6c1cb869babfa0
Anwesh43 <anweshthecool0@gmail.com>
2025-11-11 03:26:29 +0530

    linejoinarcrotupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.lineshiftbiarcview.LineShiftBiArcView
+import com.example.linejoinarcrotupview.LineJoinArcRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineShiftBiArcView.create( this)
+        LineJoinArcRotUpView.create( this)
     }
 }
