8ea127fdec6277ac68ec22141015ebf3fddabfec
Anwesh43 <anweshthecool0@gmail.com>
2025-11-12 08:50:33 +0530

    linepartextendarcview: adding LinePartExtendView class, previous commit message 'added DrawingUtil'

--- linepartextendarcview/src/main/java/com/example/linepartextendarcview/LinePartExtendArcView.kt
+++ linepartextendarcview/src/main/java/com/example/linepartextendarcview/LinePartExtendArcView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLPEANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLinePartExtendArc(scale, w, h, paint)
 }
+
+class LinePartExtendArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
