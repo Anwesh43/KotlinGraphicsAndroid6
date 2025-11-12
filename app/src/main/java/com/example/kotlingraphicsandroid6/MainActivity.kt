aa96778bbd230ad91704b5a5a06c9943b03575fc
Anwesh43 <anweshthecool0@gmail.com>
2025-11-12 09:43:34 +0530

    linepartextendarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linejoinarcrotupview.LineJoinArcRotUpView
+import com.example.linepartextendarcview.LinePartExtendArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineJoinArcRotUpView.create( this)
+        LinePartExtendArcView.create( this)
     }
 }
