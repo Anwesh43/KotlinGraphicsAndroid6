97ecfdb4f7e7b4ebd2de76fe3787f9f17953b87a
Anwesh43 <anweshthecool0@gmail.com>
2025-11-13 10:03:46 +0530

    arcrotlinerightview: adding Canvas extension methods

--- arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
+++ arcrotlinerightview/src/main/java/com/example/arcrotlinerightview/ArcRotLineRightView.kt
@@ -9,7 +9,7 @@ import android.graphics.Canvas
 import android.graphics.RectF
 import androidx.core.graphics.toColorInt
 
-val color : Array<String> = arrayOf(
+val colors : Array<String> = arrayOf(
     "#1A237E",
     "#EF5350",
     "#AA00FF",
@@ -27,4 +27,37 @@ val deg : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
-fun Float.diivdeScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcRotLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        drawArc(RectF(-size, -size, size, size), 90f, deg * (dsc(1) - dsc(4)), false, paint)
+        drawXY(0f, 0f) {
+            rotate(deg * (1 - dsc(4)))
+            drawLine(0f, size * (1 - dsc(2)), 0f, size, paint)
+        }
+    }
+}
+
+fun Canvas.drawARLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcRotLineRight(scale, w, h, paint)
+}
