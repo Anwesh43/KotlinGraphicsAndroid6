f40f0e2881623d9b294f523d7efff63e5eca1082
Anwesh43 <anweshthecool0@gmail.com>
2025-11-13 17:28:23 +0530

    arcrotlinerightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linepartextendarcview.LinePartExtendArcView
+import com.example.arcrotlinerightview.ArcRotLineRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinePartExtendArcView.create( this)
+        ArcRotLineRightView.create( this)
     }
 }
