080c1c662a2be550deb8c7f5bf2702d36d56f6b3
Anwesh43 <anweshthecool0@gmail.com>
2025-11-14 16:26:17 +0530

    arcjoinbigarcrightview: adding constants and libraries

---
+++ arcjoinbigarcrightview/src/main/java/com/example/arcjoinbigarcrightview/ArcJoinBigArcRightView.kt
@@ -0,0 +1,22 @@
+package com.example.arcjoinbigarcrightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
+
