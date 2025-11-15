e6ea47ad65ad3ddfcc749f9d2d7662796c233db6
Anwesh43 <anweshthecool0@gmail.com>
2025-11-15 09:16:06 +0530

    caparclinerectview: adding Canvas extension method

--- caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
+++ caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
@@ -24,8 +24,45 @@ val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 180f
 val sweep : Float = 180f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCapArcLineRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(deg * dsc(4))
+        drawXY(-size, -size / 2) {
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            drawXY(size, 0f) {
+                drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+            }
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawArc(RectF(0f, -size / 2, size, size / 2), sweep, sweep * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawCALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawCapArcLineRect(scale, w, h, paint)
+}
