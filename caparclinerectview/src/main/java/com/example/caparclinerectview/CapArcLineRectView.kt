9932450ba884477a5e69bf2d9c7537f0e86963b5
Anwesh43 <anweshthecool0@gmail.com>
2025-11-15 09:17:12 +0530

    caparclinerectview: adding CapArcLineRectView class

--- caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
+++ caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawCALRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawCapArcLineRect(scale, w, h, paint)
 }
+
+class CapArcLineRectView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
