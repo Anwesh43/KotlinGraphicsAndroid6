1dd3ebf89111676d376d03d9a596dcc5bf9b727d
Anwesh43 <anweshthecool0@gmail.com>
2025-11-16 01:37:42 +0530

    sizeextendlinedownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.caparclinerectview.CapArcLineRectView
+import com.example.sizeextendlinedownview.SizeExtendLineDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CapArcLineRectView.create( this)
+        SizeExtendLineDownView.create( this)
     }
 }
