92b8022eda471e8a0f6418446180425d25837dad
Anwesh43 <anweshthecool0@gmail.com>
2025-11-15 09:23:08 +0530

    caparclinerectview: adding CALRNode class

--- caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
+++ caparclinerectview/src/main/java/com/example/caparclinerectview/CapArcLineRectView.kt
@@ -129,4 +129,45 @@ class CapArcLineRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CALRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CALRNode? = null
+        private var prev : CALRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CALRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCALRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CALRNode {
+            var curr : CALRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
