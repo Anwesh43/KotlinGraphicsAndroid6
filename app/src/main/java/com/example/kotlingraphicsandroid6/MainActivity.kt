f3dfdf9d342d4dee19e78781630d4b85471446d7
Anwesh43 <anweshthecool0@gmail.com>
2025-11-15 09:28:51 +0530

    caparclinerectview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.arcjoinbigarcrightview.ArcJoinBigArcRightView
+import com.example.caparclinerectview.CapArcLineRectView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcJoinBigArcRightView.create( this)
+        CapArcLineRectView.create( this)
     }
 }
