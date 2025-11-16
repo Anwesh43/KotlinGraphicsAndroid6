212afa4c1b7c5a6159211321019b96422a82f35d
Anwesh43 <anweshthecool0@gmail.com>
2025-11-17 02:05:50 +0530

    linedownarcrightview: adding LineDownArcRight class

--- linedownarcrightview/src/main/java/com/example/linedownarcrightview/LineDownArcRightView.kt
+++ linedownarcrightview/src/main/java/com/example/linedownarcrightview/LineDownArcRightView.kt
@@ -162,4 +162,27 @@ class LineDownArcRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDownArcRight(var i : Int) {
+
+        private var curr : LDARNode = LDARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
