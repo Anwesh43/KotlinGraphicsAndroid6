ab7ab335bfdeff31584b83ebb74fcd0212e89775
Anwesh43 <anweshthecool0@gmail.com>
2025-11-18 08:44:21 +0530

    shiftlinedownarcview: adding SLDANode class

--- shiftlinedownarcview/src/main/java/com/example/shiftlinedownarcview/ShiftLineDownArcView.kt
+++ shiftlinedownarcview/src/main/java/com/example/shiftlinedownarcview/ShiftLineDownArcView.kt
@@ -121,4 +121,45 @@ class ShiftLineDownArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLDANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLDANode? = null
+        private var prev : SLDANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLDANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLDANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLDANode {
+            var curr : SLDANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
