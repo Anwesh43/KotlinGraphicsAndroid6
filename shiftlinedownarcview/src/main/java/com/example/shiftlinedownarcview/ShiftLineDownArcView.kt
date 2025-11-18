e3b04b8d209ea5d75ee6934251f806efe44aea62
Anwesh43 <anweshthecool0@gmail.com>
2025-11-18 08:23:36 +0530

    shiftlinedownarcview: adding ShiftLineDownArcView class

--- shiftlinedownarcview/src/main/java/com/example/shiftlinedownarcview/ShiftLineDownArcView.kt
+++ shiftlinedownarcview/src/main/java/com/example/shiftlinedownarcview/ShiftLineDownArcView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawSLDANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawShiftLineDownArc(scale, w, h, paint)
 }
+
+class ShiftLineDownArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
