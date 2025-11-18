7f6d04f263ee2032b6d0b397ce12b534e4b0459f
Anwesh43 <anweshthecool0@gmail.com>
2025-11-18 08:58:30 +0530

    shiftlinedownarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linedownarcrightview.LineDownArcRightView
+import com.example.shiftlinedownarcview.ShiftLineDownArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineDownArcRightView.create(this)
+        ShiftLineDownArcView.create(this)
     }
 }
