ce891da0487b7345e4d555fcab1aa5261e4b2fea
Anwesh43 <anweshthecool0@gmail.com>
2025-11-19 23:17:18 +0530

    rightcaparcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.shiftlinedownarcview.ShiftLineDownArcView
+import com.example.rightcaparcjoinview.RightCapArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ShiftLineDownArcView.create(this)
+        RightCapArcJoinView.create(this)
     }
 }
