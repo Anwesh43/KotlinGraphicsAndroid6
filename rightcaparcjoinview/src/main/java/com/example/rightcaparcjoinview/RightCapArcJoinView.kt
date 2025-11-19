8b8efd68a8327790cd036f65fc0ef65a3d1600f9
Anwesh43 <anweshthecool0@gmail.com>
2025-11-19 23:04:32 +0530

    rightcaparcjoinview: adding RCAJNode class

--- rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
+++ rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
@@ -126,4 +126,45 @@ class RightCapArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RCAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RCAJNode? = null
+        private var prev : RCAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RCAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRCAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RCAJNode {
+            var curr : RCAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
