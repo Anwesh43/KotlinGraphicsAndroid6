c7c7492b3ff1a613cc7e3d8315c6d8b3a179215f
Anwesh43 <anweshthecool0@gmail.com>
2025-11-19 23:15:17 +0530

    rightcaparcjoinview: adding static create method

--- rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
+++ rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
@@ -215,4 +215,12 @@ class RightCapArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : RightCapArcJoinView {
+            val view : RightCapArcJoinView = RightCapArcJoinView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
