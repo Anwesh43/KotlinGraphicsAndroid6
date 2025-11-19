62af3a6581e66e72fcdacb96c6ac9377d7707a2a
Anwesh43 <anweshthecool0@gmail.com>
2025-11-19 18:57:58 +0530

    rightcaparcjoinview: adding Canvas extension methods

--- rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
+++ rightcaparcjoinview/src/main/java/com/example/rightcaparcjoinview/RightCapArcJoinView.kt
@@ -21,9 +21,45 @@ val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float , cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightCapArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(3))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, 90f * dsc(0), false, paint)
+        drawXY(0f, -size / 2) {
+            drawLine(0f, 0f, size * 0.5f * dsc(1), 0f, paint)
+            drawXY(size / 2, -size / 2) {
+                drawLine(0f, 0f, 0f, size * 0.5f * dsc(2), paint)
+                drawXY(0f, size / 2) {
+                    drawLine(0f, 0f, -size * dsc(4), 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawRCAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightCapArcJoin(scale, w, h, paint)
+}
