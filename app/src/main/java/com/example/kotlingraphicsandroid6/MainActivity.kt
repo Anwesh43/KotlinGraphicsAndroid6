8790281e804789131881cce84fe0d53a86037bc7
Anwesh43 <anweshthecool0@gmail.com>
2025-11-20 08:34:56 +0530

    linedropellipserotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.rightcaparcjoinview.RightCapArcJoinView
+import com.example.linedropellipserotview.LineDropEllipseRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RightCapArcJoinView.create(this)
+        LineDropEllipseRotView.create(this)
     }
 }
