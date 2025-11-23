9847ff454b99cbe01f8300ca7e5173e83d7d8251
Anwesh43 <anweshthecool0@gmail.com>
2025-11-23 08:26:04 +0530

    biarcjoindownview: adding Canvas extension methods

--- biarcjoindownview/src/main/java/com/example/biarcjoindownview/BiArcJoinDownView.kt
+++ biarcjoindownview/src/main/java/com/example/biarcjoindownview/BiArcJoinDownView.kt
@@ -29,3 +29,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiArcJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY((w / 2) * (1f - 2 * j) * (1 - dsc(2)), -size * (1 - dsc(3)) * j) {
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), start + j * rot, rot * dsc(j), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBAJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiArcJoinDown(scale, w, h, paint)
+}
