fae87eee0b3c8bb82cea0a4437f787aee3124034
Anwesh43 <anweshthecool0@gmail.com>
2025-11-24 02:44:38 +0530

    arcrotlinedownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biarcjoindownview.BiArcJoinDownView
+import com.example.arcrotlinedownview.ArcRotLineDownView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcJoinDownView.create(this)
+        ArcRotLineDownView.create(this)
     }
 }
