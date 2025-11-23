5cc1ddf19de317b630a548f5f60c9d621d009ac6
Anwesh43 <anweshthecool0@gmail.com>
2025-11-24 02:42:11 +0530

    arcrotlinedownview: adding Renderer class

--- arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
+++ arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
@@ -189,4 +189,27 @@ class ArcRotLineDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcRotLineDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val arld : ArcRotLineDown = ArcRotLineDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            arld.draw(canvas, paint)
+            animator.animate {
+                arld.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            arld.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
