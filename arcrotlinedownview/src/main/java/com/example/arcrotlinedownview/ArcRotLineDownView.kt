bf7897aa1a8b38a57ade892074923d3deecf89af
Anwesh43 <anweshthecool0@gmail.com>
2025-11-24 02:40:28 +0530

    arcrotlinedownview: adding ArcRotLineDownv class

--- arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
+++ arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
@@ -166,4 +166,27 @@ class ArcRotLineDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcRotLineDown(var i : Int) {
+
+        private var curr : ARLDNode = ARLDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
