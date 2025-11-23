9c0452399bbda93884dd485471774ad663a4fab2
Anwesh43 <anweshthecool0@gmail.com>
2025-11-23 09:03:03 +0530

    biarcjoindownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biquarterarcsemiview.BiQuarterArcSemiView
+import com.example.biarcjoindownview.BiArcJoinDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiQuarterArcSemiView.create(this)
+        BiArcJoinDownView.create(this)
     }
 }
