adeb4afa9c21163c9cfa78b3aba97aa22e794348
Anwesh43 <anweshthecool0@gmail.com>
2025-11-24 02:38:17 +0530

    arcrotlinedownview: adding ARLDNode class

--- arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
+++ arcrotlinedownview/src/main/java/com/example/arcrotlinedownview/ArcRotLineDownView.kt
@@ -125,4 +125,45 @@ class ArcRotLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ARLDNode? = null
+        private var prev : ARLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARLDNode{
+            var curr : ARLDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
