9f9b2eeeeeb5b3ceb19a6d2f6771594617cbbc4e
Anwesh43 <anweshthecool0@gmail.com>
2025-11-25 08:03:55 +0530

    biarcleftcircleview: adding BiArcLeftCircleView class

--- biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
+++ biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBALCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiArcLeftCircle(scale, w, h, paint)
 }
+
+class BiArcLeftCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
