41181150cc918e0cc6ba2bd1637b911774332b8a
Anwesh43 <anweshthecool0@gmail.com>
2025-11-25 08:14:50 +0530

    biarcleftcircleview: adding BALCNode class

--- biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
+++ biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
@@ -122,4 +122,45 @@ class BiArcLeftCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BALCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BALCNode? = null
+        private var prev : BALCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BALCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBALCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BALCNode {
+            var curr : BALCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
