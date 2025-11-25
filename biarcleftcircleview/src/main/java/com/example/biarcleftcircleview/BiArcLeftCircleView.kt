46dfc997e4e495d51bf9204e6993a1c68cd99000
Anwesh43 <anweshthecool0@gmail.com>
2025-11-25 08:33:53 +0530

    biarcleftcircleview: adding drawLine fix

--- biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
+++ biarcleftcircleview/src/main/java/com/example/biarcleftcircleview/BiArcLeftCircleView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
@@ -40,13 +40,14 @@ fun Canvas.drawBiArcLeftCircle(scale : Float, w : Float, h : Float, paint : Pain
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
         for (j in 0..1) {
             drawXY(-size * j, 0f) {
                 rotate(-rot * dsc(2) * (1 - j))
                 drawArc(RectF(0f, -size / 2, size, size / 2), 0f, rot * dsc(j), false, paint)
             }
         }
+        drawLine(0f, 0f, -size * dsc(3), 0f, paint)
     }
 }
 
