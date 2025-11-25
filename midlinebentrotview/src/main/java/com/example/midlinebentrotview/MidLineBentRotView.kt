be30b976932361a0b9bf1a6832fad35cc1960d52
Anwesh43 <anweshthecool0@gmail.com>
2025-11-26 01:14:52 +0530

    midlinebentrotview: adding MLBRNode class

--- midlinebentrotview/src/main/java/com/example/midlinebentrotview/MidLineBentRotView.kt
+++ midlinebentrotview/src/main/java/com/example/midlinebentrotview/MidLineBentRotView.kt
@@ -123,4 +123,45 @@ class MidLineBentRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MLBRNode? = null
+        private var prev : MLBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLBRNode {
+            var curr : MLBRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
