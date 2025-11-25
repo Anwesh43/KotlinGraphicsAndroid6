79627b30e1c4d8ccd40ac810d3d5f012d587902b
Anwesh43 <anweshthecool0@gmail.com>
2025-11-26 01:08:59 +0530

    midlinebentrotview: adding Canvas extension methods

--- midlinebentrotview/src/main/java/com/example/midlinebentrotview/MidLineBentRotView.kt
+++ midlinebentrotview/src/main/java/com/example/midlinebentrotview/MidLineBentRotView.kt
@@ -23,7 +23,40 @@ val deg : Float = 45f
 val backColor : Int = "#BDBDBD".toColorInt()
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
-
+val mulDeg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMidLineBentRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(mulDeg * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, -size) {
+                rotate((rot + deg * j) * dsc(1 + j))
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawMLBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawMidLineBentRot(scale, w, h, paint)
+}
