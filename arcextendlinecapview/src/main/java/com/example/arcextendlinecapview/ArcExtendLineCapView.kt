fb7b976b06ad5a34879f74e89fdce413678d4d29
Anwesh43 <anweshthecool0@gmail.com>
2025-11-27 22:38:28 +0530

    arcextendlinecapview: adding Canvas extension methods

--- arcextendlinecapview/src/main/java/com/example/arcextendlinecapview/ArcExtendLineCapView.kt
+++ arcextendlinecapview/src/main/java/com/example/arcextendlinecapview/ArcExtendLineCapView.kt
@@ -27,3 +27,38 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcExtendLineCap(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + paint.strokeWidth) * dsc(4), h / 2) {
+        drawXY(-size, 0f) {
+            drawXY(0f, 0f) {
+                rotate(-rot * dsc(2))
+                drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f, 180f * dsc(0), false, paint)
+            }
+            drawLine(0f, 0f, size * dsc(1), -size * dsc(1), paint)
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(3), paint)
+
+    }
+}
+
+fun Canvas.drawAELCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcExtendLineCap(scale, w, h, paint)
+}
