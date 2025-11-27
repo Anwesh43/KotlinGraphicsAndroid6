8d7b5343533d93d3042f5461b04b0e4d18124af5
Anwesh43 <anweshthecool0@gmail.com>
2025-11-27 22:45:16 +0530

    arcextendlinecapview: adding AELCNode class

--- arcextendlinecapview/src/main/java/com/example/arcextendlinecapview/ArcExtendLineCapView.kt
+++ arcextendlinecapview/src/main/java/com/example/arcextendlinecapview/ArcExtendLineCapView.kt
@@ -125,4 +125,45 @@ class ArcExtendLineCapView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AELCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AELCNode? = null
+        private var prev : AELCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AELCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAELCNode(i, state.scale, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AELCNode {
+            var curr : AELCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
