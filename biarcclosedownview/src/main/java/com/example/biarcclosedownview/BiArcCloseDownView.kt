7701e5b4dc9fbb459273d2bc2ff13b51f36f79d6
Anwesh43 <anweshthecool0@gmail.com>
2025-11-29 08:00:39 +0530

    biarcclosedownview: adding BiArcCloseDownView class

--- biarcclosedownview/src/main/java/com/example/biarcclosedownview/BiArcCloseDownView.kt
+++ biarcclosedownview/src/main/java/com/example/biarcclosedownview/BiArcCloseDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBACDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiArcCloseDown(scale, w, h, paint)
 }
+
+class BiArcCloseDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
