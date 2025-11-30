fe9e4f77d79febede3e96c0aa9fe458fa04c5108
Anwesh43 <anweshthecool0@gmail.com>
2025-11-30 17:11:27 +0530

    linebentjoinarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.biarcclosedownview.BiArcCloseDownView
+import com.example.linebentjoinarcview.LineBentJoinArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcCloseDownView.create(this)
+        LineBentJoinArcView.create(this)
     }
 }
