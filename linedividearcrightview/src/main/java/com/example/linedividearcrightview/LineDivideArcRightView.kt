d231fb99571a61bde2afb55315878912b6b2ace9
Anwesh43 <anweshthecool0@gmail.com>
2025-12-01 16:47:53 +0530

    linedividearcrightview: adding LineDivideArcRight clss

--- linedividearcrightview/src/main/java/com/example/linedividearcrightview/LineDivideArcRightView.kt
+++ linedividearcrightview/src/main/java/com/example/linedividearcrightview/LineDivideArcRightView.kt
@@ -168,4 +168,27 @@ class LineDivideArcRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDivideArcRight(var i : Int) {
+
+        private var curr : LDARNode = LDARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
