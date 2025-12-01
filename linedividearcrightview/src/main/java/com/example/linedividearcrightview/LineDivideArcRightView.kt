57e0ba88c4bf6bc13d4a7c31960781b7d023e1d1
Anwesh43 <anweshthecool0@gmail.com>
2025-12-01 13:44:44 +0530

    linedividearcrightview: adding Canvas extension methods

--- linedividearcrightview/src/main/java/com/example/linedividearcrightview/LineDivideArcRightView.kt
+++ linedividearcrightview/src/main/java/com/example/linedividearcrightview/LineDivideArcRightView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 7
+val scGap : Float = 0.06f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -27,3 +27,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDivideArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(6), h / 2) {
+        rotate(rot * dsc(5))
+        drawXY(-size * dsc(4), 0f) {
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            drawArc(RectF(-size, -size, size, size), -90f, 90f * dsc(1), false, paint)
+            drawXY(size, 0f) {
+                drawArc(RectF(0f, -size, 2 * size, size), 180f, 90f * dsc(2), false, paint)
+            }
+            drawXY(0f, -size) {
+                drawLine(0f, 0f, 0f, size * dsc(3), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLDARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineDivideArcRight(scale, w, h, paint)
+}
