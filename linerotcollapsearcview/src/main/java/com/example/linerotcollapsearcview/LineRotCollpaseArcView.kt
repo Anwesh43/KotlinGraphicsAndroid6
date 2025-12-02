debf8483503c00ac2087003cf6ed860391d28066
Anwesh43 <anweshthecool0@gmail.com>
2025-12-02 16:56:58 +0530

    linerotcollapsearcview: adding LineRotCollapseArcView class

--- linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
+++ linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLRCANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineRotCollapseArc(scale, w, h, paint)
 }
+
+class LineRotCollapseArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
