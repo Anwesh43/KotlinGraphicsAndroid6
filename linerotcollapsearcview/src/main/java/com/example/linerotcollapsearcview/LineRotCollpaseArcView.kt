c90095b47dabd66c58779821281e9290bbe5560d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-02 16:53:10 +0530

    linerotcollapsearcview: adding Canvas extension methods

--- linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
+++ linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
@@ -16,14 +16,49 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val rot : Float = 90f
 val backColor : Int = "#BDBDBD".toColorInt()
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotCollapseArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, -size * j) {
+                rotate(-deg * dsc(3) * (1f - 2 * j))
+                drawLine(0f, size * 0.5f * (1 - dsc(0)) * (1 - j), 0f, size * 0.5f * (1 - j) + -size * 0.5f * dsc(2) * j, paint)
+            }
+        }
+        drawArc(RectF(-size / 2, -size, size / 2, 0f), 90f, 180f * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawLRCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineRotCollapseArc(scale, w, h, paint)
+}
