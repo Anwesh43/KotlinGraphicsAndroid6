2e839d23385caee837b749a76fcbe5d17848371c
Anwesh43 <anweshthecool0@gmail.com>
2025-12-02 17:05:39 +0530

    linerotcollapsearcview: adding Renderer class

--- linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
+++ linerotcollapsearcview/src/main/java/com/example/linerotcollapsearcview/LineRotCollpaseArcView.kt
@@ -189,4 +189,27 @@ class LineRotCollapseArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotCollapseArcView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lrca : LineRotCollapseArc = LineRotCollapseArc(0)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrca.draw(canvas, paint)
+            animator.animate {
+                lrca.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrca.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
