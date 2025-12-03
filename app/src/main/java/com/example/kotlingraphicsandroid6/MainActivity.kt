2ec143f91c87d10cc4b971a5777a1ee8689b5b71
Anwesh43 <anweshthecool0@gmail.com>
2025-12-03 15:57:43 +0530

    extendbiarclineview: adding view in activit

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linerotcollapsearcview.LineRotCollapseArcView
+import com.example.extendbiarclineview.ExtendBiArcLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotCollapseArcView.create(this)
+        ExtendBiArcLineView.create(this)
     }
 }
