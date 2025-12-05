6b37c3b6583a0c6bf7e6c94170307cdf0113cc52
Anwesh43 <anweshthecool0@gmail.com>
2025-12-05 11:57:38 +0530

    capturelineleftview: adding Canvas extension methods

--- capturnlineleftview/src/main/java/com/example/capturnlineleftview/CapTurnLineLeftView.kt
+++ capturnlineleftview/src/main/java/com/example/capturnlineleftview/CapTurnLineLeftView.kt
@@ -28,3 +28,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCapTurnLineLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, -size * (1 - dsc(3))) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+        drawLine(0f, 0f, 0f, -size * (dsc(0) - dsc(3)), paint)
+    }
+}
+
+fun Canvas.drawCTLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawCapTurnLineLeft(scale, w, h, paint)
+}
