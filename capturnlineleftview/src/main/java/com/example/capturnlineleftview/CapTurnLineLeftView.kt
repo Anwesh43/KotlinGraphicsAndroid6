88ca6e4c1d9d44f6915fad5caffad23cdef0ea01
Anwesh43 <anweshthecool0@gmail.com>
2025-12-06 09:38:38 +0530

    capturelineleftview: adding CTLLNode class

--- capturnlineleftview/src/main/java/com/example/capturnlineleftview/CapTurnLineLeftView.kt
+++ capturnlineleftview/src/main/java/com/example/capturnlineleftview/CapTurnLineLeftView.kt
@@ -121,4 +121,45 @@ class CapTurnLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CTLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CTLLNode? = null
+        private var prev : CTLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CTLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCTLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CTLLNode {
+            var curr : CTLLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
