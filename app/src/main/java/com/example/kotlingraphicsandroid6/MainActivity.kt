9f6396f9d8eafb1e586271c1504257290cde464d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-08 12:24:04 +0530

    linebentjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.capturnlineleftview.CapTurnLineLeftView
+import com.example.linebentrotjoinview.LineBentRotJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CapTurnLineLeftView.create(this)
+        LineBentRotJoinView.create(this)
     }
 }
