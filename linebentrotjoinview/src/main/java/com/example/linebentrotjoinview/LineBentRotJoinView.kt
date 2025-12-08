479a9d4ba5648582c04c39617054dd4fc5ce4032
Anwesh43 <anweshthecool0@gmail.com>
2025-12-08 11:38:42 +0530

    linebentjoinview: adding LineBentRotJoinView class

--- linebentrotjoinview/src/main/java/com/example/linebentrotjoinview/LineBentRotJoinView.kt
+++ linebentrotjoinview/src/main/java/com/example/linebentrotjoinview/LineBentRotJoinView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLBRJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentRotJoin(scale, w, h, paint)
 }
+
+class LineBentRotJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
