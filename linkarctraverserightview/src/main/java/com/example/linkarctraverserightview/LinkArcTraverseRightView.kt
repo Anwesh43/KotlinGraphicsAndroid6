a7e704e5dd78e1c5aceee4d3384952151da0f0bf
Anwesh43 <anweshthecool0@gmail.com>
2025-12-09 00:52:11 +0530

    linktraverserightview: adding module setup

---
+++ linkarctraverserightview/src/main/java/com/example/linkarctraverserightview/LinkArcTraverseRightView.kt
@@ -0,0 +1,28 @@
+package com.example.linkarctraverserightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val arcs : Int = 5
+val parts : Int = arcs + 1
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val sweep : Float = 180f
+
+
