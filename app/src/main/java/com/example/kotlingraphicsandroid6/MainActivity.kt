1d1aec22899d5cb1311963fa01cd35768f7a6c9d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-09 02:46:58 +0530

    linktraverserightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linebentrotjoinview.LineBentRotJoinView
+import com.example.linkarctraverserightview.LinkArcTraverseRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentRotJoinView.create(this)
+        LinkArcTraverseRightView.create(this)
     }
 }
