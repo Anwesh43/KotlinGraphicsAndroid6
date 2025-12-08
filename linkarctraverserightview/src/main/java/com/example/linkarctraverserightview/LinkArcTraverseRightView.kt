039cc544a20b8b50bcda868eb13385839dc84b61
Anwesh43 <anweshthecool0@gmail.com>
2025-12-09 01:02:55 +0530

    linktraverserightview: adding LinkArcTraverseRightView class

--- linkarctraverserightview/src/main/java/com/example/linkarctraverserightview/LinkArcTraverseRightView.kt
+++ linkarctraverserightview/src/main/java/com/example/linkarctraverserightview/LinkArcTraverseRightView.kt
@@ -60,3 +60,18 @@ fun Canvas.drawLATRNode(i : Int, scale : Float, paint : Paint) {
     drawLinkArcTraverseRight(scale, w, h, paint)
 }
 
+class LinkArcTraverseRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
