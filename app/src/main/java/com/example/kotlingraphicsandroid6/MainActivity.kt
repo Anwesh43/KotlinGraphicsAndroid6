aa4f32da916f180b22cdadb1862f8542b8dc1317
Anwesh43 <anweshthecool0@gmail.com>
2025-12-10 10:16:44 +0530

    linearcbentupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsandroid6/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.kotlingraphicsandroid6
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.linkarctraverserightview.LinkArcTraverseRightView
+import com.example.linearcbentupview.LineArcBentUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkArcTraverseRightView.create(this)
+        LineArcBentUpView.create(this)
     }
 }
